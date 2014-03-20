de59acc4327a1d229391030e96f7711537f43605
Vishwanath Venkatesan <vishwanath.venkatesan@intel.com>
2014-03-20 14:08:13 -0700

    Modified build.sh to use getopt

--- build.sh
+++ build.sh
@@ -2,26 +2,28 @@
 
 if [ $# -lt 2 ]
 then
-    echo "Usage: ./build.sh <install-path> <jemalloc-prefix>"
+    echo "Usage: $0 [-i <install path> (default <pwd>)] [-j <jemalloc source>] [-p prefix (default: /usr)]"
     exit 1
 fi
 
-if [ -z $1 ]
-then
-    echo "Empty string for install path"
-    exit 1
-fi
-
-if [ -z $2 ]
-then
-    echo "Empty string for jemalloc-prefix"
-    exit 1
-fi
-
-DESTDIR=$1
-JEPREFIX=$2
+DESTDIR=$(dirname $0)
 PREFIX=/usr
 
+while getopts i:j:p: opt
+do
+    case "$opt" in
+      i)  DESTDIR="$OPTARG";;
+      j)  JEPREFIX="$OPTARG";;
+      p)  PREFIX="$OPTARG";;
+        \?)# unknown flag
+          echo >&2 \
+                "usage: $0 [-i <install path> (default <pwd>)] [-j <jemalloc source>] [-p prefix] "
+  exit 1;;
+    esac
+done
+shift `expr $OPTIND - 1`
+
+echo $PREFIX
 make CC=gcc JEPREFIX=$JEPREFIX PREFIX=$PREFIX
 make install DESTDIR=$DESTDIR
 
