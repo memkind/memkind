666e6a5e0ab13713c41791cab67a6506418b53ee
Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>
2014-03-25 11:55:42 -0700

    build.sh now just calls make -f make_rpm.mk

--- build.sh
+++ build.sh
@@ -1,34 +1,3 @@
 #!/bin/sh
 
-if [ $# -lt 2 ]
-then
-    echo "Usage: $0 [-i <install path> (default <pwd>)] [-j <jemalloc source>] [-p prefix (default: /usr)]"
-    exit 1
-fi
-
-DESTDIR=`pwd`
-
-PREFIX=/usr
-
-while getopts i:j:p: opt
-do
-    case "$opt" in
-      i)  DESTDIR="$OPTARG";;
-      j)  JEPREFIX="$OPTARG";;
-      p)  PREFIX="$OPTARG";;
-        \?)# unknown flag
-          echo >&2 \
-                "usage: $0 [-i <install path> (default <pwd>)] [-j <jemalloc source>] [-p prefix] "
-  exit 1;;
-    esac
-done
-shift `expr $OPTIND - 1`
-
-echo $PREFIX
-make CC=gcc JEPREFIX=$JEPREFIX PREFIX=$PREFIX
-make install DESTDIR=$DESTDIR
-cd test
-make CC=gcc JEMALLOC_PATH=$JEPREFIX MCDRAM_INSTALL_DIR=$DESTDIR/$PREFIX ROOT_DIR=`pwd`
-
-
-
+make -f make_rpm.mk
