a7c4d6f029ffb508c2e4cffb1442105ad35c4ac1
Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>
2014-03-27 15:44:37 -0700

    got non-interactive ssh-copy-id working and some frills

--- test/bin/test.sh
+++ test/bin/test.sh
@@ -1,18 +1,37 @@
 #!/bin/bash
+ckpt=/hd2/simics/workspace/booted-knl-mcdram.ckpt
 knlmcdram=192.168.1.2
 basedir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+if [ $# -ne 0 ]
+then
+    if [ $1 == --help ] || [ $1 == -h ]
+    then
+        echo "Usage: $0 [checkpoint]"
+        exit 1
+    fi
+    ckpt=$1
+fi
 
 ping -c 1 -W 1 $knlmcdram
 if [ $? -ne 0 ]
 then
-    echo "read-configuration /hd2/simics/workspace/booted-knl-mcdram.ckpt" > simics.in
+    export DISPLAY=:0
+    echo "read-configuration $ckpt" > simics.in
     echo "continue" >> simics.in
-    nohup simics < simics.in >& simics.out &
+    simics < simics.in >& simics.out &
+    echo $! > simics-pid
     sleep 15
-fi
+    rm simics.in
+    echo '#!/bin/sh' > askpass.sh
+    echo 'echo $PASSWORD' >> askpass.sh
+    chmod u+x askpass.sh
+    export SSH_ASKPASS=./askpass.sh
+    PASSWORD=mic setsid ssh-copy-id mic@$knlmcdram
+    PASSWORD=root setsid ssh-copy-id root@$knlmcdram
+    rm askpass.sh
+ fi
 
-ssh-copy-id mic@$knlmcdram
-ssh-copy-id root@$knlmcdram
 scp ~/rpmbuild/RPMS/x86_64/numakind-0.0*.rpm mic@$knlmcdram:
 scp ~/rpmbuild/RPMS/x86_64/jemalloc-3.5*.rpm mic@$knlmcdram:
 scp /usr/lib64/libgtest*.so.0.0.0 mic@$knlmcdram:
@@ -24,4 +43,8 @@ ssh root@$knlmcdram "install ~mic/libgtest*.so.0.0.0 /usr/lib64"
 ssh root@$knlmcdram "/sbin/ldconfig"
 ssh mic@$knlmcdram "./all_tests --gtest_output=xml:all_tests.xml"
 scp mic@$knlmcdram:all_tests.xml .
-
+if [ -f simics-pid ]
+then
+    kill -9 `cat simics-pid`
+    rm simics-pid
+fi
