a760f5d09fbede9b9e5af0fe7d9423d1af1bbf03
Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>
2014-03-28 13:56:03 -0700

    whitespace change

--- test/check.h
+++ test/check.h
@@ -5,7 +5,6 @@ class Check {
   public:
     int check_page_hbw(size_t num_bandwidth, const int *bandwidth, 
                      const void *ptr, size_t size);
-
     int check_page_size(void *ptr, size_t size, size_t page_size);
   private:
     unsigned long long get_physaddr(void *vaddr, size_t *page_size);
