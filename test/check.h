22367e2bffa3bbe5e3ca6918e152e61a767266a3
Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>
2014-03-28 11:33:13 -0700

    added header guards to check.h

--- test/check.h
+++ test/check.h
@@ -1,7 +1,15 @@
+#ifndef check_include_h
+#define check_include_h
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 int check_page_hbw(size_t num_bandwidth, const int *bandwidth, 
                    const void *ptr, size_t size);
 
 int check_page_size(void *ptr, size_t size, size_t page_size);
 
-
-
+#ifdef __cplusplus
+}
+#endif
+#endif
