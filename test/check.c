941032528ff162f47d68163a2605f45fec42d19a
Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>
2014-03-28 11:29:06 -0700

    fixe compile time errors, got rid of bit mask

--- test/check.c
+++ test/check.c
@@ -1,83 +1,64 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <numaif.h>
 #include <check.h>
-#include <asm/io.h
 
-static int bandwith_to_hbw_mask(size_t num_bandwidth, int *bandwidth, 
-                                 struct bitmask *hbw_mask)
+
+int check_page_hbw(size_t num_bandwidth, const int *bandwidth, 
+                   const void *ptr, size_t size)
 {
+    int err = 0;
+    const size_t page_size = 4096;
+    int i, nr_pages, max_bandwidth;
+    void **address = NULL;
+    int *status = NULL;
+
+    if (size == 0) {
+        return 0;
+    }
+
+    nr_pages = size / page_size;
+    nr_pages += size % page_size ? 1 : 0;
+
+    address = (void**)malloc(sizeof(void *) * nr_pages);
+    if (!address) {
+        fprintf(stderr, "WARNING:  <check_page_hbw> "
+                        "failed in call to malloc(%i)\n", 
+                        nr_pages * sizeof(void*));
+        return -2;
+    }
+    status = (int*)malloc(sizeof(int) * nr_pages);
+    if (!address) {
+        fprintf(stderr, "WARNING:  <check_page_hbw> "
+                        "failed in call to malloc(%i)\n", 
+                        nr_pages * sizeof(int));
+        free(address);
+        return -2;
+    }
+
+    for (i = 0; i < nr_pages; ++i) {
+        address[i] = (char *)ptr + i * page_size;
+    }
+
+    move_pages(0, nr_pages, address, NULL, status, MPOL_MF_MOVE);
+
     max_bandwidth = 0;
     for (i = 0; i < num_bandwidth; ++i) {
         max_bandwidth = bandwidth[i] > max_bandwidth ? 
                         bandwidth[i] : max_bandwidth;
     }
 
-    if (max_bandwidth == 0) {
-        fprintf(stderr, "WARNING:  <bandwidth_to_hbw_mask> "
-                        "all bandwidths specified are zero\n");
-        numa_bitmask_clearall(*hbw_mask);
-        return -3;
-    }
-
-    for (i = 0; i < num_bandwidth; ++i) {
-        if (bandwidth[i] == max_bandwidth) {
-            num_bitmask_setbit(hbw_mask, i);
-        }
-    }
-    return 0;
-}
-
-int check_page_hbw(void *ptr, size_t size, size_t page_size, 
-                   size_t num_bandwidth, int *bandwidth)
-{
-    int err = 0;
-    int i, nr_pages;
-    struct bitmask *hbw_mask = NULL;
-    void **address = NULL;
-
-    if (size == 0) {
-        return 0;
-    }
-    hbw_mask = numa_bitmask_alloc(num_bandwidth);
-    if (hbw_mask == NULL) {
-        fprintf(stderr, "WARNING:  <check_page_hbw> "
-                        "failed in call to numa_bitmask_alloc(%i)\n", 
-                num_bandwidth);
-        return -2;
-    }
-
-    err = bandwidth_to_hbw_mask(num_bandwidth, hbw_mask);
-    if (err) {
-        numa_bitmask_free(hbw_mask);
-        return err;
-    }
-
-    nr_pages = size / page_size;
-    nr_pages += size % page_size ? 1 : 0;
-
-    address = malloc(sizeof(void *) * nr_pages);
-    if (!address) {
-        fprintf(stderr, "WARNING:  <check_pages_in_numa_group> "
-                        "failed in call to malloc(%i)\n", 
-                sizeof(void*) * nr_pages);
-        numa_bitmask_free(hbw_mask);
-        return -2;
-    }
-
-    for (i = 0; i < nr_pages; ++i) {
-        address[i] = ptr + i * pages_size;
-    }
-
-    move_pages(0, nr_pages, address, NULL, status, MPOL_MF_MOVE);
-
-    if (!numa_bitmask_isbitset(nodmaask.n, status[0])) {
+    if (bandwidth[status[0]] != max_bandwidth) {
         err = -1;
     }
     for (i = 1; i < nr_pages && !err; ++i) {
-        if (!numa_bitmask_isbitset(nodemask.n, status[i])) {
+        if (status[i] >= num_bandwidth ||
+            bandwidth[status[i]] != max_bandwidth) {
             err = i;
         }
     }
-    free(adress);
-    numa_bitmask_free(hbw_mask);
+    free(status);
+    free(address);
     return err;
             
 }
