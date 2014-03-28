6cde18b4e414f9fa9e87645620a2973c1e36666f
Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>
2014-03-28 11:30:37 -0700

    adding check.h

---
+++ test/check.h
@@ -0,0 +1,7 @@
+int check_page_hbw(size_t num_bandwidth, const int *bandwidth, 
+                   const void *ptr, size_t size);
+
+int check_page_size(void *ptr, size_t size, size_t page_size);
+
+
+
