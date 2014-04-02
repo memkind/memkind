e533b4d5fa09cb7e195a13d28127464af93abe45
Vishwanath Venkatesan <vishwanath.venkatesan@intel.com>
2014-04-01 19:27:01 -0700

    Adding the modified check page_size function Fixing errors and warnings. Needs a lot more work before testing

--- test/check.cpp
+++ test/check.cpp
@@ -6,62 +6,48 @@
 #include <fcntl.h>
 #include <numaif.h>
 #include <errno.h>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+#include <fstream>
+#include <string>
+
 #include "check.h"
 
+
+using namespace std;
 
 Check::Check(const void *ptr, size_t size)
 {
     const int min_page_size = 4096;
+    int i;
 
     num_address = size / min_page_size;
     num_address += size % min_page_size ? 1 : 0;
 
-    address = new (void *)[num_address];
+    address = new void* [num_address];
 
     for (i = 0; i < num_address; ++i) {
-        address[i] = (char *)ptr + i * page_size;
+        address[i] = (char *)ptr + i * min_page_size;
     }
 
-    pagemap_fd = open("/proc/self/pagemap", O_RDONLY);
-    if (pagemap_fd < 0) {
-        throw errno;
-    }
 }
 
 Check::~Check()
 {
     delete[] address;
-    if (close(pagemap_fd)){
-        throw errno;
-    }
-}
-
-Check::Check(const Check &other)
-{
-    num_address = other.num_address;
-
-    address = new (void *)[num_address];
-    for (i = 0; i < num_address; ++i) {
-        address[i] = other.address[i];
-
-    pagemap_fd = open("/proc/self/pagemap", O_RDONLY);
-    if (pagemap_fd < 0) {
-        throw errno;
-    }
+ 
 }
 
 int Check::check_node_hbw(size_t num_bandwidth, const int *bandwidth)
 {
     int err = 0;
-    int i, num_address, max_bandwidth;
+    int i, max_bandwidth;
     size_t j;
     int *status = NULL;
 
-    if (size == 0) {
-        return 0;
-    }
 
-    status = new (int)[num_address]
+    status = new int [num_address];
 
     move_pages(0, num_address, address, NULL, status, MPOL_MF_MOVE);
 
@@ -88,10 +74,52 @@ int Check::check_node_hbw(size_t num_bandwidth, const int *bandwidth)
 
 }
 
+void Check::skip_lines(ifstream &ip, int num_lines){
+   
+    int i;
+    string temp;
+    for (i = 0; i < num_lines;
+         i++){
+      getline (ip, temp);
+    }
+}
+
+void Check::get_address_range(string &line,
+                              unsigned long long *start_addr,
+                              unsigned long long *end_addr){
+    stringstream ss(line);
+    string token;
+    
+    getline(ss, token, '-');
+    *start_addr = strtoul(token.c_str(),
+                         NULL,
+                         16);
+    getline(ss, token, '-');
+    *end_addr = strtoul(token.c_str(),
+                        NULL,
+                        16); 
+}
+
+size_t Check::get_kpagesize(string line){
+  
+    stringstream ss(line);
+    string token;
+    size_t pagesize;
+    
+    ss  >> token; 
+    ss  >> token;
+    
+    pagesize = atol(token.c_str());
+    
+    return (size_t)pagesize;
+}
+
 int Check::check_page_size(size_t page_size)
 {
     int err = 0;
     int i;
+
+    ip.open ("/proc/self/smaps");
 
     if (!check_page_size(page_size, address[0])) {
         err = -1;
@@ -105,21 +133,36 @@ int Check::check_page_size(size_t page_size)
 }
 
 int Check::check_page_size(size_t page_size, void *vaddr){
-    int err = 0;
-    const int PAGEMAP_ENTRY = 8;
-    int n;
-    unsigned long long addr;
+  
 
-    n = pread(pagemap_fd, &addr, PAGEMAP_ENTRY,
-              (((unsigned long long)vaddr/page_size)*PAGEMAP_ENTRY));
+    ifstream ip;
+    string read;
+    unsigned long long virt_addr;
+    size_t lpagesize;
+    
+    virt_addr = (unsigned long long)(vaddr);
 
-    if (n != PAGEMAP_ENTRY) {
-        throw errno;
+    while (!ip.eof()){
+      ip >> read;
+      start_addr = end_addr = 0;
+      get_address_range(read, &start_addr,
+                        &end_addr);
+      if ((virt_addr >= start_addr) &&
+          (virt_addr <= end_addr)){
+        
+        skip_lines(ip, 12);
+        getline(ip, read);
+        lpagesize = get_kpagesize(read);
+        if (lpagesize == page_size){
+          return 1;
+        }
+      }
+      else{
+        skip_lines(ip,14);
+      }
     }
 
-    if (!(addr & (1ULL<<63))) {
-        err = 1;
-    }
-
-    return err;
+    /*Never found a match!*/
+    return 0;
 }
+
--- test/check.h
+++ test/check.h
@@ -1,17 +1,29 @@
 #ifndef check_include_h
 #define check_include_h
 
+
+using namespace std;
+
 class Check {
   public:
     Check(const void *ptr, size_t size);
     ~Check();
     int check_node_hbw(size_t num_bandwidth, const int *bandwidth);
     int check_page_size(size_t page_size);
+    int check_page_size(size_t page_size, void *vaddr);
   private:
     void **address;
-    int pagemap_fd;
+    unsigned long long start_addr;
+    unsigned long long end_addr;
+    ifstream ip;
+    int smaps_fd;
     int num_address;
-    int is_correct_page_size(void *vaddr, size_t page_size);
+    void skip_lines(ifstream &, int num_lines);
+    void get_address_range(string &line, unsigned long long *start_addr,
+                          unsigned long long *end_addr);
+    size_t get_kpagesize(string line);
+    
+    
 };
 
 #endif
