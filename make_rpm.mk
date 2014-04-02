4bef9f48e447e94f087a57db660a8b9a0fc2e55f
James Anchell <james.l.anchell@intel.com>
2014-04-02 10:40:15 -0700

    Enable incremental builds.

--- make_rpm.mk
+++ make_rpm.mk
@@ -2,15 +2,25 @@ topdir = $(HOME)/rpmbuild
 version = $(shell git describe | sed 's|[^0-9]*\([0-9]*\.[0-9]*\.[0-9]*\).*|\1|')
 specfile = $(topdir)/SPECS/numakind-$(version).spec
 source_tar = $(topdir)/SOURCES/numakind-$(version).tar.gz
-gitrev ?= $(shell git rev-parse --abbrev-ref HEAD)
+#revision ?= $(shell git rev-parse --abbrev-ref HEAD)
+arch = $(shell uname -p)
+rpm = $(topdir)/RPMS/$(arch)/numakind-$(version)-1.$(arch).rpm
+src = $(shell find)
 
 include make.spec
 
-all: $(source_tar) $(specfile)
-	rpmbuild -ba -E '%define _topdir $(topdir)' $(specfile)
+all: $(rpm)
 
-$(source_tar): $(topdir)
-	git archive $(gitrev) -o $@
+$(rpm): $(source_tar) $(specfile)
+	rpmbuild -bc --short-circuit -E '%define _topdir $(topdir)' $(specfile)
+
+$(source_tar): $(topdir) $(src)
+	if [ -n "$(revision)" ]; then \
+	  git archive $(revision) -o $@; \
+	else \
+	  tar cvf $@ *; \
+	fi ; \
+	rpmbuild -bp -E '%define _topdir $(topdir)' $(specfile)	
 
 $(specfile): $(topdir) make.spec
 	@echo "$$numakind_specfile" > $@
@@ -20,3 +30,5 @@ $(topdir):
 
 clean:
 	rm -rf $(topdir)
+
+.PHONY: all prep clean
