786aca780c9251365eec780577270c93b086d5e3
Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>
2014-04-16 17:36:05 -0700

    doxygen comments for the hbw_policy_t enum

--- hbwmalloc.h
+++ hbwmalloc.h
@@ -3,9 +3,27 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
+/*!
+ *  \file hbwmalloc.h
+ */
 
+/*!
+ *  \brief Fallback policy.
+ *
+ *  Policy that determines behavior when there is not enough free high
+ *  bandwidth memory to satisfy a user request.  This enum is used with
+ *  hbw_get_policy() and hbw_set_policy().
+ */
 typedef enum {
+/*!
+ *  If insufficient high bandwidth memory pages are available seg
+ *  fault when memory is touched (default).
+ */
     HBW_POLICY_BIND = 1,
+/*!
+ *  If insufficient high bandwidth memory pages are available fall
+ *  back on standard memory pages.
+ */
     HBW_POLICY_PREFERRED = 2
 } hbw_policy_t;
 
