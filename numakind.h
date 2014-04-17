7368b4e309e712fb81e360501269bf0700665266
Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>
2014-04-16 18:08:13 -0700

    added a mainpage entry

--- numakind.h
+++ numakind.h
@@ -4,6 +4,21 @@
 extern "C" {
 #endif
 
+/*!
+ *  \mainpage
+ *
+ *  The numakind library extends libnuma with the ability to
+ *  categorize groups of numa nodes into different "kinds" of
+ *  memory. It provides a low level interface for generating inputs to
+ *  mbind() and mmap(), and a high level interface for heap
+ *  management.  The heap management is implemented with an extension
+ *  to the jemalloc library which dedicates "arenas" to each CPU and
+ *  kind of memory.  To use numakind, jemalloc must be compiled with
+ *  the --enable-numakind option.
+
+ */
+
+
 typedef enum {
     NUMAKIND_DEFAULT = 0,
     NUMAKIND_HBW,
