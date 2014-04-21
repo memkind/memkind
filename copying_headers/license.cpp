d06f2d7124b2d202ff2b4ed50c6b9c0d4a69668e
Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>
2014-04-21 10:05:08 -0700

    fixing license headers for test source code

--- copying_headers/license.cpp
+++ copying_headers/license.cpp
@@ -7,21 +7,3 @@
  * with the terms of that agreement.
  *
  */
-
-#ifndef COMMON_H
-#define COMMON_H
-
-#include <iostream>
-#include <stdint.h>
-#include <stdlib.h>
-#include <gtest/gtest.h>
-#include "hbwmalloc.h"
-
-#define MB 1048576L
-#define GB 1073741824L
-#define KB 1024
-
-#define HBW_SUCCESS 0
-#define HBW_ERROR -1
-
-#endif
--- test/malloc_tests.cpp
+++ test/malloc_tests.cpp
@@ -1,32 +1,12 @@
-/**
- INTEL CONFIDENTIAL
-
- Copyright (C) 2007 by Intel Corporation.
- All Rights Reserved.
-
- The source code contained or described herein and all documents
- related to the source code ("Material") are owned by Intel Corporation
- or its suppliers or licensors. Title to the Material remains with Intel
- Corporation or its suppliers and licensors. The Material contains trade
- secrets and proprietary and confidential information of Intel or its
- suppliers and licensors. The Material is protected by worldwide copyright
- and trade secret laws and treaty provisions. No part of the Material may
- be used, copied, reproduced, modified, published, uploaded, posted,
- transmitted, distributed, or disclosed in any way without Intel's prior
- express written permission.
-
- No License under any patent, copyright, trade secret or other intellectual
- property right is granted to or conferred upon you by disclosure or
- delivery of the Materials, either expressly, by implication, inducement,
- estoppel or otherwise. Any license under such intellectual property rights
- must be express and approved by Intel in writing.
-
- Description:
- Helloworld for MYO shared programming model
- In this example, one global shared data(*buffer) is declared, and inited
- at myoiUserInit(). After runtime init(myoiLibInit), both host and LRB can
- have the buffer pointed to the same address.
-*/
+/*
+ * Copyright (2014) Intel Corporation All Rights Reserved.
+ *
+ * This software is supplied under the terms of a license
+ * agreement or nondisclosure agreement with Intel Corp.
+ * and may not be copied or disclosed except in accordance
+ * with the terms of that agreement.
+ *
+ */
 
 #include "common.h"
 #include "omp.h"
