7572248969d81cd10e90fe8e27a498b07b681507
Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>
2014-04-29 18:15:51 -0700

    enabling build.sh to work with knlsb rpmbuild output

--- test/build.sh
+++ test/build.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 
-jemalloc_prefix=`ls -dt $HOME/rpmbuild/BUILDROOT/jemalloc-*.x86_64/usr | head -n1`
-numakind_prefix=`ls -dt $HOME/rpmbuild/BUILDROOT/numakind-*.x86_64/usr | head -n1`
-make JEMALLOC_PREFIX=$jemalloc_prefix NUMAKIND_PREFIX=$numakind_prefix
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 <topdir>"
+    exit 1
+fi
 
+jemalloc_prefix=`ls -dt $1/BUILDROOT/jemalloc-*.x86_64/usr | head -n1`
+numakind_prefix=`ls -dt $1/BUILDROOT/numakind-*.x86_64/usr | head -n1`
+googletest_prefix=`ls -dt $1/BUILDROOT/googletest-*.x86_64/usr | head -n1`
+CPPFLAGS="-I $jemalloc_prefix/include -I $numakind_prefix/include -I $googletest_prefix/include"
+LDFLAGS="-L $jemalloc_prefix/lib64 -L $numakind_prefix/lib64 -L $googletest_prefix/lib64"
+make CPPFLAGS="$CPPFLAGS" LDFLAGS="$LDFLAGS"
