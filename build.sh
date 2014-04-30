75190279dd2f3da783868769665d832aee774b18
Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>
2014-04-29 18:18:59 -0700

    enabling build.sh to work with knlsb rpmbuild output

--- build.sh
+++ build.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 <topdir>"
+    exit 1
+fi
 
-jemalloc_prefix=`ls -dt $HOME/rpmbuild/BUILDROOT/jemalloc-*.x86_64/usr | head -n1`
-make -f make_rpm.mk JEMALLOC_PREFIX=$jemalloc_prefix $1
+jemalloc_prefix=`ls -dt $1/BUILDROOT/jemalloc-*.x86_64/usr | head -n1`
+
+CPPFLAGS="-I $jemalloc_prefix/include"
+LDFLAGS="-L $jemalloc_prefix/lib64"
+make -f make_rpm.mk CPPFLAGS="$CPPFLAGS" LDFLAGS="$LDFLAGS"
 
