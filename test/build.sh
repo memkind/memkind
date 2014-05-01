983e11009573ea8b65a653d42c520de991a3566d
Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>
2014-05-01 09:30:17 -0700

    got build.sh working for test directory, however requires prior call to 'make topdir=~/rpmbuild googletest_build' in the knlsb repo.

--- test/build.sh
+++ test/build.sh
@@ -1,13 +1,17 @@
 #!/bin/sh
-
-if [ $# -ne 1 ]; then
-    echo "Usage: $0 <topdir>"
-    exit 1
+topdir=$HOME/rpmbuild
+if [ $# -ne 0 ]; then
+    if [ $1 == --help ] || [ $1 == -h ]; then
+        echo "Usage: $0 [topdir]"
+        echo "    default topdir=$topdir"
+      exit 0
+    fi
+    topdir=$1
 fi
 
-jemalloc_prefix=`ls -dt $1/BUILDROOT/jemalloc-*.x86_64/usr | head -n1`
-numakind_prefix=`ls -dt $1/BUILDROOT/numakind-*.x86_64/usr | head -n1`
-googletest_prefix=`ls -dt $1/BUILDROOT/googletest-*.x86_64/usr | head -n1`
+jemalloc_prefix=`ls -t $topdir/BUILDROOT/jemalloc-*.x86_64/usr/lib64/libjemalloc.so | head -n1 | sed 's|/lib64/libjemalloc.so||'`
+numakind_prefix=`ls -t $topdir/BUILDROOT/numakind-*.x86_64/usr/lib64/libnumakind.so | head -n1 | sed 's|/lib64/libnumakind.so||'`
+googletest_prefix=`ls -t $topdir/BUILDROOT/googletest-*.x86_64/usr/lib64/libgtest.a | head -n1 | sed 's|/lib64/libgtest.a||'`
 CPPFLAGS="-I $jemalloc_prefix/include -I $numakind_prefix/include -I $googletest_prefix/include"
 LDFLAGS="-L $jemalloc_prefix/lib64 -L $numakind_prefix/lib64 -L $googletest_prefix/lib64"
-make CPPFLAGS="$CPPFLAGS" LDFLAGS="$LDFLAGS"
+make CPPFLAGS="$CPPFLAGS" LDFLAGS="$LDFLAGS" LD_LIBRARY_PATH="$jemalloc_prefix/lib64:$numakind_prefix/lib64:$LD_LIBRARY_PATH"
