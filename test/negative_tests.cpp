8c1b9798fd567973780af19e0ecfc5326316de19
Vishwanath Venkatesan <vishwanath.venkatesan@intel.com>
2014-06-13 09:20:58 -0700

    Negative tests without any need of mocks.

--- test/negative_tests.cpp
+++ test/negative_tests.cpp
@@ -16,6 +16,7 @@
 #include "check.h"
 #include "omp.h"
 #include "numakind.h"
+
 #include "trial_generator.h"
 
 
@@ -23,50 +24,11 @@ class NegativeTest: public :: testing::Test
 {
 
 protected:
-    size_t num_bandwidth;
-    int *bandwidth;
-    TrialGenerator *tgen;
     void SetUp()
-    {
-        size_t node;
-        char *hbw_nodes_env, *endptr;
-        tgen = new TrialGenerator();
-
-        hbw_nodes_env = getenv("NUMAKIND_HBW_NODES");
-        if (hbw_nodes_env) {
-            num_bandwidth = 128;
-            bandwidth = new int[num_bandwidth];
-            for (node = 0; node < num_bandwidth; node++) {
-                bandwidth[node] = 1;
-            }
-            node = strtol(hbw_nodes_env, &endptr, 10);
-            bandwidth[node] = 2;
-            while (*endptr == ':') {
-                hbw_nodes_env = endptr + 1;
-                node = strtol(hbw_nodes_env, &endptr, 10);
-                if (endptr != hbw_nodes_env && node >= 0 && node < num_bandwidth) {
-                    bandwidth[node] = 2;
-                }
-            }
-        } else {
-            const char *node_bandwidth_path = "/etc/numakind/node-bandwidth";
-            std::ifstream nbw_file;
-
-            nbw_file.open(node_bandwidth_path, std::ifstream::binary);
-            nbw_file.seekg(0, nbw_file.end);
-            num_bandwidth = nbw_file.tellg()/sizeof(int);
-            nbw_file.seekg(0, nbw_file.beg);
-            bandwidth = new int[num_bandwidth];
-            nbw_file.read((char *)bandwidth, num_bandwidth*sizeof(int));
-            nbw_file.close();
-        }
-    }
-
+    {}
+    
     void TearDown()
-    {
-        delete[] bandwidth;
-        delete tgen;
-    }
+    {}
 
 };
 
@@ -125,25 +87,19 @@ TEST_F(NegativeTest, ErrorAlignment)
     EXPECT_EQ(err, ret);
 }
 
-#if 0
+
 TEST_F(NegativeTest, ErrorAllocM)
 {
     int ret = 0;
     void *ptr = NULL;
     numakind_error_t err = NUMAKIND_ERROR_ALLOCM;
     
-    ptr = numakind_malloc(NUMAKIND_DEFAULT,
-			  100);
-     
     ret = numakind_posix_memalign(NUMAKIND_HBW,
 				  &ptr,
 				  16,
-				  100);
+				  100*GB);
     EXPECT_EQ(err, ret);
-				  
-    
 }
-#endif
 
 TEST_F(NegativeTest, InvalidSizeMalloc)
 {
@@ -183,4 +139,3 @@ TEST_F(NegativeTest, InvalidSizeRealloc){
     ASSERT_TRUE(ptr==NULL);
     EXPECT_EQ(errno, ENOMEM);
 }
-
