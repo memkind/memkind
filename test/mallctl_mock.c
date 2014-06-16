3555e0ad8650873b03b60bf0e7901b9310fc5eee
Vishwanath Venkatesan <vishwanath.venkatesan@intel.com>
2014-06-16 16:03:16 -0700

    Adding License information to the mock implementations

--- test/mallctl_mock.c
+++ test/mallctl_mock.c
@@ -1,3 +1,13 @@
+/*
+ * Copyright (2014) Intel Corporation All Rights Reserved.
+ *
+ * This software is supplied under the terms of a license
+ * agreement or nondisclosure agreement with Intel Corp.
+ * and may not be copied or disclosed except in accordance
+ * with the terms of that agreement.
+ *
+ */
+
 #include <stdio.h>
 
 int je_mallctl(const char *name, void *oldp, size_t *oldlenp, void *newp,
--- test/numadist_mock.c
+++ test/numadist_mock.c
@@ -1,3 +1,13 @@
+/*
+ * Copyright (2014) Intel Corporation All Rights Reserved.
+ *
+ * This software is supplied under the terms of a license
+ * agreement or nondisclosure agreement with Intel Corp.
+ * and may not be copied or disclosed except in accordance
+ * with the terms of that agreement.
+ *
+ */
+
 #include <limits.h>
 
 int numa_distance(int node1, int node2){
