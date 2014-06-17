cf3dbe0f8a53baddce2093f1ab4a67abfe14a82a
Vishwanath Venkatesan <vishwanath.venkatesan@intel.com>
2014-06-17 14:10:42 -0700

    missing license headers for trial_generator.h and trial_generator.cpp

--- test/trial_generator.cpp
+++ test/trial_generator.cpp
@@ -1,3 +1,12 @@
+/*
+ * Copyright (2014) Intel Corporation All Rights Reserved.
+ *
+ * This software is supplied under the terms of a license
+ * agreement or nondisclosure agreement with Intel Corp.
+ * and may not be copied or disclosed except in accordance
+ * with the terms of that agreement.
+ *
+ */
 #include "trial_generator.h"
 #include "check.h"
 
--- test/trial_generator.h
+++ test/trial_generator.h
@@ -1,3 +1,12 @@
+/*
+ * Copyright (2014) Intel Corporation All Rights Reserved.
+ *
+ * This software is supplied under the terms of a license
+ * agreement or nondisclosure agreement with Intel Corp.
+ * and may not be copied or disclosed except in accordance
+ * with the terms of that agreement.
+ *
+ */
 #ifndef execute_trials_include_h
 #define execute_trials_include_h
 
