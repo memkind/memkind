d48b5372e40e0d3ec79818c14e2dd538818c670d
Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>
2014-07-09 16:33:22 -0700

    fixed syntax errror

--- test/main.cpp
+++ test/main.cpp
@@ -15,7 +15,7 @@ int main(int argc, char **argv)
 {
     testing::InitGoogleTest(&argc, argv);
     if (hbw_is_available() == 0) {
-        std.cerr << "WARNING: hbw_is_available() == 0\n";
+        std::cerr << "WARNING: hbw_is_available() == 0\n";
     }
     return RUN_ALL_TESTS();
 }
