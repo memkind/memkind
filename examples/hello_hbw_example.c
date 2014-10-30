da49ab6d8405bca0e6dab04b4829de325ac0c29d
Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>
2014-10-30 10:30:54 -0700

    fixing possible memory leak in example code by adding error handling goto exit

--- examples/hello_hbw_example.c
+++ examples/hello_hbw_example.c
@@ -33,25 +33,28 @@ int main(int argc, char **argv)
     char *default_str = NULL;
     char *hbw_str = NULL;
     char *hbw_hugetlb_str = NULL;
-    int err;
+    int err = 0;
 
     default_str = (char *)malloc(size);
     if (default_str == NULL) {
         perror("je_malloc()");
         fprintf(stderr, "Unable to allocate default string\n");
-        return errno ? -errno : 1;
+        err = errno ? -errno : 1;
+        goto exit;
     }
     hbw_str = (char *)hbw_malloc(size);
     if (hbw_str == NULL) {
         perror("hbw_malloc()");
         fprintf(stderr, "Unable to allocate hbw string\n");
-        return errno ? -errno : 1;
+        err = errno ? -errno : 1;
+        goto exit;
     }
     err = hbw_posix_memalign_psize((void **)&hbw_hugetlb_str, 2097152, size, HBW_PAGESIZE_2MB);
     if (err) {
         perror("hbw_posix_memalign()");
         fprintf(stderr, "Unable to allocate hbw hugetlb string\n");
-        return errno ? -errno : 1;
+        err = errno ? -errno : 1;
+        goto exit;
     }
 
     sprintf(default_str, "Hello world from standard memory\n");
@@ -62,9 +65,15 @@ int main(int argc, char **argv)
     fprintf(stdout, "%s", hbw_str);
     fprintf(stdout, "%s", hbw_hugetlb_str);
 
-    hbw_free(hbw_hugetlb_str);
-    hbw_free(hbw_str);
-    free(default_str);
-
-    return 0;
+exit:
+    if (hbw_hugetlb_str) {
+        hbw_free(hbw_hugetlb_str);
+    }
+    if (hbw_str) {
+        hbw_free(hbw_str);
+    }
+    if (default_str) {
+        free(default_str);
+    }
+    return err;
 }
