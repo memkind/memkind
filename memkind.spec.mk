cf86205621b0d2010fa20293311fffd249ced65b
Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>
2014-11-13 16:20:51 -0800

    updated documentation for version 0.1.0 release

--- memkind.spec.mk
+++ memkind.spec.mk
@@ -179,6 +179,12 @@ fi
 $(extra_files)
 
 %changelog
+* Thu Nov 13 2014 Christopher Cantalupo <christopher.m.cantalupo@intel.com> v0.1.0
+- Increased test code coverage significantly.
+- Fixed bug in memkind_error_message() for MEMKIND_ERROR_TOOMANY.
+- Removed memkind_arena_free() API since it was redundant with memkind_default_free().
+- Static memkind structs are now declared as extern in the headers and defined in the
+  source files files rather than being statically defined in the headers.
 * Thu Oct 30 2014 Christopher Cantalupo <christopher.m.cantalupo@intel.com> v0.0.9
 - Now building with autotools.
 - Updated documentation.
