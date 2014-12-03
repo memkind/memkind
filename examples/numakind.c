f1bc02f86587a2d85249f6f6d5bdf56eb05da5a7
Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>
2014-12-03 09:42:16 -0800

    building libnumakind as an example

--- examples/numakind.c
+++ examples/numakind.c
@@ -1,14 +1,38 @@
 /*
+ * Copyright (C) 2014 Intel Corporation.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 1. Redistributions of source code must retain the above copyright notice(s),
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice(s),
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDER(S) ``AS IS'' AND ANY EXPRESS
+ * OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO
+ * EVENT SHALL THE COPYRIGHT HOLDER(S) BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+ * OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/*
  * In this example we try to create an optimal allocator for use on a
  * NUMA system with threading.  We assume that by the time a thread
  * makes an allocation call it is being run on the CPU that it will
  * remain on for the thread lifetime.  If this is not the case, and
- * the thread is migrated between allocation calls, there is no way 
- * to insure that the thread's data is local anyway.  
+ * the thread is migrated between allocation calls, there is no way
+ * to insure that the thread's data is local anyway.
  *
  * We will create one heap partition for each NUMA node to which the
  * partition will be bound.  There will be one arena per CPU, and the
- * index for the arena will be kept in thread local storage.  
+ * index for the arena will be kept in thread local storage.
  */
 
 #define _GNU_SOURCE
@@ -22,12 +46,12 @@
 #include <memkind_default.h>
 #include <memkind_arena.h>
 
-pthread_key_t numakind_key_g;
-pthread_once_t numakind_init_once_g = PTHREAD_ONCE_INIT;
-int numakind_init_err_g = 0;
-int numakind_zero_partition_g;
+static pthread_key_t numakind_key_g;
+static pthread_once_t numakind_init_once_g = PTHREAD_ONCE_INIT;
+static int numakind_init_err_g = 0;
+static int numakind_zero_partition_g;
 
-enum { NUMAKIND_MAX = 2048 };
+static enum { NUMAKIND_MAX = 2048 };
 
 #define NUMAKIND_GET_MBIND_NODEMASK_MACRO(NODE)                   \
 int get_mbind_nodemask_numa_##NODE(struct memkind *kind,          \
@@ -56,7 +80,67 @@ int get_mbind_nodemask_numa_##NODE(struct memkind *kind,          \
 
 #include "numakind_helper.h"
 
-void numakind_init(void)
+static void numakind_init(void);
+static int numakind_get_kind(memkind_t *kind);
+
+void *numakind_malloc(size_t size)
+{
+    int err = 0;
+    void *result = NULL;
+    memkind_t kind;
+
+    err = numakind_get_kind(&kind);
+    if (!err) {
+        result = (void *)memkind_malloc(kind, size);
+    }
+    return result;
+}
+
+void *numakind_calloc(size_t num, size_t size)
+{
+    int err = 0;
+    void *result = NULL;
+    memkind_t kind;
+
+    err = numakind_get_kind(&kind);
+    if (!err) {
+        result = (void *)memkind_calloc(kind, num, size);
+    }
+    return result;
+}
+
+int numakind_posix_memalign(void **memptr, size_t alignment, size_t size)
+{
+    int err = 0;
+    void *result = NULL;
+    memkind_t kind;
+
+    err = numakind_get_kind(&kind) ? EINVAL : 0;
+    if (!err) {
+        err = memkind_posix_memalign(kind, memptr, alignment, size);
+    }
+    return err;
+}
+
+void *numakind_realloc(void *ptr, size_t size)
+{
+    int err = 0;
+    void *result = NULL;
+    memkind_t kind;
+
+    err = numakind_get_kind(&kind);
+    if (!err) {
+        result = (void *)memkind_realloc(kind, ptr, size);
+    }
+    return result;
+}
+
+void numakind_free(void *ptr)
+{
+    memkind_default_free(MEMKIND_DEFAULT, ptr);
+}
+
+static void numakind_init(void)
 {
     int err = 0;
     char numakind_name[64];
@@ -69,7 +153,7 @@ void numakind_init(void)
     if (num_nodes > NUMAKIND_MAX) {
         err = MEMKIND_ERROR_TOOMANY;
     }
-    
+
     for (i = 0; !err && i < num_nodes; ++i) {
         name_length = snprintf(numakind_name, 63, "numakind_%.4d", i);
         if (name_length == 63) {
@@ -92,65 +176,32 @@ void numakind_init(void)
     }
 }
 
-
-void *numakind_malloc(size_t size)
+static int numakind_get_kind(memkind_t *kind)
 {
     int err = 0;
-    void *result = NULL;
-    memkind_t *numakind;
-    char numakind_name[64];
-    int name_length;
     unsigned int thread_numa_node;
 
     pthread_once(&numakind_init_once_g, numakind_init);
     if (numakind_init_err_g) {
-        return NULL;
+        err = numakind_init_err_g;
     }
 
-    numakind = pthread_getspecific(numakind_key_g);
-    if (numakind == NULL) {
-        numakind = (memkind_t *)je_malloc(sizeof(memkind_t));
-        if (numakind == NULL) {
-            err = MEMKIND_ERROR_MALLOC;
-        }
-        if (!err) {
-            thread_numa_node = numa_node_of_cpu(sched_getcpu());
-            err = memkind_get_kind_by_partition(numakind_zero_partition_g + thread_numa_node, numakind);
+    if (!err) {
+        kind = pthread_getspecific(numakind_key_g);
+        if (kind == NULL) {
+            kind = (memkind_t *)je_malloc(sizeof(memkind_t));
+            if (kind == NULL) {
+                err = MEMKIND_ERROR_MALLOC;
+            }
             if (!err) {
-                err = pthread_setspecific(numakind_key_g, numakind) ?
+                thread_numa_node = numa_node_of_cpu(sched_getcpu());
+                err = memkind_get_kind_by_partition(numakind_zero_partition_g + thread_numa_node, kind);
+            }
+            if (!err) {
+                err = pthread_setspecific(numakind_key_g, kind) ?
                       MEMKIND_ERROR_PTHREAD : 0;
             }
         }
     }
-    if (!err) {
-        result = (void *)memkind_malloc(*numakind, size);
-    }
-    return result;
-}
-
-void numakind_free(void *ptr)
-{
-    memkind_default_free(MEMKIND_DEFAULT, ptr);
-}
-
-
-int main(int argc, char **argv)
-{
-#pragma omp parallel
-{
-    char *data;
-    int status, pref;
-
-    data = numakind_malloc(1024);
-    if (!data) {
-        fprintf(stderr, "ERROR: %d\n", numakind_init_err_g);
-    }
-    else {
-        data[0] = '\0';
-        move_pages(0, 1, (void **)&data, NULL, &status, MPOL_MF_MOVE);
-        fprintf(stdout, "omp_thread: %.4d numa_node: %.4d\n", omp_get_thread_num(), status);
-    }
-    numakind_free(data);
-}
-    return numakind_init_err_g;
+    return err;
 }
