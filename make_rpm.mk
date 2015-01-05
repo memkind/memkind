1dd02f0a680653b0ac22629c4031e36e9c8eaeee
James Anchell <james.l.anchell@intel.com>
2015-01-05 12:05:22 -0800

    moving temporary tar ball creation outside of the repository and into topdir with unique name (time based)

--- make_rpm.mk
+++ make_rpm.mk
@@ -10,6 +10,7 @@ rpm = $(topdir)/RPMS/$(arch)/$(name)-devel-$(version)-$(release).$(arch).rpm
 srpm = $(topdir)/SRPMS/$(name)-$(version)-$(release).src.rpm
 specfile = $(topdir)/SPECS/$(name)-$(version).spec
 source_tar = $(topdir)/SOURCES/$(name)-$(version).tar.gz
+source_tmp_dir = $(topdir)/SOURCES/memkind-tmp-$(shell date +%s)
 
 rpmbuild_flags = -E '%define _topdir $(topdir)'
 rpmclean_flags = $(rpmbuild_flags) --clean --rmsource --rmspec
@@ -20,12 +21,12 @@ $(rpm): $(specfile) $(source_tar)
 	rpmbuild $(rpmbuild_flags) $(specfile) -ba
 
 $(source_tar): $(topdir)/.setup $(src) MANIFEST
-	mkdir -p source_tar_tmp
-	tar cf source_tar_tmp/tmp.tar -T MANIFEST --transform="s|^|$(name)-$(version)/|"
-	cd source_tar_tmp && tar xf tmp.tar
-	cd source_tar_tmp/$(name)-$(version) && ./autogen.sh && ./configure && make dist
-	mv source_tar_tmp/$(name)-$(version)/$(name)-$(version).tar.gz $@
-	rm -rf source_tar_tmp
+	mkdir -p $(source_tmp_dir)
+	tar cf $(source_tmp_dir)/tmp.tar -T MANIFEST --transform="s|^|$(name)-$(version)/|"
+	cd $(source_tmp_dir) && tar xf tmp.tar
+	cd $(source_tmp_dir)/$(name)-$(version) && ./autogen.sh && ./configure && make dist
+	mv $(source_tmp_dir)/$(name)-$(version)/$(name)-$(version).tar.gz $@
+	rm -rf $(source_tmp_dir)
 
 $(specfile): $(topdir)/.setup memkind.spec.mk
 	@echo "$$memkind_spec" > $@
