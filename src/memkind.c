f434b7d4dc3eeff650b74f7e2c5276da6066b446
Krzysztof Czurylo <krzysztof.czurylo@intel.com>
2015-03-02 14:56:20 +0100

    fix for incorrect partition number in memkind_arena_create
    Parition number passed to mallctl("arenas.extendk") is always 0.

--- src/memkind.c
+++ src/memkind.c
@@ -250,11 +250,11 @@ int memkind_create(const struct memkind_ops *ops, const char *name, struct memki
         goto exit;
     }
 
+    (*kind)->partition = memkind_registry_g.num_kind;
     err = ops->create(*kind, ops, name);
     if (err) {
         goto exit;
     }
-    (*kind)->partition = memkind_registry_g.num_kind;
     memkind_registry_g.partition_map[memkind_registry_g.num_kind] = *kind;
     ++memkind_registry_g.num_kind;
 
