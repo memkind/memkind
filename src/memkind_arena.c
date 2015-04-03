9e9dddc75bf8ce74df443a99e32b6cccf0377db9
Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>
2015-04-03 16:05:54 -0700

    fixed TLS implementation of memkind_thread_get_arena() to do proper error handling of null arena_map

--- src/memkind_arena.c
+++ src/memkind_arena.c
@@ -217,14 +217,18 @@ int memkind_bijective_get_arena(struct memkind *kind, unsigned int *arena)
 int memkind_thread_get_arena(struct memkind *kind, unsigned int *arena)
 {
     static __thread unsigned int MEMKIND_TLS_MODEL arena_tls = UINT_MAX;
+    int err = 0;
 
     if (arena_tls == UINT_MAX) {
-        if (kind->arena_map != NULL) {
-            arena_tls = _mm_crc32_u64(0, (uint64_t)pthread_self());
-        }
+        arena_tls = _mm_crc32_u64(0, (uint64_t)pthread_self());
     }
-    *arena = kind->arena_map[arena_tls % kind->arena_map_len];
-    return 0;
+    if (kind->arena_map != NULL) {
+        *arena = kind->arena_map[arena_tls % kind->arena_map_len];
+    }
+    else {
+        err = MEMKIND_ERROR_RUNTIME;
+    }
+    return err;
 }
 #else
 int memkind_thread_get_arena(struct memkind *kind, unsigned int *arena)
