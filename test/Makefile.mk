11733d6f953b41ca5a1b297929993e0b52b40b69
Leobardo R Rountree <leobardo.r.rountree@intel.com>
2015-05-19 16:07:57 -0500

    Creation of test rpm which will package test binaries
    Signed-off-by: Leobardo R Rountree <leobardo.r.rountree@intel.com>

--- test/Makefile.mk
+++ test/Makefile.mk
@@ -34,8 +34,10 @@ check_PROGRAMS += test/all_tests \
                   # end
 
 TESTS += test/check.sh
+
 EXTRA_DIST += test/mock-pmtt-2-nodes.hex \
-              test/mock-pmtt-empty-controller.hex
+              test/mock-pmtt-empty-controller.hex \
+              test/memkind-dt.ts
 
 test_all_tests_LDADD = libgtest.a libmemkind.la
 test_schedcpu_test_LDADD = libgtest.a libmemkind.la
