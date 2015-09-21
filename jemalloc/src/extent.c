94f92c68d9eebd09812eaf1b27e17c5ea609a639
Krzysztof Kułakowski <krzysztof.kulakowski@intel.com>
2015-09-21 12:44:37 +0200

    [jemalloc_integration] original commit: Author: Christopher M. Cantalupo <christopher.m.cantalupo@intel.com> Date:   Fri Dec 12 17:47:45 2014 -0800
        modifying comparison operators to order first by partition index, this enables the partitioning of the heap

--- jemalloc/src/extent.c
+++ jemalloc/src/extent.c
@@ -7,16 +7,27 @@ static inline int
 extent_szad_comp(extent_node_t *a, extent_node_t *b)
 {
 	int ret;
-	size_t a_size = a->size;
-	size_t b_size = b->size;
+#ifdef JEMALLOC_ENABLE_MEMKIND
+        unsigned a_partition = a->partition;
+        unsigned b_partition = b->partition;
 
-	ret = (a_size > b_size) - (a_size < b_size);
+        ret = (a_partition > b_partition) - (a_partition < b_partition);
+
 	if (ret == 0) {
-		uintptr_t a_addr = (uintptr_t)a->addr;
-		uintptr_t b_addr = (uintptr_t)b->addr;
+#endif
+		size_t a_size = a->size;
+		size_t b_size = b->size;
 
-		ret = (a_addr > b_addr) - (a_addr < b_addr);
+		ret = (a_size > b_size) - (a_size < b_size);
+		if (ret == 0) {
+			uintptr_t a_addr = (uintptr_t)a->addr;
+			uintptr_t b_addr = (uintptr_t)b->addr;
+
+			ret = (a_addr > b_addr) - (a_addr < b_addr);
+		}
+#ifdef JEMALLOC_ENABLE_MEMKIND
 	}
+#endif
 
 	return (ret);
 }
@@ -28,10 +39,24 @@ rb_gen(, extent_tree_szad_, extent_tree_t, extent_node_t, link_szad,
 static inline int
 extent_ad_comp(extent_node_t *a, extent_node_t *b)
 {
-	uintptr_t a_addr = (uintptr_t)a->addr;
-	uintptr_t b_addr = (uintptr_t)b->addr;
+	int ret;
+#ifdef JEMALLOC_ENABLE_MEMKIND
+        unsigned a_partition = a->partition;
+        unsigned b_partition = b->partition;
 
-	return ((a_addr > b_addr) - (a_addr < b_addr));
+        ret = (a_partition > b_partition) - (a_partition < b_partition);
+
+	if (ret == 0) {
+#endif
+		uintptr_t a_addr = (uintptr_t)a->addr;
+		uintptr_t b_addr = (uintptr_t)b->addr;
+
+		ret = (a_addr > b_addr) - (a_addr < b_addr);
+#ifdef JEMALLOC_ENABLE_MEMKIND
+	}
+#endif
+
+	return (ret);
 }
 
 /* Generate red-black tree functions. */
