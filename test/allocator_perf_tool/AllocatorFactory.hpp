d2e0d03c57a623b1d3af6e6acc7b5989f6de1f1f
akoziej <artur.koziej@intel.com>
2015-10-06 14:08:08 +0200

    Add Allocator Perf Tool to test memory workload execution time and memory footprint between different heap managers.

---
+++ test/allocator_perf_tool/AllocatorFactory.hpp
@@ -0,0 +1,113 @@
+/*
+* Copyright (C) 2015 Intel Corporation.
+* All rights reserved.
+*
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following conditions are met:
+* 1. Redistributions of source code must retain the above copyright notice(s),
+*    this list of conditions and the following disclaimer.
+* 2. Redistributions in binary form must reproduce the above copyright notice(s),
+*    this list of conditions and the following disclaimer in the documentation
+*    and/or other materials provided with the distribution.
+*
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDER(S) ``AS IS'' AND ANY EXPRESS
+* OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+* MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO
+* EVENT SHALL THE COPYRIGHT HOLDER(S) BE LIABLE FOR ANY DIRECT, INDIRECT,
+* INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+* LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+* PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+* LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+* OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+* ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+#pragma once
+
+#include "Allocator.hpp"
+#include "StandardAllocatorWithTimer.hpp"
+#include "VectorIterator.hpp"
+#include "Configuration.hpp"
+#include "JemallocAllocatorWithTimer.hpp"
+#include "MemkindAllocatorWithTimer.hpp"
+#include "memkind.h"
+
+#include <vector>
+#include <assert.h>
+#include <string.h>
+
+class AllocatorFactory
+{
+public:
+	AllocatorFactory() :
+		memkind_default(MEMKIND_DEFAULT, AllocatorTypes::MEMKIND_DEFAULT),
+		memkind_hbw(MEMKIND_HBW, AllocatorTypes::MEMKIND_HBW)
+	{}
+
+	//Get existing allocator without creating new.
+	//The owner of existing allocator is AllocatorFactory object.
+	Allocator* get_existing(unsigned type)
+	{
+		switch(type)
+		{
+			case AllocatorTypes::STANDARD_ALLOCATOR:
+				return &standard_allocator;
+
+			case AllocatorTypes::JEMALLOC:
+				return &jemalloc;
+
+			case AllocatorTypes::MEMKIND_DEFAULT:
+				return &memkind_default;
+
+			case AllocatorTypes::MEMKIND_HBW:
+				return &memkind_hbw;
+
+			default:
+				assert(!"'type' out of range!");
+		}
+	}
+
+	memory_operation initialize_allocator(unsigned type)
+	{
+		size_t initial_size = 512;
+		Allocator* allocator = get_existing(type);
+		memory_operation data = allocator->wrapped_malloc(initial_size);
+		memset(data.ptr, 0, initial_size);
+		allocator->wrapped_free(data.ptr);
+
+		return data;
+	}
+
+	VectorIterator<Allocator*> generate_random_allocator_calls(int num, int seed)
+	{
+		srand(seed);
+		std::vector<Allocator*> allocators_calls;
+
+		for (int i=0; i<num; i++)
+		{
+			int index = (rand() % AllocatorTypes::NUM_OF_ALLOCATOR_TYPES);
+
+			allocators_calls.push_back(get_existing(index));
+		}
+
+		return VectorIterator<Allocator*>::create(allocators_calls);
+	}
+
+	VectorIterator<Allocator*> generate_const_allocator_calls(unsigned allocator_type, int num, int seed)
+	{
+		srand(seed);
+		std::vector<Allocator*> allocators_calls;
+
+		for (int i=0; i<num; i++)
+		{
+			allocators_calls.push_back(get_existing(allocator_type));
+		}
+
+		return VectorIterator<Allocator*>::create(allocators_calls);
+	}
+
+private:
+	StandardAllocatorWithTimer standard_allocator;
+	JemallocAllocatorWithTimer jemalloc;
+	MemkindAllocatorWithTimer memkind_default;
+	MemkindAllocatorWithTimer memkind_hbw;
+};
---
+++ test/allocator_perf_tool/Configuration.hpp
@@ -0,0 +1,87 @@
+/*
+* Copyright (C) 2015 Intel Corporation.
+* All rights reserved.
+*
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following conditions are met:
+* 1. Redistributions of source code must retain the above copyright notice(s),
+*    this list of conditions and the following disclaimer.
+* 2. Redistributions in binary form must reproduce the above copyright notice(s),
+*    this list of conditions and the following disclaimer in the documentation
+*    and/or other materials provided with the distribution.
+*
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDER(S) ``AS IS'' AND ANY EXPRESS
+* OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+* MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO
+* EVENT SHALL THE COPYRIGHT HOLDER(S) BE LIABLE FOR ANY DIRECT, INDIRECT,
+* INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+* LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+* PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+* LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+* OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+* ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+#pragma once
+
+#include <string>
+#include <map>
+#include <assert.h>
+
+//AllocatorTypes class represent allocator types and names related to this types.
+class AllocatorTypes
+{
+public:
+	enum {STANDARD_ALLOCATOR, JEMALLOC, MEMKIND_DEFAULT, MEMKIND_HBW, NUM_OF_ALLOCATOR_TYPES};
+
+	static const std::string& allocator_name(unsigned type)
+	{
+		static const std::string names[] = {"STANDARD_ALLOCATOR", "JEMALLOC", "MEMKIND_DEFAULT", "MEMKIND_HBW"};
+
+		if((type < 0) || (type >= NUM_OF_ALLOCATOR_TYPES)) assert(!"Invalidate input argument!");
+
+		return names[type];
+	}
+};
+
+//Enable or disable enum values (types).
+class TypesConf
+{
+public:
+	void enable_type(unsigned type) {types[type] = true;}
+
+	void disable_type(unsigned type)
+	{
+		if(types.count(type))
+			types[type] = false;
+	}
+
+	bool is_enabled(unsigned type) {return (types.count(type) ? types[type] : false);}
+
+private:
+	std::map<unsigned, bool> types;
+};
+
+//AllocationSizesConf class represents allocation sizes configuration.
+//This data is needed to generate "n" sizes in range from "size_from" to "size_to".
+class AllocationSizesConf
+{
+public:
+	int n;
+	size_t size_from;
+	size_t size_to;
+};
+
+//TaskConf class contain configuration data for task,
+//where:
+// - "n" is number of iterations,
+// - "allocation_sizes_conf" - allocation sizes configuration,
+// - "func_calls" - enabled or disabled function calls,
+// - "seed" - random seed.
+class TaskConf
+{
+public:
+	int n;
+	AllocationSizesConf allocation_sizes_conf;
+	TypesConf func_calls;
+	unsigned seed;
+};
