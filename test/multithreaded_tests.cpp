d20c0df281c51caf8671ab0bf2cd39bb4cf6dbe8
Grzegorz Ozanski <grzegorz.ozanski@intel.com>
2015-10-09 19:41:57 +0200

    One common base class for all test cases utilizing TrialGenerator API

--- test/multithreaded_tests.cpp
+++ test/multithreaded_tests.cpp
@@ -37,56 +37,8 @@
  * malloc, calloc, memalign, realloc, with different memory sizes but this time
  * it will use Open MP threads with NTHREADS set to 2
  */
-class MultithreadedTest : public :: testing :: Test
+class MultithreadedTest : public TGTest
 {
-protected:
-    size_t num_bandwidth;
-    int *bandwidth;
-    TrialGenerator *tgen;
-
-    void SetUp()
-    {
-        size_t node;
-        char *hbw_nodes_env, *endptr;
-        tgen = new TrialGenerator();
-
-        hbw_nodes_env = getenv("MEMKIND_HBW_NODES");
-        if (hbw_nodes_env) {
-            num_bandwidth = 128;
-            bandwidth = new int[num_bandwidth];
-            for (node = 0; node < num_bandwidth; node++) {
-                bandwidth[node] = 1;
-            }
-            node = strtol(hbw_nodes_env, &endptr, 10);
-            bandwidth[node] = 2;
-            while (*endptr == ':') {
-                hbw_nodes_env = endptr + 1;
-                node = strtol(hbw_nodes_env, &endptr, 10);
-                if (endptr != hbw_nodes_env && node >= 0 && node < num_bandwidth) {
-                    bandwidth[node] = 2;
-                }
-            }
-        }
-        else {
-            const char *node_bandwidth_path = "/var/run/memkind/node-bandwidth";
-            std::ifstream nbw_file;
-
-            nbw_file.open(node_bandwidth_path, std::ifstream::binary);
-            nbw_file.seekg(0, nbw_file.end);
-            num_bandwidth = nbw_file.tellg()/sizeof(int);
-            nbw_file.seekg(0, nbw_file.beg);
-            bandwidth = new int[num_bandwidth];
-            nbw_file.read((char *)bandwidth, num_bandwidth*sizeof(int));
-            nbw_file.close();
-        }
-
-    }
-
-    void TearDown()
-    {
-        delete[] bandwidth;
-        delete tgen;
-    }
 };
 
 TEST_F(MultithreadedTest, TC_Memkind_Multithread_HBW_Malloc_2bytes_2KB_2MB_sizes)
--- test/trial_generator.cpp
+++ test/trial_generator.cpp
@@ -560,3 +560,45 @@ void TrialGenerator :: run(int num_bandwidth, int *bandwidth)
     }
 }
 
+void TGTest :: SetUp()
+{
+    size_t node;
+    char *hbw_nodes_env, *endptr;
+    tgen = new TrialGenerator();
+
+    hbw_nodes_env = getenv("MEMKIND_HBW_NODES");
+    if (hbw_nodes_env) {
+        num_bandwidth = 128;
+        bandwidth = new int[num_bandwidth];
+        for (node = 0; node < num_bandwidth; node++) {
+            bandwidth[node] = 1;
+        }
+        node = strtol(hbw_nodes_env, &endptr, 10);
+        bandwidth[node] = 2;
+        while (*endptr == ':') {
+            hbw_nodes_env = endptr + 1;
+            node = strtol(hbw_nodes_env, &endptr, 10);
+            if (endptr != hbw_nodes_env && node >= 0 && node < num_bandwidth) {
+                bandwidth[node] = 2;
+            }
+        }
+    }
+    else {
+        const char *node_bandwidth_path = "/var/run/memkind/node-bandwidth";
+        std::ifstream nbw_file;
+
+        nbw_file.open(node_bandwidth_path, std::ifstream::binary);
+        nbw_file.seekg(0, nbw_file.end);
+        num_bandwidth = nbw_file.tellg() / sizeof(int);
+        nbw_file.seekg(0, nbw_file.beg);
+        bandwidth = new int[num_bandwidth];
+        nbw_file.read((char *)bandwidth, num_bandwidth*sizeof(int));
+        nbw_file.close();
+    }
+}
+
+void TGTest :: TearDown()
+{
+    delete[] bandwidth;
+    delete tgen;
+}
