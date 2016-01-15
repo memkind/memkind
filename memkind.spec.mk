6f8f48db08d9ed43edf08ea5bbf8010b630ec725
akoziej <artur.koziej@intel.com>
2016-01-15 10:18:33 +0100

    Merge changes by <aquini@linux.com>: - Merging changes from RedHat spec file contributed by Rafael Aquini. and <christopher.m.cantalupo@intel.com>: - Moving to systemd defined service, dropping init.d script. - Added test/performance/.deps to .gitignore.
    Other changes:
    - Add memkind service start/stop.
    - No warnings after RPM installation.
    - memkind-devel has been split to mpsp-memkind and mpsp-memkind-devel packages
    - Add to spec obsoletes and provides for memkind-devel

--- memkind.spec.mk
+++ memkind.spec.mk
@@ -1,5 +1,5 @@
 #
-#  Copyright (C) 2014, 2015 Intel Corporation.
+#  Copyright (C) 2014, 2015, 2016 Intel Corporation.
 #  All rights reserved.
 #
 #  Redistribution and use in source and binary forms, with or without
@@ -26,9 +26,7 @@
 version ?= 0.0.0
 release ?= 1
 arch = $(shell uname -p)
-ifeq ($(jemalloc_installed),true)
-	rpmbuild_flags += -E '%define jemalloc_installed true'
-endif
+name ?= memkind
 
 rpm: $(name)-$(version).tar.gz
 	rpmbuild $(rpmbuild_flags) $^ -ta
@@ -44,13 +42,11 @@ Summary: User Extensible Heap Manager
 Name: $(name)
 Version: $(version)
 Release: $(release)
-License: See COPYING
+License: BSD-2-Clause
 Group: System Environment/Libraries
-Vendor: Intel Corporation
 URL: http://memkind.github.io/memkind
-Source0: memkind-%{version}.tar.gz
-BuildRoot: %{_tmppath}/%{name}-%{version}-%{release}-root
-BuildRequires:  gcc-c++
+BuildRoot: %{_tmppath}/%{name}-%{version}-%{release}-root-%(%{__id_u} -n)
+BuildRequires: automake libtool
 %if %{defined suse_version}
 BuildRequires: libnuma-devel
 %else
@@ -58,166 +54,170 @@ BuildRequires: numactl-devel
 %endif
 
 Prefix: %{_prefix}
-Prefix: %{_initddir}
+Prefix: %{_unitdir}
+Obsoletes: memkind
+Provides: memkind
+
+%define namespace memkind
 
 %if %{defined suse_version}
-%define docdir %{_defaultdocdir}/memkind
+%define docdir %{_defaultdocdir}/%{namespace}
 %else
-%define docdir %{_defaultdocdir}/memkind-%{version}
+%define docdir %{_defaultdocdir}/%{namespace}-%{version}
 %endif
 
-%define statedir %{_localstatedir}/run/memkind
+%define statedir %{_localstatedir}/run/%{namespace}
+
+
+# x86_64 is the only arch memkind will build due to its
+# current dependency on SSE4.2 CRC32 instruction which
+# is used to compute thread local storage arena mappings
+# with polynomial accumulations via GCC's intrinsic _mm_crc32_u64
+# For further info check:
+# - /lib/gcc/<target>/<version>/include/smmintrin.h
+# - https://gcc.gnu.org/bugzilla/show_bug.cgi?id=36095
+# - http://en.wikipedia.org/wiki/SSE4
+ExclusiveArch: x86_64
+
+# default values if version is a tagged release on github
+%{!?commit: %define commit %{version}}
+%{!?buildsubdir: %define buildsubdir %{namespace}-%{commit}}
+Source0: https://github.com/%{namespace}/%{namespace}/archive/v%{commit}/%{buildsubdir}.tar.gz
 
 %description
-The memkind library is a user extensible heap manager built on top of
+The memkind library is an user extensible heap manager built on top of
 jemalloc which enables control of memory characteristics and a
-partitioning of the heap between kinds of memory.  The kinds of memory
+partitioning of the heap between kinds of memory. The kinds of memory
 are defined by operating system memory policies that have been applied
-to virtual address ranges.  Memory characteristics supported by
+to virtual address ranges. Memory characteristics supported by
 memkind without user extension include control of NUMA and page size
-features.  The jemalloc non-standard interface has been extended to
+features. The jemalloc non-standard interface has been extended to
 enable specialized arenas to make requests for virtual memory from the
-operating system through the memkind partition interface.  Through the
+operating system through the memkind partition interface. Through the
 other memkind interfaces the user can control and extend memory
 partition features and allocate memory while selecting enabled
-features.
-
-%prep
-
-%setup
+features. This software is being made available for early evaluation.
+Feedback on design or implementation is greatly appreciated.
 
 %package devel
-Summary: Extention to libnuma for kinds of memory - development
+Summary: Memkind User Extensible Heap Manager development lib and tools
 Group: Development/Libraries
-$(opt_obsolete)
-$(opt_provides)
+Requires: %{name} = %{version}-%{release}
+Obsoletes: memkind-devel
+Provides: memkind-devel
 
 %description devel
-The memkind library is a user extensible heap manager built on top of
-jemalloc which enables control of memory characteristics and a
-partitioning of the heap between kinds of memory.  The kinds of memory
-are defined by operating system memory policies that have been applied
-to virtual address ranges.  Memory characteristics supported by
-memkind without user extension include control of NUMA and page size
-features.  The jemalloc non-standard interface has been extended to
-enable specialized arenas to make requests for virtual memory from the
-operating system through the memkind partition interface.  Through the
-other memkind interfaces the user can control and extend memory
-partition features and allocate memory while selecting enabled
-features.  The devel package installs header files.
+Install header files and development aids to link memkind library into
+applications.
 
 %package tests
 Summary: Extention to libnuma for kinds of memory - validation
 Group: Validation/Libraries
+Requires: %{name} = %{version}-%{release}
 
 %description tests
 memkind functional tests
 
-%build
-test -f configure || ./autogen.sh
+%prep
+%setup -q -a 0 -n $(name)-%{version}
 
-pushd jemalloc
+%build
+
+# It is required that we configure and build the jemalloc subdirectory
+# before we configure and start building the top level memkind directory.
+# To ensure the memkind build step is able to discover the output
+# of the jemalloc build we must create an 'obj' directory, and build
+# from within that directory.
+cd %{_builddir}/%{buildsubdir}/jemalloc/
+echo %{version} > %{_builddir}/%{buildsubdir}/jemalloc/VERSION
 test -f configure || %{__autoconf}
-%{__mkdir_p} obj
-pushd obj
+mkdir %{_builddir}/%{buildsubdir}/jemalloc/obj
+ln -s %{_builddir}/%{buildsubdir}/jemalloc/configure %{_builddir}/%{buildsubdir}/jemalloc/obj/
+cd %{_builddir}/%{buildsubdir}/jemalloc/obj
 ../configure --enable-autogen --with-jemalloc-prefix=jemk_ --enable-memkind \
              --enable-safe --enable-cc-silence \
              --prefix=%{_prefix} --includedir=%{_includedir} --libdir=%{_libdir} \
              --bindir=%{_bindir} --docdir=%{_docdir} --mandir=%{_mandir}
 $(make_prefix)%{__make} %{?_smp_mflags} $(make_postfix)
-popd
-popd
 
+# Build memkind lib and tools
+cd %{_builddir}/%{buildsubdir}
+echo %{version} > %{_builddir}/%{buildsubdir}/VERSION
+test -f configure || ./autogen.sh
 ./configure --prefix=%{_prefix} --libdir=%{_libdir} \
-    --includedir=%{_includedir} --sbindir=%{_sbindir} --enable-cxx11 \
-    --mandir=%{_mandir} --docdir=%{docdir}
+           --includedir=%{_includedir} --sbindir=%{_sbindir} --enable-cxx11 \
+           --mandir=%{_mandir} --docdir=%{_docdir}/%{namespace}
 $(make_prefix)%{__make} %{?_smp_mflags} libgtest.a $(make_postfix)
 $(make_prefix)%{__make} %{?_smp_mflags} checkprogs $(make_postfix)
 
+
 %install
+cd %{_builddir}/%{buildsubdir}
 %{__make} DESTDIR=%{buildroot} install
-%{__install} -d %{buildroot}/%{_initddir}
 %{__install} -d %{buildroot}$(memkind_test_dir)
-%{__install} init.d/memkind %{buildroot}/%{_initddir}/memkind
+%{__install} -d %{buildroot}/%{_unitdir}
+%{__install} %{namespace}.service %{buildroot}/%{_unitdir}/%{namespace}.service
+%if 0%{?suse_version} <= 1310
 %{__install} -d %{buildroot}/%{statedir}
 touch %{buildroot}/%{statedir}/node-bandwidth
+%endif
 %{__install} test/.libs/* test/*.sh test/*.hex test/*.ts test/memkind_ft.py test/*.so %{buildroot}$(memkind_test_dir)
-rm -f %{buildroot}/%{_libdir}/libmemkind.a
-rm -f %{buildroot}/%{_libdir}/libmemkind.la
-rm -f %{buildroot}/%{_libdir}/libnumakind.*
-rm -f %{buildroot}/%{_libdir}/liballocatorperftool.*
-rm -f %{buildroot}/%{_libdir}/libautohbw.*
 rm -f %{buildroot}$(memkind_test_dir)/libautohbw.*
-
-
+rm -f %{buildroot}/%{_libdir}/lib%{namespace}.{l,}a
+rm -f %{buildroot}/%{_libdir}/lib{numakind,autohbw}.*
 $(extra_install)
 
-%clean
+%pre
+%if %{defined suse_version}
+%service_add_pre %{namespace}.service
+%endif
 
-%post devel
+%post
 /sbin/ldconfig
-if [ -x /usr/lib/lsb/install_initd ]; then
-    /usr/lib/lsb/install_initd %{_initddir}/memkind
-elif [ -x /sbin/chkconfig ]; then
-    /sbin/chkconfig --add memkind
-else
-    for i in 3 4 5; do
-        ln -sf %{_initddir}/memkind /etc/rc.d/rc${i}.d/S90memkind
-    done
-    for i in 0 1 2 6; do
-        ln -sf %{_initddir}/memkind /etc/rc.d/rc${i}.d/K10memkind
-    done
-fi
-%{_initddir}/memkind restart >/dev/null 2>&1 || :
+systemctl enable  %{namespace}.service >/dev/null 2>&1
+systemctl start  %{namespace}.service >/dev/null 2>&1
+%if %{defined suse_version}
+%{service_add_post %{namespace}.service}
+%endif
 
-%preun devel
-if [ -z "$1" ] || [ "$1" == 0 ]; then
-    %{_initddir}/memkind stop >/dev/null 2>&1 || :
-    if [ -x /usr/lib/lsb/remove_initd ]; then
-        /usr/lib/lsb/remove_initd %{_initddir}/memkind
-    elif [ -x /sbin/chkconfig ]; then
-        /sbin/chkconfig --del memkind
-    else
-        rm -f /etc/rc.d/rc?.d/???memkind
-    fi
-fi
+%preun
+systemctl stop  %{namespace}.service >/dev/null 2>&1
+systemctl disable  %{namespace}.service >/dev/null 2>&1
+%if %{defined suse_version}
+%{service_del_preun %{namespace}.service}
+%endif
 
-%postun devel
+%postun
+%if %{defined suse_version}
+%{service_del_postun %{namespace}.service}
+%endif
 /sbin/ldconfig
 
+%files
+%defattr(-,root,root,-)
+%license %{_docdir}/%{namespace}/COPYING
+%doc %{_docdir}/%{namespace}/README
+%doc %{_docdir}/%{namespace}/VERSION
+%dir %{_docdir}/%{namespace}
+%dir %{_unitdir}
+%{_libdir}/lib%{namespace}.so.*
+%{_bindir}/%{namespace}-hbw-nodes
+%{_sbindir}/%{namespace}-pmtt
+%{_unitdir}/%{namespace}.service
+%if 0%{?suse_version} <= 1310
+%ghost %dir %{statedir}
+%ghost %{statedir}/node-bandwidth
+%endif
 %files devel
 %defattr(-,root,root,-)
-%{_includedir}/memkind.h
+%{_includedir}/%{namespace}*.h
 %{_includedir}/hbwmalloc.h
-%{_includedir}/memkind_arena.h
-%{_includedir}/memkind_default.h
-%{_includedir}/memkind_gbtlb.h
-%{_includedir}/memkind_hbw.h
-%{_includedir}/memkind_hugetlb.h
-%{_includedir}/memkind_pmem.h
 %{_includedir}/hbw_allocator.h
-%{_libdir}/libmemkind.so.0.0.1
-%{_libdir}/libmemkind.so.0
-%{_libdir}/libmemkind.so
-%{_bindir}/memkind-hbw-nodes
-%{_sbindir}/memkind-pmtt
-%{_initddir}/memkind
-%{statedir}/node-bandwidth
-%dir %{statedir}
-%dir %{docdir}
-%doc %{docdir}/README
-%doc %{docdir}/COPYING
-%doc %{docdir}/VERSION
-%doc %{_mandir}/man3/hbwmalloc.3.gz
-%doc %{_mandir}/man3/hbwallocator.3.gz
-%doc %{_mandir}/man3/memkind.3.gz
-%doc %{_mandir}/man3/memkind_arena.3.gz
-%doc %{_mandir}/man3/memkind_default.3.gz
-%doc %{_mandir}/man3/memkind_gbtlb.3.gz
-%doc %{_mandir}/man3/memkind_hbw.3.gz
-%doc %{_mandir}/man3/memkind_hugetlb.3.gz
-%doc %{_mandir}/man3/memkind_pmem.3.gz
-$(extra_files)
+%{_libdir}/lib%{namespace}.so
+%{_mandir}/man3/hbwmalloc.3.*
+%{_mandir}/man3/hbwallocator.3.*
+%{_mandir}/man3/%{namespace}*.3.*
+
 
 %files tests
 %defattr(-,root,root,-)
@@ -260,7 +260,6 @@ $(memkind_test_dir)/libsched.so
 
 %exclude $(memkind_test_dir)/*.pyo
 %exclude $(memkind_test_dir)/*.pyc
-%exclude $(memkind_test_dir)/liballocatorperftool.*
 
 %changelog
 endef
