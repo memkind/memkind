018c837681dd70e8ddaf3a235bf26f6573e22436
Krzysztof Kułakowski <krzysztof.kulakowski@intel.com>
2016-03-01 17:45:06 +0100

    Build instruction cleanup:   [] added build_jemalloc.sh script that is able to perform default compilation of jemalloc   [] added warning to autogen.sh (in case of absence of jemalloc/obj subdir)   [] all detailed instructions are moved to CONTRIBUTING document

--- build_jemalloc.sh
+++ build_jemalloc.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-#  Copyright (C) 2014-2016 Intel Corporation.
+#  Copyright (C) 2016 Intel Corporation.
 #  All rights reserved.
 #
 #  Redistribution and use in source and binary forms, with or without
@@ -21,38 +21,11 @@
 #  LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
 #  OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
 #  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-#
 
-# If the VERSION file does not exist, then create it based on git
-# describe or if not in a git repo just set VERSION to 0.0.0.
-if [ ! -f VERSION ]; then
-    if [ -f .git/config ]; then
-        sha=$(git describe --long | awk -F- '{print $(NF)}')
-	release=$(git describe --long | awk -F- '{print $(NF-1)}')
-	version=$(git describe --long | sed -e "s|\(.*\)-$release-$sha|\1|" -e "s|-|+|g" -e "s|^v||")
-	if [ ${release} != "0" ]; then
-	    version=${version}+dev${release}${sha}
-	fi
-    else
-        echo "WARNING:  VERSION file does not exist and working directory is not a git repository, setting verison to 0.0.0" 2>&1
-        version=0.0.0
-    fi
-    echo $version > VERSION
-fi
-
-pushd jemalloc
-autoconf
-popd
-
-mkdir -p m4
-autoreconf -i -f
-
-if [ ! -e jemalloc/obj ]; then
-    echo
-    echo "WARNING: library build process assume existance of compiled jemalloc objects located in jemalloc/obj subdir. You can simply run:"
-    echo
-    echo "   ./build_jemalloc.sh"
-    echo
-    echo "or see CONTRIBUTING document for more information about configuring and building included version of jemalloc."
-    echo
-fi
+cd jemalloc
+test -e configure || autoconf
+test -e obj || mkdir obj
+cd obj
+../configure --enable-autogen --enable-memkind \
+             --with-jemalloc-prefix=jemk_ --enable-cc-silence
+make
