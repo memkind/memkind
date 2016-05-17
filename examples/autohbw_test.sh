ef3fe16dde41279e75db047f07e53ae35d17909e
Agata Wozniak <agata.wozniak@intel.com>
2016-05-17 13:31:23 +0200

    Update copyright headers year
    Change-Id: I8eb0fb6ca7e53ba3ea24e415bffb917a295045dd

--- examples/autohbw_test.sh
+++ examples/autohbw_test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#  Copyright (C) 2015 Intel Corporation.
+#  Copyright (C) 2015 - 2016 Intel Corporation.
 #  All rights reserved.
 #
 #  Redistribution and use in source and binary forms, with or without
