02fa69ef961b08ea9314c11a003a88a39b9e0c34
Egor Promyshlennikov <egor.promyshlennikov@intel.com>
2018-07-31 16:17:10 +0300

    Introduce C++ allocator for PMEM
    We have created STL-like C++ allocator for PMEM kind.
    This allocator could be used with any STL-like data structures.
    We created set of test and examples for STL data structures.
    Also, we tested this allocator privately with Intel TBB data structures.
    
    Fix issues with pmem allocator and basic_string
    
    On systems with old (non CXX11 ABI) C++ runtimes pmem allocator cannot
    be compiled with std::basic_string.
    We disabled tests and examples with std::basic_string for the systems
    with old C++ runtime. Furthermore, we add static assert to
    pmem::allocator implementation to check C++ ABI at compile time.

--- examples/Makefile.mk
+++ examples/Makefile.mk
@@ -31,6 +31,7 @@ noinst_PROGRAMS += examples/hello_memkind \
                    # end
 if HAVE_CXX11
 noinst_PROGRAMS += examples/memkind_allocated
+noinst_PROGRAMS += examples/pmem_cpp_allocator
 endif
 
 examples_hello_memkind_LDADD = libmemkind.la
@@ -42,6 +43,7 @@ examples_autohbw_candidates_LDADD = libmemkind.la
 
 if HAVE_CXX11
 examples_memkind_allocated_LDADD = libmemkind.la
+examples_pmem_cpp_allocator_LDADD = libmemkind.la
 endif
 
 examples_hello_memkind_SOURCES = examples/hello_memkind_example.c
@@ -52,6 +54,7 @@ examples_pmem_memkind_SOURCES = examples/pmem_example.c
 examples_autohbw_candidates_SOURCES = examples/autohbw_candidates.c
 if HAVE_CXX11
 examples_memkind_allocated_SOURCES = examples/memkind_allocated_example.cpp examples/memkind_allocated.hpp
+examples_pmem_cpp_allocator_SOURCES = examples/pmem_cpp_allocator.cpp
 endif
 
 clean-local:
