9d74b4cfa055a65e1e4ce34e16a2430426bd0103
Sergei Vinogradov <sergey.vinogradov@intel.com>
2018-09-13 13:04:44 +0300

    Address review comments
    Egor (who did initial raises code review) left Intel. So, I am addresing
    review comments.

--- include/pmem_allocator.h
+++ include/pmem_allocator.h
@@ -28,56 +28,79 @@
 #include <string>
 #include <exception>
 #include <type_traits>
-#include <cstddef>
-#include <utility>
 #include <atomic>
+#include <cstddef>
 
 #include "memkind.h"
 
+/*
+ * Header file for the C++ allocator compatible with the C++ standard library allocator concepts.
+ * More details in pmemallocator(3) man page.
+ * Note: memory heap management is based on memkind_malloc, refer to the memkind(3) man page for more
+ * information.
+ *
+ * Functionality defined in this header is considered as EXPERIMENTAL API.
+ * API standards are described in memkind(3) man page.
+ */
 namespace pmem
 {
+    namespace internal
+    {
+        class kind_wrapper_t
+        {
+        public:
+            kind_wrapper_t(const char *dir, std::size_t max_size)
+            {
+                int err_c  = memkind_create_pmem(dir, max_size, &kind);
+                if (err_c) {
+                    throw std::runtime_error(
+                        std::string("An error occured while creating pmem kind; error code: ") +
+                        std::to_string(err_c));
+                }
+            }
+
+            kind_wrapper_t(const kind_wrapper_t&) = delete;
+            void operator=(const kind_wrapper_t&) = delete;
+
+            ~kind_wrapper_t()
+            {
+                memkind_destroy_kind(kind);
+            }
+
+            memkind_t get() const
+            {
+                return kind;
+            }
+
+        private:
+            memkind_t kind;
+        };
+    }
+
     template<typename T>
-    struct allocator {
-        typedef T value_type;
-        typedef value_type* pointer;
-        typedef const value_type* const_pointer;
-        typedef value_type& reference;
-        typedef const value_type& const_reference;
-        typedef size_t size_type;
-        typedef ptrdiff_t difference_type;
+    class allocator
+    {
+        using kind_wrapper_t = internal::kind_wrapper_t;
+        std::shared_ptr<kind_wrapper_t> kind_wrapper_ptr;
+    public:
+        using value_type = T;
+        using pointer = value_type*;
+        using const_pointer = const value_type*;
+        using reference = value_type&;
+        using const_reference = const value_type&;
+        using size_type = size_t;
+        using difference_type = ptrdiff_t;
 
         template<class U>
         struct rebind {
-            typedef allocator<U> other;
+            using other = allocator<U>;
         };
 
         template<typename U>
-        friend struct allocator;
+        friend class allocator;
 
-    private:
-        memkind_t kind_ptr;
-        std::atomic<size_t> * kind_cnt;
-
-        void clean_up()
-        {
-            if (kind_cnt->fetch_sub(1) == 1) {
-
-                memkind_destroy_kind(kind_ptr);
-                delete kind_cnt;
-            }
-        }
-
-        template <typename U>
-        inline void assign(U&&  other)
-        {
-            kind_ptr = other.kind_ptr;
-            kind_cnt = other.kind_cnt;
-            ++(*kind_cnt);
-        }
-
-    public:
 #ifndef _GLIBCXX_USE_CXX11_ABI
-        /* This is a workaround for compilers that uses C++11 standard,
+        /* This is a workaround for compilers (e.g GCC 4.8) that uses C++11 standard,
          * but use old - non C++11 ABI */
         template<typename V = void>
         explicit allocator()
@@ -87,107 +110,93 @@ namespace pmem
         }
 #endif
 
-        explicit allocator(const char *dir, size_t max_size)
+        explicit allocator(const char *dir,
+                           size_t max_size) : kind_wrapper_ptr(std::make_shared<kind_wrapper_t>(dir,
+                                                                   max_size))
         {
-            int err_c  = memkind_create_pmem(dir, max_size, &kind_ptr);
-            if (err_c) {
-                throw std::runtime_error(
-                    std::string("An error occured while creating pmem kind; error code: ") +
-                    std::to_string(err_c));
-            }
-            kind_cnt = new std::atomic<size_t>(1);
         }
 
         explicit allocator(const std::string& dir,
                            size_t max_size) : allocator(dir.c_str(), max_size)
         {
-        };
-
-        allocator(const allocator& other) noexcept
-        {
-            assign(other);
         }
+
+        allocator(const allocator& other) = default;
 
         template <typename U>
-        allocator(const allocator<U>& other) noexcept
+        allocator(const allocator<U>& other) noexcept : kind_wrapper_ptr(
+                other.kind_wrapper_ptr)
         {
-            assign(other);
         }
 
-        allocator(const allocator&& other) noexcept
-        {
-            assign(other);
-        }
+        allocator(allocator&& other) = default;
 
         template <typename U>
-        allocator(const allocator<U>&& other) noexcept
+        allocator(allocator<U>&& other) noexcept : kind_wrapper_ptr(std::move(
+                                                                            other.kind_wrapper_ptr))
         {
-            assign(other);
         }
 
-        void operator = (const allocator& other) noexcept
-        {
-            clean_up();
-            assign(other);
-        }
+        allocator<T>& operator = (const allocator& other) = default;
 
         template <typename U>
-        void operator = (const allocator<U>& other) noexcept
+        allocator<T>& operator = (const allocator<U>& other) noexcept
         {
-            clean_up();
-            assign(other);
+            kind_wrapper_ptr = other.kind_wrapper_ptr;
+            return *this;
         }
 
-        void operator = (const allocator&& other) noexcept
-        {
-            clean_up();
-            assign(other);
-        }
+        allocator<T>& operator = (allocator&& other) = default;
 
         template <typename U>
-        void operator = (const allocator<U>&& other) noexcept
+        allocator<T>& operator = (allocator<U>&& other) noexcept
         {
-            clean_up();
-            assign(other);
+            kind_wrapper_ptr = std::move(other.kind_wrapper_ptr);
+            return *this;
         }
 
-        template <typename U>
-        bool operator ==(const allocator<U>& other) const
+        pointer allocate(size_type n) const
         {
-            return kind_ptr == other.kind_ptr;
+            pointer result = static_cast<pointer>(memkind_malloc(kind_wrapper_ptr->get(),
+                                                                 n*sizeof(T)));
+            if (!result) {
+                throw std::bad_alloc();
+            }
+            return result;
         }
 
-        template <typename U>
-        bool operator !=(const allocator<U>& other) const
+        void deallocate(pointer p, size_type n) const
         {
-            return !(*this ==other);
-        }
-
-        T* allocate(std::size_t n) const
-        {
-            void* mptr = memkind_calloc(kind_ptr, n, sizeof(T));
-            return static_cast<T*>(mptr);
-        }
-
-        void deallocate(T* const p, std::size_t n) const
-        {
-            memkind_free(kind_ptr, static_cast<void*>(p));
+            memkind_free(kind_wrapper_ptr->get(), static_cast<void*>(p));
         }
 
         template <class U, class... Args>
-        void construct(U* p, Args... args) const
+        void construct(U* p, Args&&... args) const
         {
             ::new((void *)p) U(std::forward<Args>(args)...);
         }
 
-        void destroy(T* const p) const
+        void destroy(pointer p) const
         {
             p->~value_type();
         }
 
-        ~allocator()
-        {
-            clean_up();
-        }
+        template <typename U, typename V>
+        friend bool operator ==(const allocator<U>& lhs, const allocator<V>& rhs);
+
+        template <typename U, typename V>
+        friend bool operator !=(const allocator<U>& lhs, const allocator<V>& rhs);
     };
+
+    template <typename U, typename V>
+    bool operator ==(const allocator<U>& lhs, const allocator<V>& rhs)
+    {
+        return lhs.kind_wrapper_ptr->get() == rhs.kind_wrapper_ptr->get();
+    }
+
+    template <typename U, typename V>
+    bool operator !=(const allocator<U>& lhs, const allocator<V>& rhs)
+    {
+        return !(lhs == rhs);
+    }
 }
