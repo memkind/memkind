f70da9e574182da3a3d33e7e9edad950621def32
Sergei Vinogradov <sergey.vinogradov@intel.com>
2018-09-13 13:04:44 +0300

    Address review comments
    Egor (who did initial raises code review) left Intel. So, I am addresing
    review comments.

--- memkind.spec.mk
+++ memkind.spec.mk
@@ -1,5 +1,5 @@
 #
-#  Copyright (C) 2014 - 2017 Intel Corporation.
+#  Copyright (C) 2014 - 2018 Intel Corporation.
 #  All rights reserved.
 #
 #  Redistribution and use in source and binary forms, with or without
@@ -169,6 +169,7 @@ rm -f %{buildroot}/%{_libdir}/libautohbw.{l,}a
 %{_includedir}
 %{_includedir}/hbwmalloc.h
 %{_includedir}/hbw_allocator.h
+%{_includedir}/pmem_allocator.h
 %{_libdir}/lib%{namespace}.so
 %{_libdir}/libautohbw.so
 %{_includedir}/%{namespace}.h
@@ -176,6 +177,7 @@ rm -f %{buildroot}/%{_libdir}/libautohbw.{l,}a
 %{_includedir}/%{internal_include}/%{namespace}*.h
 %{_mandir}/man3/hbwmalloc.3.*
 %{_mandir}/man3/hbwallocator.3.*
+%{_mandir}/man3/pmemallocator.3.*
 %{_mandir}/man3/%{namespace}*.3.*
 
 %exclude %{_includedir}/%{internal_include}/%{namespace}_log.h
