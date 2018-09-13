689e05726f369013e9b9e2f83c2ade4f36cbae0e
Adrian Bonislawski <adrianx.bonislawski@intel.com>
2018-09-13 15:00:17 +0200

    Memkind PMEM multithreads stress kinds create test
    This test will start simultaneously a large number of threads which will try to create a pmem kind in a situation where only a few slots for pmem kinds are available.
    
    Signed-off-by: Adrian Bonislawski <adrianx.bonislawski@intel.com>

--- test/memkind_pmem_tests.cpp
+++ test/memkind_pmem_tests.cpp
@@ -28,11 +28,14 @@
 #include <sys/param.h>
 #include <sys/mman.h>
 #include <stdio.h>
+#include <pthread.h>
 #include "common.h"
 
 static const size_t PMEM_PART_SIZE = MEMKIND_PMEM_MIN_SIZE + 4096;
 static const size_t PMEM_NO_LIMIT = 0;
 extern const char*  PMEM_DIR;
+static pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+static pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
 
 class MemkindPmemTests: public :: testing::Test
 {
@@ -195,7 +198,7 @@ TEST_F(MemkindPmemTests, test_TC_MEMKIND_PmemMallocUsableSize)
     EXPECT_TRUE(nullptr != pmem_temp);
     size_t usable_size = memkind_malloc_usable_size(pmem_temp, nullptr);
     EXPECT_EQ(0u, usable_size);
-    for (unsigned int i = 0 ; i < (sizeof(check_sizes) / sizeof(check_sizes[0]));
+    for (unsigned int i = 0; i < (sizeof(check_sizes) / sizeof(check_sizes[0]));
          ++i) {
         size_t size = check_sizes[i].size;
         void *alloc = memkind_malloc(pmem_temp, size);
@@ -385,12 +388,12 @@ TEST_F(MemkindPmemTests,
 {
     const size_t pmem_number = 25000;
     struct memkind *pmem_temp[pmem_number] = { nullptr };
-    unsigned i, j = 0;
+    unsigned i = 0, j = 0;
     int err = 0;
 
     for (i = 0; i < pmem_number; ++i) {
         err = memkind_create_pmem(PMEM_DIR, MEMKIND_PMEM_MIN_SIZE, &pmem_temp[i]);
-        if ( err ) {
+        if (err) {
             EXPECT_EQ(err, MEMKIND_ERROR_ARENAS_CREATE);
             break;
         }
@@ -399,4 +402,75 @@ TEST_F(MemkindPmemTests,
         err = memkind_destroy_kind(pmem_temp[j]);
         EXPECT_EQ(err, 0);
     }
+}
+
+static void* thread_func_kinds(void* arg)
+{
+    memkind_t pmem_thread_kind;
+    int err = 0;
+
+    pthread_mutex_lock(&mutex);
+    pthread_cond_wait(&cond, &mutex);
+    pthread_mutex_unlock(&mutex);
+
+    err = memkind_create_pmem(PMEM_DIR, PMEM_PART_SIZE, &pmem_thread_kind);
+
+    if(err == 0) {
+        void *test = memkind_malloc(pmem_thread_kind, 32);
+        EXPECT_TRUE(test != nullptr);
+
+        memkind_free(pmem_thread_kind, test);
+        err = memkind_destroy_kind(pmem_thread_kind);
+        EXPECT_EQ(0, err);
+    }
+
+    return nullptr;
+}
+
+TEST_F(MemkindPmemTests, test_TC_MEMKIND_PmemMultithreadsStressKindsCreate)
+{
+    const int nthreads = 50;
+    int i, t, err;
+    int max_possible_kind = 0;
+    memkind_t pmem_kinds[MEMKIND_MAX_KIND] = {nullptr};
+    pthread_t *threads = (pthread_t*)calloc(nthreads, sizeof(pthread_t));
+    ASSERT_TRUE(threads != nullptr);
+
+    // This loop will create as many kinds as possible
+    // to obtain a real kind limit
+    for (i = 0; i < MEMKIND_MAX_KIND; i++) {
+        err = memkind_create_pmem(PMEM_DIR, PMEM_PART_SIZE, &pmem_kinds[i]);
+        if(err != 0) {
+            ASSERT_TRUE(i > 0);
+            max_possible_kind = i;
+            --i;
+            break;
+        }
+        ASSERT_TRUE(nullptr != pmem_kinds[i]);
+    }
+
+    // destroy last kind so it will be possible
+    // to create only one pmem kind in threads
+    err = memkind_destroy_kind(pmem_kinds[i]);
+    ASSERT_EQ(0, err);
+
+    for (t = 0; t < nthreads; t++) {
+        err = pthread_create(&threads[t], nullptr, thread_func_kinds, nullptr);
+        ASSERT_EQ(0, err);
+    }
+
+    sleep(1);
+    pthread_cond_broadcast(&cond);
+
+    for (t = 0; t < nthreads; t++) {
+        err = pthread_join(threads[t], nullptr);
+        ASSERT_EQ(0, err);
+    }
+
+    for (i = 0; i < max_possible_kind - 1; i++) {
+        err = memkind_destroy_kind(pmem_kinds[i]);
+        ASSERT_EQ(0, err);
+    }
+
+    free(threads);
 }
