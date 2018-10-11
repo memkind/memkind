4632d019b58ce6792da3874bc0dc547ebc004b49
michalbiesek <michal.biesek@intel.com>
2018-10-11 15:55:35 +0200

    Update description regarding memkind_destroy_kind

--- include/memkind.h
+++ include/memkind.h
@@ -170,12 +170,13 @@ int memkind_create_kind(memkind_memtype_t memtype_flags,
                         memkind_t* kind);
 
 ///
-/// \brief Destroy the kind object. The kind object needs to be initialized by
-///        memkind_create_kind() or memkind_create_pmem() before it is destroyed.
-///        The function has undefined behavior when the handle is invalid.
+/// \brief Destroy previously created kind object, which must have been returned
+///        by a previous call to memkind_create_kind() or memkind_create_pmem().
+///        The function has undefined behavior when the handle is invalid or
+///        memkind_destroy_kind(kind) was already called before
 /// \warning EXPERIMENTAL API
-/// \note all allocated memory must be freed before kind is destroyed, otherwise
-///       this will cause memory leak.
+/// \note when the kind was returned by memkind_create_kind() all allocated memory must be freed
+///       before kind is destroyed, otherwise this will cause memory leak.
 /// \param kind specified memory kind
 /// \return Memkind operation status, MEMKIND_SUCCESS on success, MEMKIND_ERROR_OPERATION_FAILED on failure
 ///
