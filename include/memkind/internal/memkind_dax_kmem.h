008d9ebb2008c78cb6b2bffe3c0baef6b53d5b7e
Krzysztof Filipek <krzysztof.filipek@intel.com>
2019-07-22 17:24:05 +0200

    Add static kind: DAX KMEM

---
+++ include/memkind/internal/memkind_dax_kmem.h
@@ -0,0 +1,51 @@
+/*
+ * Copyright (C) 2019 Intel Corporation.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 1. Redistributions of source code must retain the above copyright notice(s),
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice(s),
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDER(S) ``AS IS'' AND ANY EXPRESS
+ * OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO
+ * EVENT SHALL THE COPYRIGHT HOLDER(S) BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+ * OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#pragma once
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <memkind.h>
+
+/*
+ * Header file for the DAX KMEM memory memkind operations.
+ * More details in memkind_dax_kmem(3) man page.
+ *
+ * Functionality defined in this header is considered as EXPERIMENTAL API.
+ * API standards are described in memkind(3) man page.
+ */
+
+int memkind_dax_kmem_check_available(struct memkind *kind);
+int memkind_dax_kmem_get_mbind_nodemask(struct memkind *kind,
+                                        unsigned long *nodemask, unsigned long maxnode);
+int memkind_dax_kmem_all_get_mbind_nodemask(struct memkind *kind,
+                                            unsigned long *nodemask, unsigned long maxnode);
+void memkind_dax_kmem_init_once(void);
+
+extern struct memkind_ops MEMKIND_DAX_KMEM_OPS;
+
+#ifdef __cplusplus
+}
+#endif
