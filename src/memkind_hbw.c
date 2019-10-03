d25b04166acc1c61ae7de9b73459f98aaf480fce
michalbiesek <michal.biesek@intel.com>
2019-10-03 13:03:55 +0200

    Change setting bandwidth arbitary values mechanism
    - replace malloc with calloc to set BANDWIDTH_NODE_NOT_PRESENT as
    default value in badwidth array
    - NUMA node configuration could not be continuous, e.g. NUMA
     configuration like 0,1,4 is full legitimate, where 4 is NUMA node
     created from persistent memory

--- src/memkind_hbw.c
+++ src/memkind_hbw.c
@@ -353,7 +353,7 @@ static int fill_bandwidth_values_heuristically(int *bandwidth)
 static void memkind_hbw_closest_numanode_init(void)
 {
     struct bandwidth_closest_numanode_t *g = &memkind_hbw_closest_numanode_g;
-    int *bandwidth = (int *)jemk_malloc(sizeof(int) * NUMA_NUM_NODES);
+    int *bandwidth = (int *)jemk_calloc(NUMA_NUM_NODES, sizeof(int));
     int num_unique = 0;
 
     struct bandwidth_nodes_t *bandwidth_nodes = NULL;
