af2c5a3d3240ca4ed40747624cc3102c96b717d5
michalbiesek <michal.biesek@intel.com>
2019-11-08 09:13:11 +0100

    Add unit_tests_dax_kmem target
    - add new target to test separate dax kmem functionality

--- memkind.spec.mk
+++ memkind.spec.mk
@@ -183,6 +183,7 @@ rm -f %{buildroot}/%{_libdir}/libautohbw.{l,}a
 %defattr(-,root,root,-)
 $(memkind_test_dir)/all_tests
 $(memkind_test_dir)/environ_err_hbw_malloc_test
+${memkind_test_dir}/dax_kmem_test
 $(memkind_test_dir)/decorator_test
 $(memkind_test_dir)/locality_test
 $(memkind_test_dir)/freeing_memory_segfault_test
