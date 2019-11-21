da838e9b0bc11d7eacd845dbcd598380e310d73d
kasiawasiuta <katarzyna.wasiuta@intel.com>
2019-11-21 15:55:48 +0100

    Tests for auto kmem dax

--- test/memkind_dax_kmem_test.cpp
+++ test/memkind_dax_kmem_test.cpp
@@ -23,7 +23,14 @@
  */
 
 #include <memkind.h>
+#include <numa.h>
+#include <numaif.h>
+#include <set>
+#include <vector>
+
 #include "common.h"
+
+#define GTEST_COUT std::cerr << "[          ] [ INFO ]"
 
 class MemkindDaxKmemTests: public ::testing::Test
 {
@@ -42,4 +49,77 @@ TEST_F(MemkindDaxKmemTests,
     void *ptr = memkind_malloc(MEMKIND_DAX_KMEM, 4096);
     ASSERT_NE(nullptr, ptr);
     memkind_free(nullptr, ptr);
+}
+
+TEST_F(MemkindDaxKmemTests,
+       test_TC_MEMKIND_MEMKIND_DAX_KMEM_alloc_until_full_numa)
+{
+    void *ptr;
+    long long free_space;
+    const size_t alloc_size = 1 * MB;
+    size_t numa_size;
+    int numa_id = -1;
+    std::vector<void *> allocations;
+
+    ptr = memkind_malloc(MEMKIND_DAX_KMEM, alloc_size);
+    ASSERT_TRUE(nullptr != ptr);
+    memset(ptr, 'a', alloc_size);
+    allocations.push_back(ptr);
+
+    get_mempolicy(&numa_id, nullptr, 0, ptr, MPOL_F_NODE | MPOL_F_ADDR);
+    numa_size = numa_node_size64(numa_id, nullptr);
+
+    while (0.85 * numa_size > alloc_size * allocations.size()) {
+        ptr = memkind_malloc(MEMKIND_DAX_KMEM, alloc_size);
+        memset(ptr, 'a', alloc_size);
+        numa_node_size64(numa_id, &free_space);
+        allocations.push_back(ptr);
+    }
+
+    EXPECT_GE(10 * MB + alloc_size, unsigned(free_space));
+
+    for (auto const &ptr: allocations) {
+        memkind_free(MEMKIND_DAX_KMEM, ptr);
+    }
+}
+
+TEST_F(MemkindDaxKmemTests,
+       test_TC_MEMKIND_MEMKIND_DAX_KMEM_ALL_alloc_until_full_numa)
+{
+    void *ptr;
+    const size_t alloc_size = 1 * MB;
+    int numa_id = -1;
+    long  sum_of_kmem_dax_free_space = 0;
+    long long free_space;
+    std::vector<void *> allocations;
+    std::set<int> dax_kmem_nodes;
+    struct bitmask *cpu_mask = numa_allocate_cpumask();
+
+    const int MAXNODE_ID = numa_num_configured_nodes();
+    for (int id = 0; id < MAXNODE_ID; ++id) {
+        numa_node_to_cpus(id, cpu_mask);
+
+        // Check if numa node exists and if it is KMEM DAX NUMA
+        if (numa_node_size64(id, &free_space) > 0
+        && numa_bitmask_weight(cpu_mask) == 0) {
+            dax_kmem_nodes.insert(id);
+            sum_of_kmem_dax_free_space += free_space;
+        }
+    }
+    numa_free_cpumask(cpu_mask);
+
+    while (0.85 * sum_of_kmem_dax_free_space > alloc_size * allocations.size()) {
+        ptr = memkind_malloc(MEMKIND_DAX_KMEM_ALL, alloc_size);
+        ASSERT_TRUE(nullptr != ptr);
+        memset(ptr, 'a', alloc_size);
+
+        get_mempolicy(&numa_id, nullptr, 0, ptr, MPOL_F_NODE | MPOL_F_ADDR);
+        ASSERT_EQ(size_t(1), dax_kmem_nodes.count(numa_id));
+
+        allocations.push_back(ptr);
+    }
+
+    for (auto const &ptr: allocations) {
+        memkind_free(MEMKIND_DAX_KMEM_ALL, ptr);
+    }
 }
