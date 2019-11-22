1ce95711435836d1d46deb3d26ceb04ac8b8cde0
michalbiesek <michal.biesek@intel.com>
2019-11-22 16:15:03 +0100

    Provide function to improve memory utilization
    - extend API by memkind_defrag_reallocate
    - useful in case reduce fragmentation for small allocation

--- src/heap_manager.c
+++ src/heap_manager.c
@@ -42,6 +42,7 @@ struct heap_manager_ops {
     struct memkind *(*heap_manager_detect_kind)(void *ptr);
     int (*heap_manager_update_cached_stats)(void);
     int (*heap_manager_get_stat)(memkind_stat_type stat, size_t *value);
+    void *(*heap_manager_defrag_reallocate)(void *ptr);
 };
 
 static struct heap_manager_ops arena_heap_manager_g = {
@@ -51,7 +52,8 @@ static struct heap_manager_ops arena_heap_manager_g = {
     .heap_manager_realloc = memkind_arena_realloc_with_kind_detect,
     .heap_manager_detect_kind = memkind_arena_detect_kind,
     .heap_manager_update_cached_stats = memkind_arena_update_cached_stats,
-    .heap_manager_get_stat = memkind_arena_get_global_stat
+    .heap_manager_get_stat = memkind_arena_get_global_stat,
+    .heap_manager_defrag_reallocate = memkind_arena_defrag_reallocate_with_kind_detect
 };
 
 static struct heap_manager_ops tbb_heap_manager_g = {
@@ -61,7 +63,8 @@ static struct heap_manager_ops tbb_heap_manager_g = {
     .heap_manager_realloc = tbb_pool_realloc_with_kind_detect,
     .heap_manager_detect_kind = tbb_detect_kind,
     .heap_manager_update_cached_stats = tbb_update_cached_stats,
-    .heap_manager_get_stat = tbb_get_global_stat
+    .heap_manager_get_stat = tbb_get_global_stat,
+    .heap_manager_defrag_reallocate = tbb_pool_defrag_reallocate_with_kind_detect
 };
 
 static void set_heap_manager()
@@ -111,5 +114,10 @@ int heap_manager_update_cached_stats(void)
 
 int heap_manager_get_stat(memkind_stat_type stat, size_t *value)
 {
-    return  get_heap_manager()->heap_manager_get_stat(stat, value);
+    return get_heap_manager()->heap_manager_get_stat(stat, value);
+}
+
+void *heap_manager_defrag_reallocate(void *ptr)
+{
+    return get_heap_manager()->heap_manager_defrag_reallocate(ptr);
 }
