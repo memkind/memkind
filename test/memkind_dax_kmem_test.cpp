3f46c71b8f059630e5e8eb4bfd27df4539d44caf
kasiawasiuta <katarzyna.wasiuta@intel.com>
2019-12-02 11:29:42 +0100

    Provide API useful for DAX_KMEM tests
        -check usage of swap for current process
        -finding persistent memory NUMA node(s) for current process
        -finding closest NUMA node(s) for current process
        -getting sum of free space of pm NUMA nodes

--- test/memkind_dax_kmem_test.cpp
+++ test/memkind_dax_kmem_test.cpp
@@ -23,7 +23,93 @@
  */
 
 #include <memkind.h>
+
+#include <assert.h>
+#include <climits>
+#include <numa.h>
+#include <set>
+#include <unistd.h>
+
 #include "common.h"
+
+static std::set<int> get_dax_kmem_nodes(void)
+{
+    struct bitmask *cpu_mask = numa_allocate_cpumask();
+    std::set<int> dax_kmem_nodes;
+    long long free_space;
+
+    const int MAXNODE_ID = numa_num_configured_nodes();
+    for (int id = 0; id < MAXNODE_ID; ++id) {
+        numa_node_to_cpus(id, cpu_mask);
+
+        // Check if numa node exists and if it is NUMA node created from persistent memory
+        if (numa_node_size64(id, &free_space) > 0 &&
+            numa_bitmask_weight(cpu_mask) == 0) {
+            dax_kmem_nodes.insert(id);
+        }
+    }
+    numa_free_cpumask(cpu_mask);
+
+    return dax_kmem_nodes;
+}
+
+static size_t get_free_dax_kmem_space(void)
+{
+    size_t sum_of_dax_kmem_free_space = 0;
+    long long free_space;
+    std::set<int> dax_kmem_nodes = get_dax_kmem_nodes();
+
+    for(auto const &node: dax_kmem_nodes) {
+        numa_node_size64(node, &free_space);
+        sum_of_dax_kmem_free_space += free_space;
+    }
+
+    return sum_of_dax_kmem_free_space;
+}
+
+static std::set<int> get_closest_dax_kmem_numa_nodes(void)
+{
+    int min_distance = INT_MAX;
+    int process_cpu = sched_getcpu();
+    int process_node = numa_node_of_cpu(process_cpu);
+    std::set<int> closest_numa_ids;
+    std::set<int> dax_kmem_nodes = get_dax_kmem_nodes();
+
+    for (auto const &node: dax_kmem_nodes) {
+        int distance_to_i_node = numa_distance(process_node, node);
+
+        if (distance_to_i_node < min_distance) {
+            min_distance = distance_to_i_node;
+            closest_numa_ids.clear();
+            closest_numa_ids.insert(node);
+        } else if (distance_to_i_node == min_distance) {
+            closest_numa_ids.insert(node);
+        }
+    }
+
+    return closest_numa_ids;
+}
+
+static size_t get_used_swap_space(void)
+{
+    size_t vm_swap = 0;
+    bool status = false;
+    FILE *fp = fopen("/proc/self/status", "r");
+
+    if (fp) {
+        char buffer[BUFSIZ];
+        while (fgets(buffer, sizeof(buffer), fp)) {
+            if (sscanf(buffer, "VmSwap: %zu kB", &vm_swap) == 1) {
+                status = true;
+                break;
+            }
+        }
+        fclose(fp);
+    }
+    assert(status && "Couldn't access swap space");
+
+    return vm_swap * KB;
+}
 
 class MemkindDaxKmemTests: public ::testing::Test
 {
@@ -42,4 +128,10 @@ TEST_F(MemkindDaxKmemTests,
     void *ptr = memkind_malloc(MEMKIND_DAX_KMEM, 4096);
     ASSERT_NE(nullptr, ptr);
     memkind_free(nullptr, ptr);
+
+    // TODO: remove four below lines, when adding more tests
+    get_used_swap_space();
+    get_dax_kmem_nodes();
+    get_closest_dax_kmem_numa_nodes();
+    get_free_dax_kmem_space();
 }
