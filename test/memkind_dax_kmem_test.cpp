c7ab9ac92945263bffa7c08d9ae682857afdfb3c
kasiawasiuta <katarzyna.wasiuta@intel.com>
2019-12-02 11:29:42 +0100

    Api for kmem dax tests

--- test/memkind_dax_kmem_test.cpp
+++ test/memkind_dax_kmem_test.cpp
@@ -23,6 +23,12 @@
  */
 
 #include <memkind.h>
+
+#include <climits>
+#include <numa.h>
+#include <unistd.h>
+#include <vector>
+
 #include "common.h"
 
 class MemkindDaxKmemTests: public ::testing::Test
@@ -42,4 +48,65 @@ TEST_F(MemkindDaxKmemTests,
     void *ptr = memkind_malloc(MEMKIND_DAX_KMEM, 4096);
     ASSERT_NE(nullptr, ptr);
     memkind_free(nullptr, ptr);
+}
+
+
+static size_t get_kmem_dax_nodes(std::vector<int>& dax_kmem_nodes) {
+    struct bitmask *cpu_mask = numa_allocate_cpumask();
+    long sum_of_kmem_dax_free_space = 0;
+    long long free_space;
+
+    const int MAXNODE_ID = numa_num_configured_nodes();
+    for (int id = 0; id < MAXNODE_ID; ++id) {
+        numa_node_to_cpus(id, cpu_mask);
+
+        // Check if numa node exists and if it is KMEM DAX NUMA
+        if (numa_node_size64(id, &free_space) > 0 && numa_bitmask_weight(cpu_mask) == 0) {
+            dax_kmem_nodes.push_back(id);
+            sum_of_kmem_dax_free_space += free_space;
+        }
+    }
+    numa_free_cpumask(cpu_mask);
+
+    return sum_of_kmem_dax_free_space;
+}
+
+static std::vector<int> get_closest_numa_node(std::vector<int> dax_kmem_nodes) {
+    int min_distance = INT_MAX;
+    int process_cpu = sched_getcpu();
+    int process_node = numa_node_of_cpu(process_cpu);
+    std::vector<int> closest_numa_ids;
+
+    for (unsigned i = 0; i < dax_kmem_nodes.size(); ++i) {
+        int distance_to_i_node = numa_distance(process_node, dax_kmem_nodes[i]);
+        if (distance_to_i_node < min_distance) {
+            min_distance = distance_to_i_node;
+            closest_numa_ids.clear();
+            closest_numa_ids.push_back(dax_kmem_nodes[i]);
+        } else if (distance_to_i_node == min_distance) {
+            closest_numa_ids.push_back(dax_kmem_nodes[i]);
+        }
+    }
+
+    return closest_numa_ids;
+}
+
+static bool is_swap_used(size_t alloc_size_KB) {
+    char command[128];
+    char output[128];
+
+    sprintf(command, "awk '/VmSwap/{print $2}' /proc/%d/status", getpid());
+    FILE *pipe = popen(command, "r");
+    if (!pipe) {
+        std::cerr << "Could not start pipe command." << std::endl;
+        return false;
+    }
+
+    while (fgets(output, sizeof(output), pipe) != nullptr) {}
+    pclose(pipe);
+
+    size_t used_swap_KB = std::stoul(output);
+    if (used_swap_KB > alloc_size_KB) return true;
+
+    return false;
 }
