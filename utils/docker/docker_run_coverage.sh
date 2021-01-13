8869a612bfdb1c82ad20826eb86ef49a13e47fd7
michalbiesek <michal.biesek@intel.com>
2021-01-13 20:37:10 +0100

    Update curl timeouts
    - by default codecov-bash uses:
     --retry 5 --retry-delay 2 --connect-timeout 2
    - override defaults using -U option
    - ref: https://docs.codecov.io/docs/about-the-codecov-bash-uploader

--- utils/docker/docker_run_coverage.sh
+++ utils/docker/docker_run_coverage.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # SPDX-License-Identifier: BSD-2-Clause
-# Copyright (C) 2019 - 2020 Intel Corporation.
+# Copyright (C) 2019 - 2021 Intel Corporation.
 
 # docker_run_coverage.sh - is called inside a Docker container;
 # calculates coverage and creates online report
@@ -12,4 +12,5 @@
 # -Z           Exit with 1 if not successful. Default will Exit with 0
 
 MEMKIND_REPO_PATH=$1
-bash <(curl -s https://codecov.io/bash) -t "$CODECOV_TOKEN" -cF "$TEST_SUITE_NAME" -p "$MEMKIND_REPO_PATH" -Z -v
+CURL_ARGS="--retry 10 --retry-delay 2 --connect-timeout 10"
+bash <(curl -s  https://codecov.io/bash) -t "$CODECOV_TOKEN" -cF "$TEST_SUITE_NAME" -p "$MEMKIND_REPO_PATH" -Z -U "$CURL_ARGS" -v
