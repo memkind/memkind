221f290e62acaa6869f01dafac7309495622b549
michalbiesek <michal.biesek@intel.com>
2021-01-21 11:33:57 +0100

    Add low latency local kinds
    - MEMKIND_LOWEST_LATENCY_LOCAL
    - MEMKIND_LOWEST_LATENCY_LOCAL_PREFERRED

--- test/memkind_hmat_tests.cpp
+++ test/memkind_hmat_tests.cpp
@@ -87,7 +87,9 @@ INSTANTIATE_TEST_CASE_P(
     KindParam, MemkindHMATFunctionalTestsParam,
     ::testing::Values(MEMKIND_HBW,
                       MEMKIND_HIGHEST_CAPACITY_LOCAL,
-                      MEMKIND_HIGHEST_CAPACITY_LOCAL_PREFERRED));
+                      MEMKIND_HIGHEST_CAPACITY_LOCAL_PREFERRED,
+                      MEMKIND_LOWEST_LATENCY_LOCAL,
+                      MEMKIND_LOWEST_LATENCY_LOCAL_PREFERRED));
 
 TEST_P(MemkindHMATFunctionalTestsParam,
        test_tc_memkind_HMAT_verify_InitTargetNode)
