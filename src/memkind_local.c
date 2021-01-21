b1d2dbcaffd319c4a4020e46b22601d666c8faf8
michalbiesek <michal.biesek@intel.com>
2021-01-21 14:15:16 +0100

    Add highest bandwidth local kinds
    - MEMKIND_HIGHEST_BANDWDITH_LOCAL
    - MEMKIND_HIGHEST_BANDWIDTH_LOCAL_PREFERRED

--- src/memkind_local.c
+++ src/memkind_local.c
@@ -31,6 +31,8 @@ static struct loc_numanodes_t memkind_hi_cap_loc_numanodes_g[NODE_VARIANT_MAX];
 static pthread_once_t memkind_hi_cap_loc_numanodes_once_g[NODE_VARIANT_MAX] = {PTHREAD_ONCE_INIT};
 static struct loc_numanodes_t memkind_low_lat_loc_numanodes_g[NODE_VARIANT_MAX];
 static pthread_once_t memkind_low_lat_loc_numanodes_once_g[NODE_VARIANT_MAX] = {PTHREAD_ONCE_INIT};
+static struct loc_numanodes_t memkind_hi_bw_loc_numanodes_g[NODE_VARIANT_MAX];
+static pthread_once_t memkind_hi_bw_loc_numanodes_once_g[NODE_VARIANT_MAX] = {PTHREAD_ONCE_INIT};
 
 static void memkind_hi_cap_loc_numanodes_init(void)
 {
@@ -62,6 +64,21 @@ static void memkind_low_lat_loc_preferred_numanodes_init(void)
             &memkind_low_lat_loc_numanodes_g[NODE_VARIANT_SINGLE];
     g->init_err = get_per_cpu_local_nodes_mask(&g->per_cpu_numa_nodes,
                                                NODE_VARIANT_SINGLE, MEM_ATTR_LATENCY);
+}
+
+static void memkind_hi_bw_loc_numanodes_init(void)
+{
+    struct loc_numanodes_t *g =
+            &memkind_hi_bw_loc_numanodes_g[NODE_VARIANT_MULTIPLE];
+    g->init_err = get_per_cpu_local_nodes_mask(&g->per_cpu_numa_nodes,
+                                               NODE_VARIANT_MULTIPLE, MEM_ATTR_BANDWIDTH);
+}
+
+static void memkind_hi_bw_loc_preferred_numanodes_init(void)
+{
+    struct loc_numanodes_t *g = &memkind_hi_bw_loc_numanodes_g[NODE_VARIANT_SINGLE];
+    g->init_err = get_per_cpu_local_nodes_mask(&g->per_cpu_numa_nodes,
+                                               NODE_VARIANT_SINGLE, MEM_ATTR_BANDWIDTH);
 }
 
 static int memkind_hi_cap_loc_get_mbind_nodemask(
@@ -134,6 +151,40 @@ static int memkind_low_lat_loc_preferred_get_mbind_nodemask(
     return g->init_err;
 }
 
+static int memkind_hi_bw_loc_get_mbind_nodemask(
+    struct memkind *kind, unsigned long *nodemask, unsigned long maxnode)
+{
+    struct loc_numanodes_t *g =
+            &memkind_hi_bw_loc_numanodes_g[NODE_VARIANT_MULTIPLE];
+    pthread_once(&memkind_hi_bw_loc_numanodes_once_g[NODE_VARIANT_MULTIPLE],
+                 memkind_hi_bw_loc_numanodes_init);
+
+    if (MEMKIND_LIKELY(!g->init_err)) {
+        int cpu_id = sched_getcpu();
+        struct bitmask nodemask_bm = {maxnode, nodemask};
+        copy_bitmask_to_bitmask(g->per_cpu_numa_nodes[cpu_id], &nodemask_bm);
+    }
+
+    return g->init_err;
+}
+
+static int memkind_hi_bw_loc_preferred_get_mbind_nodemask(
+    struct memkind *kind, unsigned long *nodemask, unsigned long maxnode)
+{
+    struct loc_numanodes_t *g = &memkind_hi_bw_loc_numanodes_g[NODE_VARIANT_SINGLE];
+
+    pthread_once(&memkind_hi_bw_loc_numanodes_once_g[NODE_VARIANT_SINGLE],
+                 memkind_hi_bw_loc_preferred_numanodes_init);
+
+    if (MEMKIND_LIKELY(!g->init_err)) {
+        int cpu_id = sched_getcpu();
+        struct bitmask nodemask_bm = {maxnode, nodemask};
+        copy_bitmask_to_bitmask(g->per_cpu_numa_nodes[cpu_id], &nodemask_bm);
+    }
+
+    return g->init_err;
+}
+
 static int memkind_hi_cap_loc_finalize(memkind_t kind)
 {
     local_kind_finalize(&memkind_hi_cap_loc_numanodes_g[NODE_VARIANT_MULTIPLE]);
@@ -155,6 +206,18 @@ static int memkind_low_lat_loc_finalize(memkind_t kind)
 static int memkind_low_lat_loc_preferred_finalize(memkind_t kind)
 {
     local_kind_finalize(&memkind_low_lat_loc_numanodes_g[NODE_VARIANT_SINGLE]);
+    return memkind_arena_finalize(kind);
+}
+
+static int memkind_hi_bw_loc_finalize(memkind_t kind)
+{
+    local_kind_finalize(&memkind_hi_bw_loc_numanodes_g[NODE_VARIANT_MULTIPLE]);
+    return memkind_arena_finalize(kind);
+}
+
+static int memkind_hi_bw_loc_preferred_finalize(memkind_t kind)
+{
+    local_kind_finalize(&memkind_hi_bw_loc_numanodes_g[NODE_VARIANT_SINGLE]);
     return memkind_arena_finalize(kind);
 }
 
@@ -181,6 +244,15 @@ static void memkind_low_lat_loc_init_once(void)
 static void memkind_low_lat_loc_preferred_init_once(void)
 {
     memkind_init(MEMKIND_LOWEST_LATENCY_LOCAL_PREFERRED, true);
+}
+static void memkind_hi_bw_loc_init_once(void)
+{
+    memkind_init(MEMKIND_HIGHEST_BANDWIDTH_LOCAL, true);
+}
+
+static void memkind_hi_bw_loc_preferred_init_once(void)
+{
+    memkind_init(MEMKIND_HIGHEST_BANDWIDTH_LOCAL_PREFERRED, true);
 }
 
 MEMKIND_EXPORT struct memkind_ops MEMKIND_HIGHEST_CAPACITY_LOCAL_OPS = {
@@ -264,6 +336,49 @@ MEMKIND_EXPORT struct memkind_ops MEMKIND_LOWEST_LATENCY_LOCAL_PREFERRED_OPS = {
     .init_once = memkind_low_lat_loc_preferred_init_once,
     .malloc_usable_size = memkind_default_malloc_usable_size,
     .finalize = memkind_low_lat_loc_preferred_finalize,
+    .get_stat = memkind_arena_get_kind_stat,
+    .defrag_reallocate = memkind_arena_defrag_reallocate
+};
+
+MEMKIND_EXPORT struct memkind_ops MEMKIND_HIGHEST_BANDWIDTH_LOCAL_OPS = {
+    .create = memkind_arena_create,
+    .destroy = memkind_default_destroy,
+    .malloc = memkind_arena_malloc,
+    .calloc = memkind_arena_calloc,
+    .posix_memalign = memkind_arena_posix_memalign,
+    .realloc = memkind_arena_realloc,
+    .free = memkind_arena_free,
+    .check_available = memkind_loc_check_available,
+    .mbind = memkind_default_mbind,
+    .get_mmap_flags = memkind_default_get_mmap_flags,
+    .get_mbind_mode = memkind_default_get_mbind_mode,
+    .get_mbind_nodemask = memkind_hi_bw_loc_get_mbind_nodemask,
+    .get_arena = memkind_thread_get_arena,
+    .init_once = memkind_hi_bw_loc_init_once,
+    .malloc_usable_size = memkind_default_malloc_usable_size,
+    .finalize = memkind_hi_bw_loc_finalize,
+    .get_stat = memkind_arena_get_kind_stat,
+    .defrag_reallocate = memkind_arena_defrag_reallocate
+};
+
+MEMKIND_EXPORT struct memkind_ops MEMKIND_HIGHEST_BANDWIDTH_LOCAL_PREFERRED_OPS
+    = {
+    .create = memkind_arena_create,
+    .destroy = memkind_default_destroy,
+    .malloc = memkind_arena_malloc,
+    .calloc = memkind_arena_calloc,
+    .posix_memalign = memkind_arena_posix_memalign,
+    .realloc = memkind_arena_realloc,
+    .free = memkind_arena_free,
+    .check_available = memkind_loc_check_available,
+    .mbind = memkind_default_mbind,
+    .get_mmap_flags = memkind_default_get_mmap_flags,
+    .get_mbind_mode = memkind_preferred_get_mbind_mode,
+    .get_mbind_nodemask = memkind_hi_bw_loc_preferred_get_mbind_nodemask,
+    .get_arena = memkind_thread_get_arena,
+    .init_once = memkind_hi_bw_loc_preferred_init_once,
+    .malloc_usable_size = memkind_default_malloc_usable_size,
+    .finalize = memkind_hi_bw_loc_preferred_finalize,
     .get_stat = memkind_arena_get_kind_stat,
     .defrag_reallocate = memkind_arena_defrag_reallocate
 };
