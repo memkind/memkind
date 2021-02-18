3d4b76d69da56914eb7969e338dd25d2ba129441
Rafal Rudnicki <rafal.rudnicki@intel.com>
2021-02-18 15:27:02 +0000

    Create libmemtier lib

---
+++ tiering/memtier_log.h
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: BSD-2-Clause
+/* Copyright (C) 2021 Intel Corporation. */
+
+#include <stdio.h>
+
+typedef enum
+{
+    MESSAGE_TYPE_ERROR, // default
+    MESSAGE_TYPE_INFO,
+    MESSAGE_TYPE_DEBUG,
+    MESSAGE_TYPE_MAX_VALUE,
+} message_type_t;
+
+void log_generic(message_type_t type, const char *format, va_list args);
+void log_info(const char *format, ...);
+void log_err(const char *format, ...);
+void log_debug(const char *format, ...);
+void log_init_once(void);
