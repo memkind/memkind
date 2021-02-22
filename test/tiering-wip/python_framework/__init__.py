8b1ca48bfbedb306d404a6283ac91a14583be5c2
Rafal Rudnicki <rafal.rudnicki@intel.com>
2021-02-22 08:32:21 +0000

    Make "unit_tests_tiering" target for tiering tests

---
+++ test/tiering-wip/python_framework/__init__.py
@@ -0,0 +1,5 @@
+# SPDX-License-Identifier: BSD-2-Clause
+# Copyright (C) 2016 - 2020 Intel Corporation.
+
+from python_framework.cmd_helper import CMD_helper
+from python_framework.huge_page_organizer import Huge_page_organizer
