ca61b922dc7d0087b981a75b118cbfa7b29d0ee5
Adam Borowski <kilobyte@angband.pl>
2021-02-25 11:57:06 +0100

    Basic malloc wrappers.
    For now, no functionality but calling glibc, regardless of LD_PRELOADs.
    
    Add tests for memory allocator wrappers

--- tiering/memtier.c
+++ tiering/memtier.c
@@ -109,6 +109,40 @@ void log_generic(message_type_t type, const char *format, va_list args)
     }
 }
 
+// glibc defines these, a version portable to others libcs would need to call
+// dlsym() at runtime.
+extern void *__libc_malloc(size_t size);
+extern void *__libc_calloc(size_t nmemb, size_t size);
+extern void *__libc_realloc(void *ptr, size_t size);
+extern void __libc_free(void *ptr);
+
+void UTILS_EXPORT *malloc(size_t size)
+{
+    void *ret = __libc_malloc(size);
+    log_debug("malloc(%u) = %p", size, ret);
+    return ret;
+}
+
+void UTILS_EXPORT *calloc(size_t nmemb, size_t size)
+{
+    void *ret = __libc_calloc(nmemb, size);
+    log_debug("calloc(%u, %u) = %p", nmemb, size, ret);
+    return ret;
+}
+
+void UTILS_EXPORT *realloc(void *ptr, size_t size)
+{
+    void *ret = __libc_realloc(ptr, size);
+    log_debug("realloc(%p, %u) = %p", ptr, size, ret);
+    return ret;
+}
+
+void UTILS_EXPORT free(void *ptr)
+{
+    log_debug("free(%p)", ptr);
+    return __libc_free(ptr);
+}
+
 static void UTILS_INIT utils_init(void)
 {
     pthread_once(&init_once, log_init_once);
@@ -120,17 +154,3 @@ static void UTILS_FINI utils_fini(void)
 {
     log_info("Unloading memkind mem tiering utils lib!");
 }
-
-UTILS_EXPORT void *test(size_t size)
-{
-    log_debug("test");
-
-    return 0;
-}
-
-#ifndef __MALLOC_HOOK_VOLATILE
-#define __MALLOC_HOOK_VOLATILE
-#endif
-
-void *(*__MALLOC_HOOK_VOLATILE __test_hook)(size_t size,
-                                            const void *caller) = (void *)test;
