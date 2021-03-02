dabdf8b58dcff4ad4050c47c73a82ed5d6db2780
michalbiesek <michal.biesek@intel.com>
2021-03-02 17:29:45 +0100

    Add tests for memkind_memtier

--- memkind.spec.mk
+++ memkind.spec.mk
@@ -202,6 +202,7 @@ $(memkind_test_dir)/memkind_allocated
 $(memkind_test_dir)/memkind_cpp_allocator
 $(memkind_test_dir)/memkind_get_stat
 ${memkind_test_dir}/memkind_highcapacity_test
+${memkind_test_dir}/memkind_memtier_test
 $(memkind_test_dir)/memkind_stat_test
 $(memkind_test_dir)/autohbw_candidates
 ${memkind_test_dir}/pmem_kinds
