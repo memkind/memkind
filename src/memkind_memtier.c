0f1c8c2e152162de6c0b887ba057441a88ff3a3e
Rafal Rudnicki <rafal.rudnicki@intel.com>
2021-03-05 11:06:22 +0000

    Add logging to memkind_memtier

--- src/memkind_memtier.c
+++ src/memkind_memtier.c
@@ -104,52 +104,75 @@ MEMKIND_EXPORT void memtier_delete_kind(struct memtier_kind *kind)
 MEMKIND_EXPORT void *memtier_kind_malloc(struct memtier_kind *kind, size_t size)
 {
     // TODO provide tiering_kind logic
-    return memkind_malloc(kind->todo_kind, size);
+    void *ptr = memkind_malloc(kind->todo_kind, size);
+    log_info("malloc(%zu) = %p from kind: %s", size, ptr,
+             kind->todo_kind->name);
+    return ptr;
 }
 
 MEMKIND_EXPORT void *memtier_tier_malloc(struct memtier_tier *tier, size_t size)
 {
     // TODO provide increment counter logic
-    return memkind_malloc(tier->kind, size);
+    void *ptr = memkind_malloc(tier->kind, size);
+    log_info("malloc(%zu) = %p from tier: %s", size, ptr, tier->kind->name);
+    return ptr;
 }
 
 MEMKIND_EXPORT void *memtier_kind_calloc(struct memtier_kind *kind, size_t num,
                                          size_t size)
 {
     // TODO provide tiering_kind logic
-    return memkind_calloc(kind->todo_kind, num, size);
+    void *ptr = memkind_calloc(kind->todo_kind, num, size);
+    log_info("calloc(%zu, %zu) = %p from kind: %s", num, size, ptr,
+             kind->todo_kind->name);
+    return ptr;
 }
 
 MEMKIND_EXPORT void *memtier_tier_calloc(struct memtier_tier *tier, size_t num,
                                          size_t size)
 {
     // TODO provide increment counter logic
-    return memkind_calloc(tier->kind, num, size);
+    void *ptr = memkind_calloc(tier->kind, num, size);
+    log_info("calloc(%zu, %zu) = %p from kind: %s", num, size, ptr,
+             tier->kind->name);
+    return ptr;
 }
 
 MEMKIND_EXPORT void *memtier_kind_realloc(struct memtier_kind *kind, void *ptr,
                                           size_t size)
 {
     // TODO provide tiering_kind logic
-    return memkind_realloc(kind->todo_kind, ptr, size);
+    void *ret = memkind_realloc(kind->todo_kind, ptr, size);
+    log_info("realloc(%p, %zu) = %p from kind: %s", ptr, size, ret,
+             kind->todo_kind->name);
+    return ret;
 }
 
 MEMKIND_EXPORT void *memtier_tier_realloc(struct memtier_tier *tier, void *ptr,
                                           size_t size)
 {
+    void *ret = NULL;
     if (size == 0 && ptr != NULL) {
+        log_info("free(%p) from kind: %s", ptr, tier->kind->name);
         memkind_free(tier->kind, ptr);
         // TODO provide decrement counter logic
-        return NULL;
+        return ret;
     } else if (ptr == NULL) {
         // TODO provide increment counter logic
-        return memkind_malloc(tier->kind, size);
+        log_info("malloc(%zu) = %p from tier: %s", size, ptr, tier->kind->name);
+        ret = memkind_malloc(tier->kind, size);
     } else {
         size_t prev_size = memkind_malloc_usable_size(tier->kind, ptr);
         (void)(prev_size);
         // TODO provide increment/decrement counter logic
-        return memkind_realloc(tier->kind, ptr, size);
+        log_info("realloc(%p, %zu) = %p from kind: %s", ptr, size, ret,
+                 tier->kind->name);
+        ret = memkind_realloc(tier->kind, ptr, size);
     }
+
+    log_info("realloc(%p, %zu) = %p from kind: %s", ptr, size, ret,
+             tier->kind->name);
+    return ret;
 }
 
 MEMKIND_EXPORT int memtier_kind_posix_memalign(struct memtier_kind *kind,
@@ -157,7 +180,10 @@ MEMKIND_EXPORT int memtier_kind_posix_memalign(struct memtier_kind *kind,
                                                size_t size)
 {
     // TODO provide tiering_kind logic
-    return memkind_posix_memalign(kind->todo_kind, memptr, alignment, size);
+    int ret = memkind_posix_memalign(kind->todo_kind, memptr, alignment, size);
+    log_info("posix_memalign(%p, %zu, %zu) = %d from kind: %s", memptr,
+             alignment, size, ret, kind->todo_kind->name);
+    return ret;
 }
 
 MEMKIND_EXPORT int memtier_tier_posix_memalign(struct memtier_tier *tier,
@@ -165,7 +191,10 @@ MEMKIND_EXPORT int memtier_tier_posix_memalign(struct memtier_tier *tier,
                                                size_t size)
 {
     // TODO provide increment counter logic
-    return memkind_posix_memalign(tier->kind, memptr, alignment, size);
+    int ret = memkind_posix_memalign(tier->kind, memptr, alignment, size);
+    log_info("posix_memalign(%p, %zu, %zu) = %d from kind: %s", memptr,
+             alignment, size, ret, tier->kind->name);
+    return ret;
 }
 
 MEMKIND_EXPORT size_t memtier_usable_size(void *ptr)
@@ -181,5 +210,6 @@ MEMKIND_EXPORT void memtier_free(void *ptr)
     struct memtier_tier *tier = memtier_registry_g.kind_map[kind->partition];
     (void)(tier);
     // TODO provide increment/decrement counter logic
+    log_info("free(%p) from kind: %s", ptr, kind->name);
     memkind_free(kind, ptr);
 }
--- test/tiering_tests.py
+++ test/tiering_tests.py
@@ -7,7 +7,7 @@ import re
 from python_framework import CMD_helper
 
 
-class Test_tiering_log_level(object):
+class Test_tiering_log(object):
     # NOTE: this script should be called from the root of memkind repository
     ld_preload_env = "LD_PRELOAD=tiering/.libs/libmemtier.so"
     # TODO create separate, parametrized binary that could be used for testing
@@ -15,23 +15,13 @@ class Test_tiering_log_level(object):
     bin_path = "ls -l"
     cmd_helper = CMD_helper()
 
-    log_msg_prefix = "MEMKIND_MEM_TIERING_LOG_DEBUG: "
-    re_hex_or_nil = r"((0[xX][a-fA-F0-9]+)|\(nil\))"
-    re_log_valid_messages = [
-        log_msg_prefix + r"malloc\(\d+\) = " + re_hex_or_nil,
-        log_msg_prefix +
-        r"realloc\(" + re_hex_or_nil + r", \d+\) = " + re_hex_or_nil,
-        log_msg_prefix + r"calloc\(\d+, \d+\) = " + re_hex_or_nil,
-        log_msg_prefix + r"memalign\(\d+, \d +\) = " + re_hex_or_nil,
-        log_msg_prefix + r"free\(" + re_hex_or_nil + r"\)",
-        log_msg_prefix + r"kind_name: \w+",
-        log_msg_prefix + r"pmem_path: .*",  # TODO add path re
-        log_msg_prefix + r"pmem_size: (N/A)|\d+",
-        log_msg_prefix + r"ratio_value: \d+",
-    ]
+    log_prefix = "MEMKIND_MEM_TIERING_"
+    log_debug_prefix = log_prefix + "LOG_DEBUG: "
+    log_error_prefix = log_prefix + "LOG_ERROR: "
+    log_info_prefix = log_prefix + "LOG_INFO: "
 
     def get_ld_preload_cmd_output(self, config_env, log_level=None):
-        log_level_env = "MEMKIND_MEM_TIERING_LOG_LEVEL=" + log_level \
+        log_level_env = self.log_prefix + "LOG_LEVEL=" + log_level \
             if log_level else ""
         command = " ".join([self.ld_preload_env, log_level_env,
                             config_env, self.bin_path])
@@ -40,7 +30,7 @@ class Test_tiering_log_level(object):
         assert retcode == 0, \
             "Test failed with error: \nExecution of: '" + command + \
             "' returns: " + str(retcode) + "\noutput: " + output
-        return output
+        return output.splitlines()
 
     def get_default_cmd_output(self):
         output, retcode = self.cmd_helper.execute_cmd(self.bin_path)
@@ -48,17 +38,15 @@ class Test_tiering_log_level(object):
         assert retcode == 0, \
             "Test failed with error: \nExecution of: '" + self.bin_path + \
             "' returns: " + str(retcode) + "\noutput: " + output
-        return output
+        return output.splitlines()
 
     def test_utils_init(self):
         default_output = self.get_default_cmd_output()
-        default_output = default_output.split("\n")
 
         output = self.get_ld_preload_cmd_output(
             "MEMKIND_MEM_TIERING_CONFIG=DRAM:1", log_level="1")
-        output = output.split("\n")
 
-        assert output[0] == "MEMKIND_MEM_TIERING_LOG_INFO: " + \
+        assert output[0] == self.log_info_prefix + \
             "Memkind memtier lib loaded!", "Bad init message"
 
         # check if rest of output from command is unchanged
@@ -67,45 +55,54 @@ class Test_tiering_log_level(object):
 
     def test_utils_log_level_default(self):
         default_output = self.get_default_cmd_output()
-        default_output = default_output.split("\n")
 
         output = self.get_ld_preload_cmd_output(
             "MEMKIND_MEM_TIERING_CONFIG=DRAM:1", log_level="0")
-        output = output.split("\n")
 
         assert output == default_output, "Bad command output"
 
     def test_utils_log_level_not_set(self):
         default_output = self.get_default_cmd_output()
-        default_output = default_output.split("\n")
 
         output = self.get_ld_preload_cmd_output(
-            "MEMKIND_MEM_TIERING_CONFIG=DRAM:1", log_level=None)
-        output = output.split("\n")
+            "MEMKIND_MEM_TIERING_CONFIG=DRAM:1")
 
         assert output == default_output, "Bad command output"
 
     def test_utils_log_level_debug(self):
+        re_hex_or_nil = r"((0[xX][a-fA-F0-9]+)|\(nil\))"
+        re_log_debug_valid_messages = [
+            self.log_debug_prefix + r"malloc\(\d+\) = " + re_hex_or_nil + r"$",
+            self.log_debug_prefix + r"realloc\(" + re_hex_or_nil +
+            r", \d+\) = " + re_hex_or_nil + r"$",
+            self.log_debug_prefix + r"calloc\(\d+, \d+\) = " +
+            re_hex_or_nil + r"$",
+            self.log_debug_prefix + r"memalign\(\d+, \d +\) = " +
+            re_hex_or_nil + r"$",
+            self.log_debug_prefix + r"free\(" + re_hex_or_nil + r"\)$",
+            self.log_debug_prefix + r"kind_name: \w+$",
+            self.log_debug_prefix + r"pmem_path: .*$",  # TODO add path re
+            self.log_debug_prefix + r"pmem_size: (\(null\))|\d+$",
+            self.log_debug_prefix + r"ratio_value: \d+$",
+        ]
+
         default_output = self.get_default_cmd_output()
-        default_output = default_output.split("\n")
 
         output = self.get_ld_preload_cmd_output(
             "MEMKIND_MEM_TIERING_CONFIG=DRAM:1", log_level="2")
-        output = output.split("\n")
 
-        assert output[0] == "MEMKIND_MEM_TIERING_LOG_DEBUG: " + \
+        assert output[0] == self.log_debug_prefix + \
             "Setting log level to: 2", "Bad init message"
 
-        assert output[1] == "MEMKIND_MEM_TIERING_LOG_INFO: " + \
+        assert output[1] == self.log_prefix + "LOG_INFO: " + \
             "Memkind memtier lib loaded!", "Bad init message"
 
         # extract from the output all lines starting with
         # "MEMKIND_MEM_TIERING_LOG" prefix and check if they are correct
-        log_output = [l for l in output[2:]
-                      if l.startswith("MEMKIND_MEM_TIERING_LOG")]
+        log_output = [l for l in output[2:] if l.startswith(self.log_prefix)]
         for log_line in log_output:
-            log_line_valid = any(re.match(pattern, log_line)
-                                 for pattern in self.re_log_valid_messages)
+            log_line_valid = any(re.match(pattern, log_line) for pattern in
+                                 re_log_debug_valid_messages)
             assert log_line_valid, "Bad log message format: " + log_line
 
         # finally check if rest of output from command is unchanged
@@ -113,22 +110,62 @@ class Test_tiering_log_level(object):
         assert output == default_output, "Bad ls output"
 
     def test_utils_log_level_negative(self):
-        log_level_env = "MEMKIND_MEM_TIERING_LOG_LEVEL=-1"
+        log_level_env = self.log_prefix + "LOG_LEVEL=-1"
         command = " ".join([self.ld_preload_env, log_level_env, self.bin_path])
         output_level_neg, _ = self.cmd_helper.execute_cmd(command)
 
-        assert output_level_neg.split("\n")[0] == \
-            "MEMKIND_MEM_TIERING_LOG_ERROR: Wrong value of " + \
-            "MEMKIND_MEM_TIERING_LOG_LEVEL=-1", "Bad init message"
+        assert output_level_neg.splitlines()[0] == \
+            self.log_error_prefix + "Wrong value of " + \
+            self.log_prefix + "LOG_LEVEL=-1", "Bad init message"
 
     def test_utils_log_level_too_high(self):
-        log_level_env = "MEMKIND_MEM_TIERING_LOG_LEVEL=4"
+        log_level_env = self.log_prefix + "LOG_LEVEL=4"
         command = " ".join([self.ld_preload_env, log_level_env, self.bin_path])
         output_level_neg, _ = self.cmd_helper.execute_cmd(command)
 
-        assert output_level_neg.split("\n")[0] == \
-            "MEMKIND_MEM_TIERING_LOG_ERROR: Wrong value of " + \
-            "MEMKIND_MEM_TIERING_LOG_LEVEL=4", "Bad init message"
+        assert output_level_neg.splitlines()[0] == \
+            self.log_error_prefix + "Wrong value of " + \
+            self.log_prefix + "LOG_LEVEL=4", "Bad init message"
+
+
+class Test_memkind_log(object):
+    # NOTE: this script should be called from the root of memkind repository
+    ld_preload_env = "LD_PRELOAD=tiering/.libs/libmemtier.so"
+    # TODO create separate, parametrized binary that could be used for testing
+    # instead of using ls here
+    bin_path = "ls -l"
+    cmd_helper = CMD_helper()
+
+    log_info_prefix = "MEMKIND_INFO: "
+
+    re_hex_or_nil = r"((0[xX][a-fA-F0-9]+)|\(nil\))"
+    re_log_valid_messages = [
+        log_info_prefix + r"malloc\(\d+\) = " + re_hex_or_nil +
+        r" from kind: ",
+        log_info_prefix + r"realloc\(" + re_hex_or_nil + r", \d+\) = " +
+        re_hex_or_nil + r" from kind: ",
+        log_info_prefix + r"calloc\(\d+, \d+\) = " + re_hex_or_nil +
+        r" from kind: ",
+        log_info_prefix + r"memalign\(\d+, \d +\) = " + re_hex_or_nil +
+        r" from kind: ",
+        log_info_prefix + r"free\(" + re_hex_or_nil + r"\) from kind: ",
+    ]
+
+    def test_DRAM_only(self):
+        memkind_debug_env = "MEMKIND_DEBUG=1"
+        tiering_cfg_env = "MEMKIND_MEM_TIERING_CONFIG=DRAM:1"
+        kind_name = "memkind_default$"
+
+        command = " ".join([self.ld_preload_env, tiering_cfg_env,
+                            memkind_debug_env, self.bin_path])
+        output, _ = self.cmd_helper.execute_cmd(command)
+        output = output.splitlines()
+        output = [l for l in output if l.startswith(self.log_info_prefix)]
+
+        for log_line in output:
+            log_line_valid = any(re.match(pattern + kind_name, log_line)
+                                 for pattern in self.re_log_valid_messages)
+            assert log_line_valid, "Bad log message format: " + log_line
 
 
 class Test_tiering_config_env(object):
@@ -136,8 +173,9 @@ class Test_tiering_config_env(object):
     cmd_helper = CMD_helper()
 
     def get_cmd_output(self, config_env, log_level="0"):
-        command = " ".join(
-            [self.ld_preload_env, "MEMKIND_MEM_TIERING_LOG_LEVEL=" + log_level, config_env, "ls"])
+        log_level_env = "MEMKIND_MEM_TIERING_LOG_LEVEL=" + log_level
+        command = " ".join([self.ld_preload_env, log_level_env, config_env,
+                            "ls"])
         output, retcode = self.cmd_helper.execute_cmd(command)
 
         assert retcode == 0, \
@@ -149,88 +187,89 @@ class Test_tiering_config_env(object):
         output = self.get_cmd_output(
             "MEMKIND_MEM_TIERING_CONFIG=DRAM:1", log_level="2")
 
-        assert "MEMKIND_MEM_TIERING_LOG_DEBUG: kind_name: DRAM" in output.splitlines(), \
-               "Wrong message"
-        assert "MEMKIND_MEM_TIERING_LOG_DEBUG: ratio_value: 1" in output.splitlines(), \
-               "Wrong message"
+        assert "MEMKIND_MEM_TIERING_LOG_DEBUG: kind_name: DRAM" in \
+            output.splitlines(), "Wrong message"
+        assert "MEMKIND_MEM_TIERING_LOG_DEBUG: ratio_value: 1" in \
+            output.splitlines(), "Wrong message"
 
     def test_FSDAX_only(self):
         output = self.get_cmd_output(
             "MEMKIND_MEM_TIERING_CONFIG=FS_DAX:/tmp/:10G:1", log_level="2")
 
-        assert "MEMKIND_MEM_TIERING_LOG_DEBUG: kind_name: FS_DAX" in output.splitlines(), \
-               "Wrong message"
-        assert "MEMKIND_MEM_TIERING_LOG_DEBUG: pmem_path: /tmp/" in output.splitlines(), \
-               "Wrong message"
-        assert "MEMKIND_MEM_TIERING_LOG_DEBUG: pmem_size: 10G" in output.splitlines(), \
-               "Wrong message"
-        assert "MEMKIND_MEM_TIERING_LOG_DEBUG: ratio_value: 1" in output.splitlines(), \
-               "Wrong message"
+        assert "MEMKIND_MEM_TIERING_LOG_DEBUG: kind_name: FS_DAX" in \
+            output.splitlines(), "Wrong message"
+        assert "MEMKIND_MEM_TIERING_LOG_DEBUG: pmem_path: /tmp/" in \
+            output.splitlines(), "Wrong message"
+        assert "MEMKIND_MEM_TIERING_LOG_DEBUG: pmem_size: 10G" in \
+            output.splitlines(), "Wrong message"
+        assert "MEMKIND_MEM_TIERING_LOG_DEBUG: ratio_value: 1" in \
+            output.splitlines(), "Wrong message"
 
     def test_FSDAX_negative_size(self):
         output = self.get_cmd_output(
             "MEMKIND_MEM_TIERING_CONFIG=FS_DAX:/tmp/:-1:1")
 
-        assert output.splitlines()[0] == \
-            "MEMKIND_MEM_TIERING_LOG_ERROR: Unsupported pmem_size format: -1", "Wrong message"
+        assert output.splitlines()[0] == "MEMKIND_MEM_TIERING_LOG_ERROR: " + \
+            "Unsupported pmem_size format: -1", "Wrong message"
 
     def test_FSDAX_negative_size_min(self):
         output = self.get_cmd_output(
             "MEMKIND_MEM_TIERING_CONFIG=FS_DAX:/tmp/:-9223372036854775808:1")
 
-        assert output.splitlines()[0] == \
-            "MEMKIND_MEM_TIERING_LOG_ERROR: Unsupported pmem_size format: -9223372036854775808", "Wrong message"
+        assert output.splitlines()[0] == "MEMKIND_MEM_TIERING_LOG_ERROR: " + \
+            "Unsupported pmem_size format: -9223372036854775808", \
+            "Wrong message"
 
     def test_FSDAX_wrong_size(self):
         output = self.get_cmd_output(
             "MEMKIND_MEM_TIERING_CONFIG=FS_DAX:/tmp/:as:1")
 
-        assert output.splitlines()[0] == \
-            "MEMKIND_MEM_TIERING_LOG_ERROR: Unsupported pmem_size format: as", "Wrong message"
+        assert output.splitlines()[0] == "MEMKIND_MEM_TIERING_LOG_ERROR: " + \
+            "Unsupported pmem_size format: as", "Wrong message"
 
     def test_FSDAX_negative_ratio(self):
         output = self.get_cmd_output(
             "MEMKIND_MEM_TIERING_CONFIG=FS_DAX:/tmp/:10G:-1")
 
-        assert output.splitlines()[0] == \
-            "MEMKIND_MEM_TIERING_LOG_ERROR: Unsupported ratio: -1", "Wrong message"
+        assert output.splitlines()[0] == "MEMKIND_MEM_TIERING_LOG_ERROR: " + \
+            "Unsupported ratio: -1", "Wrong message"
 
     def test_FSDAX_wrong_ratio(self):
         output = self.get_cmd_output(
             "MEMKIND_MEM_TIERING_CONFIG=FS_DAX:/tmp/:10G:a")
 
-        assert output.splitlines()[0] == \
-            "MEMKIND_MEM_TIERING_LOG_ERROR: Unsupported ratio: a", "Wrong message"
+        assert output.splitlines()[0] == "MEMKIND_MEM_TIERING_LOG_ERROR: " + \
+            "Unsupported ratio: a", "Wrong message"
 
     def test_class_not_defined(self):
         output = self.get_cmd_output("MEMKIND_MEM_TIERING_CONFIG=2")
 
-        assert output.splitlines()[0] == \
-            "MEMKIND_MEM_TIERING_LOG_ERROR: Unsupported kind: 2", "Wrong message"
+        assert output.splitlines()[0] == "MEMKIND_MEM_TIERING_LOG_ERROR: " + \
+            "Unsupported kind: 2", "Wrong message"
 
     def test_bad_ratio(self):
         output = self.get_cmd_output("MEMKIND_MEM_TIERING_CONFIG=DRAM:A")
 
-        assert output.splitlines()[0] == \
-            "MEMKIND_MEM_TIERING_LOG_ERROR: Unsupported ratio: A", "Wrong message"
+        assert output.splitlines()[0] == "MEMKIND_MEM_TIERING_LOG_ERROR: " + \
+            "Unsupported ratio: A", "Wrong message"
 
     def test_no_ratio(self):
         output = self.get_cmd_output("MEMKIND_MEM_TIERING_CONFIG=DRAM")
 
-        assert output.splitlines()[0] == \
-            "MEMKIND_MEM_TIERING_LOG_ERROR: Ratio not provided", "Wrong message"
+        assert output.splitlines()[0] == "MEMKIND_MEM_TIERING_LOG_ERROR: " + \
+            "Ratio not provided", "Wrong message"
 
     def test_bad_class(self):
         output = self.get_cmd_output("MEMKIND_MEM_TIERING_CONFIG=a1b2:10")
 
-        assert output.splitlines()[0] == \
-            "MEMKIND_MEM_TIERING_LOG_ERROR: Unsupported kind: a1b2", "Wrong message"
+        assert output.splitlines()[0] == "MEMKIND_MEM_TIERING_LOG_ERROR: " + \
+            "Unsupported kind: a1b2", "Wrong message"
 
     def test_negative_ratio(self):
         output = self.get_cmd_output("MEMKIND_MEM_TIERING_CONFIG=DRAM:-1")
 
-        assert output.splitlines()[0] == \
-            "MEMKIND_MEM_TIERING_LOG_ERROR: Unsupported ratio: -1", "Wrong message"
+        assert output.splitlines()[0] == "MEMKIND_MEM_TIERING_LOG_ERROR: " + \
+            "Unsupported ratio: -1", "Wrong message"
 
     @pytest.mark.parametrize("config_str", ["", ",", ",,,"])
     def test_negative_config_str_invalid(self, config_str):
@@ -246,6 +285,5 @@ class Test_tiering_config_env(object):
         output = self.get_cmd_output(
             "MEMKIND_MEM_TIERING_CONFIG=" + query_str)
 
-        assert output.splitlines()[0] == \
-            "MEMKIND_MEM_TIERING_LOG_ERROR: Kind name string not found in: " + \
-            query_str, "Wrong message"
+        assert output.splitlines()[0] == "MEMKIND_MEM_TIERING_LOG_ERROR: " + \
+            "Kind name string not found in: " + query_str, "Wrong message"
