b10b3c5de947990ff62ebd7d27c69cdfbde9b6a8
Patryk Kaminski <patryk.kaminski@intel.com>
2021-03-05 09:49:14 +0100

    Validate pmem size in MEMKIND_MEM_TIERING_CONFIG env var

--- tiering/ctl.c
+++ tiering/ctl.c
@@ -6,12 +6,16 @@
 #include <errno.h>
 #include <limits.h>
 #include <regex.h>
-#include <stddef.h>
 #include <stdlib.h>
 #include <string.h>
 
 #define CTL_VALUE_SEPARATOR        ":"
 #define CTL_STRING_QUERY_SEPARATOR ","
+
+#define KB       (1024)
+#define MB       (1024 * 1024)
+#define GB       (1024 * 1024 * 1024)
+#define SIZE_MAX ((size_t)-1)
 
 /*
  * ctl_parse_u -- (internal) parses and returns an unsigned integer
@@ -37,6 +41,30 @@ static int ctl_parse_u(const char *str, unsigned *dest)
     return 0;
 }
 
+/*
+ * ctl_parse_ull -- (internal) parses and returns an unsigned long long
+ */
+static int ctl_parse_ull(const char *str, unsigned long long *dest)
+{
+    if (str[0] == '-') {
+        return -1;
+    }
+
+    char *endptr;
+    int olderrno = errno;
+    errno = 0;
+    unsigned long long val_ul = strtoull(str, &endptr, 0);
+
+    if (endptr == str || errno != 0) {
+        errno = olderrno;
+        return -1;
+    }
+
+    errno = olderrno;
+    *dest = val_ul;
+    return 0;
+}
+
 static int ctl_validate_kind_name(const char *kind_name)
 {
     if (strcmp(kind_name, "DRAM") && strcmp(kind_name, "FS_DAX")) {
@@ -47,24 +75,48 @@ static int ctl_validate_kind_name(const char *kind_name)
     return 0;
 }
 
-// TODO: Parse pmem_size string to unsigned long long supporting kMGT suffixes
-static int ctl_validate_pmem_size(const char *pmem_size)
+static int ctl_parse_pmem_size(const char *pmem_size_str, size_t *pmem_size)
 {
-    if (pmem_size == NULL) {
+    if (pmem_size_str == NULL) {
         log_err("PMEM size not provided");
         return -1;
     }
 
     regex_t regex;
-    int ret = regcomp(&regex, "[[:digit:]+]G", 0);
+    unsigned long long parsed_size;
+
+    int ret = regcomp(&regex, "^[[:digit:]]+[kMG]?$", REG_EXTENDED);
     if (ret) {
         log_err(
             "Unsuccessful compilation of regex occurred during validation of PMEM size");
         return -1;
     }
-    int regex_ret = regexec(&regex, pmem_size, 0, NULL, 0);
+    int regex_ret = regexec(&regex, pmem_size_str, 0, NULL, 0);
     if (regex_ret == REG_NOMATCH) {
-        log_err("Unsupported pmem_size format: %s", pmem_size);
+        log_err("Unsupported pmem size: %s", pmem_size_str);
+        return -1;
+    }
+
+    ret = ctl_parse_ull(pmem_size_str, &parsed_size);
+    if (ret != 0) {
+        log_err("Unsupported pmem size: %s", pmem_size_str);
+        return -1;
+    }
+
+    if (strchr(pmem_size_str, 'k') != NULL && parsed_size != 0 &&
+        SIZE_MAX / KB >= parsed_size) {
+        *pmem_size = (size_t)parsed_size * KB;
+    } else if (strchr(pmem_size_str, 'M') != NULL && parsed_size != 0 &&
+               SIZE_MAX / MB >= parsed_size) {
+        *pmem_size = (size_t)parsed_size * MB;
+    } else if (strchr(pmem_size_str, 'G') != NULL && parsed_size != 0 &&
+               SIZE_MAX / GB >= parsed_size) {
+        *pmem_size = (size_t)parsed_size * GB;
+    } else if (parsed_size <= SIZE_MAX &&
+               strpbrk(pmem_size_str, "kMG") == NULL) {
+        *pmem_size = (size_t)parsed_size;
+    } else {
+        log_err("Provided pmem size is too big: %s", pmem_size_str);
         return -1;
     }
 
@@ -93,7 +145,7 @@ static int ctl_parse_ratio(const char *ratio_str, unsigned *dest)
  * into single queries
  */
 static int ctl_parse_query(char *qbuf, char **kind_name, char **pmem_path,
-                           char **pmem_size, unsigned *ratio_value)
+                           size_t *pmem_size_value, unsigned *ratio_value)
 {
     char *sptr = NULL;
     *kind_name = strtok_r(qbuf, CTL_VALUE_SEPARATOR, &sptr);
@@ -106,16 +158,14 @@ static int ctl_parse_query(char *qbuf, char **kind_name, char **pmem_path,
         return -1;
     }
 
+    char *pmem_size_str = NULL;
     if (!strcmp(*kind_name, "FS_DAX")) {
         *pmem_path = strtok_r(NULL, CTL_VALUE_SEPARATOR, &sptr);
-        *pmem_size = strtok_r(NULL, CTL_VALUE_SEPARATOR, &sptr);
-        ret = ctl_validate_pmem_size(*pmem_size);
+        pmem_size_str = strtok_r(NULL, CTL_VALUE_SEPARATOR, &sptr);
+        ret = ctl_parse_pmem_size(pmem_size_str, pmem_size_value);
         if (ret != 0) {
             return -1;
         }
-    } else {
-        pmem_path = NULL;
-        pmem_size = NULL;
     }
 
     char *ratio_value_str = NULL;
@@ -138,7 +188,7 @@ static int ctl_parse_query(char *qbuf, char **kind_name, char **pmem_path,
  * ctl_load_config -- splits an entire config into query strings
  */
 int ctl_load_config(char *buf, char **kind_name, char **pmem_path,
-                    char **pmem_size, unsigned *ratio_value)
+                    size_t *pmem_size, unsigned *ratio_value)
 {
     int ret = 0;
     char *sptr = NULL;
