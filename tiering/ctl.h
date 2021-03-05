6fab54d24563f2a7f5a5737e0607b6f5931d3bbd
Patryk Kaminski <patryk.kaminski@intel.com>
2021-03-05 09:49:14 +0100

    Validate pmem size in MEMKIND_MEM_TIERING_CONFIG env var

--- tiering/ctl.h
+++ tiering/ctl.h
@@ -2,12 +2,13 @@
 /* Copyright (C) 2021 Intel Corporation. */
 
 #pragma once
+#include <stddef.h>
 #ifdef __cplusplus
 extern "C" {
 #endif
 
 int ctl_load_config(char *buf, char **kind_name, char **pmem_path,
-                    char **pmem_size, unsigned *ratio_value);
+                    size_t *pmem_size, unsigned *ratio_value);
 
 #ifdef __cplusplus
 }
