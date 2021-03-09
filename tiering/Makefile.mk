cde83517ba41893f72f2603df2c5dbcc5cf9cbec
Rafal Rudnicki <rafal.rudnicki@intel.com>
2021-03-09 12:28:08 +0000

    Handle DRAM:1 scenario in libmemtier

--- tiering/Makefile.mk
+++ tiering/Makefile.mk
@@ -6,6 +6,8 @@ lib_LTLIBRARIES += tiering/libmemtier.la \
 
 tiering_libmemtier_la_SOURCES = tiering/ctl.c tiering/memtier.c tiering/memtier_log.c
 
+tiering_libmemtier_la_LIBADD = libmemkind.la
+
 clean-local: tiering-clean
 
 tiering-clean:
