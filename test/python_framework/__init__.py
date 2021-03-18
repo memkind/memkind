5e55159ba48a67623fb4ff1d83920a64919e1ded
Patryk Kaminski <patryk.kaminski@intel.com>
2021-03-18 11:25:38 +0100

    Add python linter to GitHub Actions

--- test/python_framework/__init__.py
+++ test/python_framework/__init__.py
@@ -1,5 +1,5 @@
 # SPDX-License-Identifier: BSD-2-Clause
-# Copyright (C) 2016 - 2020 Intel Corporation.
+# Copyright (C) 2016 - 2021 Intel Corporation.
 
 from python_framework.cmd_helper import CMD_helper
 from python_framework.huge_page_organizer import Huge_page_organizer
