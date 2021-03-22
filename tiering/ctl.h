ed0633d4bed89146a9ef1dc91c5d63e4629840e3
Patryk Kaminski <patryk.kaminski@intel.com>
2021-03-22 11:52:22 +0100

    Add tier_draft struct
    Represent arguments in functions related to parsing environment
    variable in a more concise way.

--- tiering/ctl.h
+++ tiering/ctl.h
@@ -2,9 +2,20 @@
 /* Copyright (C) 2021 Intel Corporation. */
 
 #pragma once
+#include <memkind_memtier.h>
+
+#include <stddef.h>
+
 #ifdef __cplusplus
 extern "C" {
 #endif
+
+typedef struct tier_draft {
+    char *kind_name;
+    char *pmem_path;
+    size_t pmem_size;
+    unsigned ratio_value;
+} tier_draft;
 
 struct memtier_kind *ctl_create_tier_kind_from_env(char *env_var_string);
 void ctl_destroy_kind(struct memtier_kind *kind);
