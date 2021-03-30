d43701ec5506967d6413acfa469d3f895ddb3221
Jacek Grzybek <jacek.grzybek@intel.com>
2021-03-30 08:23:03 +0000

    Increasing the readability of the code according to the linter-flake8 guidelines

--- test/dax_kmem_env_var_test.py
+++ test/dax_kmem_env_var_test.py
@@ -5,43 +5,69 @@ from distutils.spawn import find_executable
 from python_framework import CMD_helper
 import os
 
+
 class Test_dax_kmem_env_var(object):
-    os.environ["PATH"] += os.pathsep + os.path.dirname(os.path.dirname(__file__))
+    os.environ["PATH"] += (os.pathsep
+                           + os.path.dirname(os.path.dirname(__file__)))
     binary_path = find_executable("memkind-auto-dax-kmem-nodes")
     environ_err_test = "../environ_err_dax_kmem_malloc_test"
     environ_err_positive_test = "../environ_err_dax_kmem_malloc_positive_test"
-    expected_libnuma_warning = "libnuma: Warning: node argument -1 is out of range\n\n"
+    expected_libnuma_warning = (
+        "libnuma: Warning: node argument -1 is out of range\n\n")
     fail_msg = "Test failed with:\n {0}"
     cmd_helper = CMD_helper()
 
     def get_dax_kmem_nodes(self, nodemask=None):
-        """ This function executes memkind function 'get_mbind_nodemask' and returns its output - comma-separated DAX_KMEM nodes """
+        """ This function executes memkind function 'get_mbind_nodemask'
+            and returns its output - comma-separated DAX_KMEM nodes """
         command = self.binary_path
         if (nodemask):
             command = "MEMKIND_DAX_KMEM_NODES={}".format(nodemask) + command
         output, retcode = self.cmd_helper.execute_cmd(command)
-        assert retcode == 0, self.fail_msg.format("\nError: Execution of \'{0}\' returns {1}, \noutput: {2}".format(command, retcode, output))
+        assert retcode == 0, self.fail_msg.format(
+            "\nError: Execution of \'{0}\' returns {1}, \noutput: {2}"
+            .format(command, retcode, output))
         print("\nExecution of {} returns output {}".format(command, output))
         return output
 
     def test_TC_MEMKIND_dax_kmem_env_var_compare_nodemask_default_and_env_variable(self):
-        """ This test checks whether dax_kmem_nodemask_default and dax_kmem_nodemask_env_variable have the same value """
+        """ This test checks whether dax_kmem_nodemask_default and
+            dax_kmem_nodemask_env_variable have the same value """
         dax_kmem_nodemask_default = self.get_dax_kmem_nodes()
-        dax_kmem_nodemask_env_variable = self.get_dax_kmem_nodes(dax_kmem_nodemask_default)
-        assert dax_kmem_nodemask_default == dax_kmem_nodemask_env_variable, self.fail_msg.format("Error: Nodemask dax_kmem_nodemask_default ({0}) " \
-               "is not the same as nodemask dax_kmem_nodemask_env_variable ({1})".format(dax_kmem_nodemask_default, dax_kmem_nodemask_env_variable))
+        dax_kmem_nodemask_env_variable = self.get_dax_kmem_nodes(
+            dax_kmem_nodemask_default)
+        assert dax_kmem_nodemask_default == (
+            dax_kmem_nodemask_env_variable,
+            self.fail_msg.format(
+                "Error: Nodemask dax_kmem_nodemask_default ({0}) ",
+                "is not the same as nodemask",
+                " dax_kmem_nodemask_env_variable ({1})".format(
+                    dax_kmem_nodemask_default,
+                    dax_kmem_nodemask_env_variable)))
 
     def test_TC_MEMKIND_dax_kmem_env_var_negative_memkind_malloc(self):
-        """ This test sets unsupported value of MEMKIND_DAX_KMEM_NODES, then tries to perform a successful allocation from DRAM using memkind_malloc() """
-        command = "MEMKIND_DAX_KMEM_NODES=-1 " + self.cmd_helper.get_command_path(self.environ_err_test)
+        """ This test sets unsupported value of MEMKIND_DAX_KMEM_NODES,
+            then tries to perform a successful allocation
+            from DRAM using memkind_malloc() """
+        command = ("MEMKIND_DAX_KMEM_NODES=-1 "
+                   + self.cmd_helper.get_command_path(self.environ_err_test))
         output, retcode = self.cmd_helper.execute_cmd(command)
-        assert retcode != 0, self.fail_msg.format("\nError: Execution of: \'{0}\' returns: {1} \noutput: {2}".format(command, retcode, output))
-        assert self.expected_libnuma_warning == output, self.fail_msg.format("Error: expected libnuma warning ({0}) " \
-               "was not found (output: {1})").format(self.expected_libnuma_warning, output)
+        assert retcode != 0, self.fail_msg.format(
+            "\nError: Execution of: \'{0}\' returns: {1} \noutput: {2}"
+            .format(command, retcode, output))
+        assert self.expected_libnuma_warning == output, self.fail_msg.format(
+            "Error: expected libnuma warning ({0}) ",
+            "was not found (output: {1})").format(
+                self.expected_libnuma_warning, output)
 
     def test_TC_MEMKIND_dax_kmem_env_var_proper_memkind_malloc(self):
-        """ This test checks if allocation is performed on persistent memory correctly """
+        """ This test checks if allocation is performed
+            on persistent memory correctly """
         dax_kmem_nodemask_default = self.get_dax_kmem_nodes().strip()
-        command = "MEMKIND_DAX_KMEM_NODES={} ".format(dax_kmem_nodemask_default) + self.cmd_helper.get_command_path(self.environ_err_positive_test)
+        command = "MEMKIND_DAX_KMEM_NODES={} ".format(
+            dax_kmem_nodemask_default) + self.cmd_helper.get_command_path(
+                self.environ_err_positive_test)
         output, retcode = self.cmd_helper.execute_cmd(command)
-        assert retcode == 0, self.fail_msg.format("\nError: Execution of: \'{0}\' returns: {1} \noutput: {2}".format(command, retcode, output))
+        assert retcode == 0, self.fail_msg.format(
+            "\nError: Execution of: \'{0}\' returns: {1} \noutput: {2}"
+            .format(command, retcode, output))
--- test/hbw_detection_test.py
+++ test/hbw_detection_test.py
@@ -1,40 +1,57 @@
 # SPDX-License-Identifier: BSD-2-Clause
-# Copyright (C) 2017 - 2020 Intel Corporation.
+# Copyright (C) 2017 - 2021 Intel Corporation.
 
 from distutils.spawn import find_executable
 from python_framework import CMD_helper
 import os
 
+
 class Test_hbw_detection(object):
-    os.environ["PATH"] += os.pathsep + os.path.dirname(os.path.dirname(__file__))
+    os.environ["PATH"] += os.pathsep + os.path.dirname(
+        os.path.dirname(__file__))
     binary_path = find_executable("memkind-hbw-nodes")
     environ_err_test = "../environ_err_hbw_malloc_test"
-    expected_libnuma_warning = "libnuma: Warning: node argument -1 is out of range\n\n"
+    expected_libnuma_warning = ("libnuma: Warning: node",
+                                " argument -1 is out of range\n\n")
     fail_msg = "Test failed with:\n {0}"
     cmd_helper = CMD_helper()
 
     def get_hbw_nodes(self, nodemask=None):
-        """ This function executes memkind function 'get_mbind_nodemask' and returns its output - comma-separated HBW nodes """
+        """ This function executes memkind function 'get_mbind_nodemask'
+            and returns its output - comma-separated HBW nodes """
         command = self.binary_path
         if (nodemask):
             command = "MEMKIND_HBW_NODES={}".format(nodemask) + command
         output, retcode = self.cmd_helper.execute_cmd(command, sudo=False)
-        assert retcode == 0, self.fail_msg.format("\nError: Execution of \'{0}\' returns {1}, \noutput: {2}".format(command, retcode, output))
+        assert retcode == 0, self.fail_msg.format(
+            "\nError: Execution of \'{0}\' returns {1},",
+            " \noutput: {2}".format(command, retcode, output))
         print("\nExecution of {} returns output {}".format(command, output))
         return output
 
     def test_TC_MEMKIND_hbw_detection_compare_nodemask_default_and_env_variable(self):
-        """ This test checks whether hbw_nodemask_default and hbw_nodemask_env_variable has the same value """
+        """ This test checks whether hbw_nodemask_default
+            and hbw_nodemask_env_variable has the same value """
         hbw_nodemask_default = self.get_hbw_nodes()
         hbw_nodemask_env_variable = self.get_hbw_nodes(hbw_nodemask_default)
-        assert hbw_nodemask_default == hbw_nodemask_env_variable, self.fail_msg.format("Error: Nodemask hbw_nodemask_default ({0}) " \
-               "is not the same as nodemask hbw_nodemask_env_variable ({1})".format(hbw_nodemask_default, hbw_nodemask_env_variable))
+        assert hbw_nodemask_default == (
+            hbw_nodemask_env_variable, self.fail_msg.format(
+                "Error: Nodemask hbw_nodemask_default ({0}) ",
+                "is not the same as nodemask",
+                " hbw_nodemask_env_variable ({1})".format(
+                    hbw_nodemask_default, hbw_nodemask_env_variable)))
 
     def test_TC_MEMKIND_hbw_detection_negative_hbw_malloc(self):
-        """ This test sets unsupported value of MEMKIND_HBW_NODES, then try to perform a successful allocation from DRAM using hbw_malloc()
-        thanks to default HBW_POLICY_PREFERRED policy """
-        command = "MEMKIND_HBW_NODES=-1 " + self.cmd_helper.get_command_path(self.environ_err_test)
+        """ This test sets unsupported value of MEMKIND_HBW_NODES,
+            then try to perform a successful allocation from DRAM using
+            hbw_malloc() thanks to default HBW_POLICY_PREFERRED policy """
+        command = "MEMKIND_HBW_NODES=-1 " \
+            + self.cmd_helper.get_command_path(self.environ_err_test)
         output, retcode = self.cmd_helper.execute_cmd(command, sudo=False)
-        assert retcode != 0, self.fail_msg.format("\nError: Execution of: \'{0}\' returns: {1} \noutput: {2}".format(command, retcode, output))
-        assert self.expected_libnuma_warning == output, self.fail_msg.format("Error: expected libnuma warning ({0}) " \
-               "was not found (output: {1})").format(self.expected_libnuma_warning, output)
+        assert retcode != 0, self.fail_msg.format(
+            "\nError: Execution of: \'{0}\' returns: {1} \noutput: {2}".format(
+                command, retcode, output))
+        assert self.expected_libnuma_warning == output, self.fail_msg.format(
+            "Error: expected libnuma warning ({0}) ",
+            "was not found (output: {1})").format(
+                self.expected_libnuma_warning, output)
