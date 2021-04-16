741995f45b79b04a2f245542fd063be9bcbd1cd5
michalbiesek <michal.biesek@intel.com>
2021-04-16 15:47:12 +0200

    Executable to measure counter impact

---
+++ utils/memtier_counter_bench/run_perf.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# SPDX-License-Identifier: BSD-2-Clause
+# Copyright (C) 2021 Intel Corporation.
+
+set -e
+
+PERF_CMD="perf stat -r 5 -B"
+NUMA_CMD="numactl -N0"
+MEMKIND_BIN="./memtier_counter_bench -m"
+MEMTIER_KIND_BIN="./memtier_counter_bench -k"
+MEMTIER_BIN="./memtier_counter_bench -r"
+MEMTIER_MULTIPLE_BIN="./memtier_counter_bench -p"
+THREADS=(1 2 4 8 16 25 32 64)
+
+for thread in ${THREADS[*]}
+do
+    $PERF_CMD $MEMKIND_BIN -t "$thread"
+    $PERF_CMD $MEMTIER_KIND_BIN -t "$thread"
+    $PERF_CMD $MEMTIER_BIN -t "$thread"
+    $PERF_CMD $MEMTIER_MULTIPLE_BIN -t "$thread"
+done
+
+echo "NUMA test with CPU binding"
+
+for thread in ${THREADS[*]}
+do
+    $NUMA_CMD $PERF_CMD $MEMKIND_BIN -t "$thread"
+    $NUMA_CMD $PERF_CMD $MEMTIER_KIND_BIN -t "$thread"
+    $NUMA_CMD $PERF_CMD $MEMTIER_BIN -t "$thread"
+    $NUMA_CMD $PERF_CMD $MEMTIER_MULTIPLE_BIN -t "$thread"
+done
