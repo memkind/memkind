59dc5af0b7d547162ac0576715e9bad2369288dc
michalbiesek <michal.biesek@intel.com>
2021-06-21 14:34:39 +0200

    Provide decorators for memkind_memtier layer
    - enabled by ./configure --enable-decorators and MEMKIND_MEM_TIERING_LOG_LEVEL=1

--- tiering/memtier.c
+++ tiering/memtier.c
@@ -28,77 +28,106 @@
 #define mt_posix_memalign     MT_SYMBOL(posix_memalign)
 #define mt_malloc_usable_size MT_SYMBOL(malloc_usable_size)
 
+#ifdef MEMKIND_DECORATION_ENABLED
+#include <memkind/internal/memkind_private.h>
+MEMTIER_EXPORT void memtier_kind_malloc_post(struct memkind *kind, size_t size,
+                                             void **result)
+{
+    log_info("kind: %s, malloc:(%zu) = %p", kind->name, size, *result);
+}
+
+MEMTIER_EXPORT void memtier_kind_calloc_post(memkind_t kind, size_t num,
+                                             size_t size, void **result)
+{
+    log_info("kind: %s, calloc:(%zu, %zu) = %p", kind->name, num, size,
+             *result);
+}
+
+MEMTIER_EXPORT void memtier_kind_realloc_post(struct memkind *kind, void *ptr,
+                                              size_t size, void **result)
+{
+    log_info("kind: %s, realloc(%p, %zu) = %p", kind->name, ptr, size, *result);
+}
+
+MEMTIER_EXPORT void memtier_kind_posix_memalign_post(memkind_t kind,
+                                                     void **memptr,
+                                                     size_t alignment,
+                                                     size_t size, int *err)
+{
+    log_info("kind: %s, posix_memalign(%p, %zu, %zu) = %d", kind->name, *memptr,
+             alignment, size, err);
+}
+
+MEMTIER_EXPORT void memtier_kind_free_pre(void **ptr)
+{
+    struct memkind *kind = memkind_detect_kind(*ptr);
+    if (kind)
+        log_info("kind: %s, free(%p)", kind->name, *ptr);
+    else
+        log_info("free(%p)", *ptr);
+}
+
+MEMTIER_EXPORT void memtier_kind_usable_size_post(void **ptr, size_t size)
+{
+    struct memkind *kind = memkind_detect_kind(*ptr);
+    if (kind)
+        log_info("kind: %s, malloc_usable_size(%p) = %zu", kind->name, *ptr,
+                 size);
+    else
+        log_info("malloc_usable_size(%p) = %zu", *ptr, size);
+}
+#endif
+
 static int destructed;
 
 static struct memtier_memory *current_memory;
 
 MEMTIER_EXPORT void *malloc(size_t size)
 {
-    void *ret = NULL;
-
     if (MEMTIER_LIKELY(current_memory)) {
-        ret = memtier_malloc(current_memory, size);
+        return memtier_malloc(current_memory, size);
     } else if (destructed == 0) {
-        ret = memkind_malloc(MEMKIND_DEFAULT, size);
+        return memkind_malloc(MEMKIND_DEFAULT, size);
     }
-
-    // TODO after implementation of decorators memtier decorators this
-    // logging call will be obsolete
-    log_debug("malloc(%zu) = %p", size, ret);
-    return ret;
+    return NULL;
 }
 
 MEMTIER_EXPORT void *calloc(size_t num, size_t size)
 {
-    void *ret = NULL;
-
     if (MEMTIER_LIKELY(current_memory)) {
-        ret = memtier_calloc(current_memory, num, size);
+        return memtier_calloc(current_memory, num, size);
     } else if (destructed == 0) {
-        ret = memkind_calloc(MEMKIND_DEFAULT, num, size);
+        return memkind_calloc(MEMKIND_DEFAULT, num, size);
     }
-
-    log_debug("calloc(%zu, %zu) = %p", num, size, ret);
-    return ret;
+    return NULL;
 }
 
 MEMTIER_EXPORT void *realloc(void *ptr, size_t size)
 {
-    void *ret = NULL;
-
     if (MEMTIER_LIKELY(current_memory)) {
-        ret = memtier_realloc(current_memory, ptr, size);
+        return memtier_realloc(current_memory, ptr, size);
     } else if (destructed == 0) {
-        ret = memkind_realloc(MEMKIND_DEFAULT, ptr, size);
+        return memkind_realloc(MEMKIND_DEFAULT, ptr, size);
     }
-
-    log_debug("realloc(%p, %zu) = %p", ptr, size, ret);
-    return ret;
+    return NULL;
 }
 
 // clang-format off
 MEMTIER_EXPORT int posix_memalign(void **memptr, size_t alignment, size_t size)
 {
-    int ret = 0;
-
     if (MEMTIER_LIKELY(current_memory)) {
-        ret = memtier_posix_memalign(current_memory, memptr, alignment,
+        return memtier_posix_memalign(current_memory, memptr, alignment,
                                           size);
     } else if (destructed == 0) {
-        ret = memkind_posix_memalign(MEMKIND_DEFAULT, memptr, alignment,
+        return memkind_posix_memalign(MEMKIND_DEFAULT, memptr, alignment,
                                      size);
     }
-
-    log_debug("posix_memalign(%p, %zu, %zu) = %d",
-              memptr, alignment, size, ret);
-    return ret;
+    return 0;
 }
 // clang-format on
 
 MEMTIER_EXPORT void free(void *ptr)
 {
-    log_debug("free(%p)", ptr);
-
     if (MEMTIER_LIKELY(current_memory)) {
         memtier_realloc(current_memory, ptr, 0);
     } else if (destructed == 0) {
@@ -108,8 +137,6 @@ MEMTIER_EXPORT void free(void *ptr)
 
 MEMTIER_EXPORT size_t malloc_usable_size(void *ptr)
 {
-    log_debug("malloc_usable_size(%p)", ptr);
-
     return memtier_usable_size(ptr);
 }
 
