6df94b2ba59c33849ac4bb5e5d3d6b7765669825
Patryk Kaminski <patryk.kaminski@intel.com>
2021-09-07 13:44:02 +0200

    Add memkind_get_capacity() tests

--- test/memkind_hmat_tests.cpp
+++ test/memkind_hmat_tests.cpp
@@ -2,6 +2,7 @@
 /* Copyright (C) 2020 - 2021 Intel Corporation. */
 
 #include <memkind.h>
+#include <memkind/internal/memkind_private.h>
 #include <memory>
 #include <numa.h>
 #include <omp.h>
@@ -17,6 +18,8 @@ class MemkindHMATFunctionalTestsParam: public ::Memkind_Param_Test
 protected:
     const char *memory_tpg;
     TestPrereq tp;
+    int threads_num = get_nprocs();
+
     void SetUp()
     {
         memory_tpg = std::getenv("MEMKIND_TEST_TOPOLOGY");
@@ -43,7 +46,6 @@ TEST_P(MemkindHMATFunctionalTestsParam,
     ASSERT_EQ(status, 0);
     // use big size to ensure that we call jemalloc extent
     const size_t size = 11 * MB - 5;
-    int threads_num = get_nprocs();
     auto topology = TopologyFactory(memory_tpg, memory_kind);
 #pragma omp parallel for num_threads(threads_num)
     for (int thread_id = 0; thread_id < threads_num; ++thread_id) {
@@ -64,5 +66,46 @@ TEST_P(MemkindHMATFunctionalTestsParam,
         } else {
             EXPECT_TRUE(ptr == nullptr);
         }
+    }
+}
+
+class MemkindHMATGetCapacityTestsParam: public ::MemkindHMATFunctionalTestsParam
+{
+};
+
+INSTANTIATE_TEST_CASE_P(KindParam, MemkindHMATGetCapacityTestsParam,
+                        ::testing::Values(MEMKIND_HBW, MEMKIND_HBW_ALL,
+                                          MEMKIND_HIGHEST_CAPACITY_LOCAL,
+                                          MEMKIND_LOWEST_LATENCY_LOCAL,
+                                          MEMKIND_HIGHEST_BANDWIDTH_LOCAL));
+
+TEST_P(MemkindHMATGetCapacityTestsParam,
+       test_tc_memkind_HMAT_verify_get_capacity)
+{
+    int status = numa_available();
+    ASSERT_EQ(status, 0);
+    auto topology = TopologyFactory(memory_tpg, memory_kind);
+    if (!topology->is_kind_supported()) {
+        GTEST_SKIP() << "Kind " << memory_kind->name
+                     << " is unsupported with topology " << memory_tpg << " ."
+                     << std::endl;
+    }
+
+#pragma omp parallel for num_threads(threads_num)
+    for (int thread_id = 0; thread_id < threads_num; ++thread_id) {
+        cpu_set_t cpu_set;
+        CPU_ZERO(&cpu_set);
+        CPU_SET(thread_id, &cpu_set);
+        status = sched_setaffinity(0, sizeof(cpu_set_t), &cpu_set);
+        int cpu = sched_getcpu();
+        EXPECT_EQ(thread_id, cpu);
+
+        int init_node = numa_node_of_cpu(cpu);
+        auto numa_nodes = topology->get_target_nodes(init_node);
+
+        ssize_t capacity = tp.get_capacity(numa_nodes);
+        EXPECT_GT(capacity, 0);
+
+        EXPECT_EQ(capacity, memkind_get_capacity(memory_kind));
     }
 }
