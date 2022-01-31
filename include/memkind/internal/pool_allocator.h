c9eed2ba3b68bb4679cbfd3d0a91b6562ae056b4
Maciej Paczocha <maciej.paczocha@intel.com>
2022-01-31 19:14:38 +0100

    Add pool allocator

---
+++ include/memkind/internal/pool_allocator.h
@@ -0,0 +1,43 @@
+// SPDX-License-Identifier: BSD-2-Clause
+/* Copyright (C) 2022 Intel Corporation. */
+
+#pragma once
+
+#include "memkind/internal/slab_allocator.h"
+
+#include "stddef.h"
+#include "stdint.h"
+
+#ifdef __cplusplus
+#include <atomic>
+// HACK
+#define _Atomic(x) std::atomic<x>
+#define atomic_slab_alloc_ptr_t _Atomic(slab_alloc_t *)
+extern "C" {
+#else
+
+#if defined(HAVE_STDATOMIC_H) || true
+#include <stdatomic.h>
+#define MEMKIND_ATOMIC _Atomic
+#else
+#define MEMKIND_ATOMIC
+#endif
+#define atomic_slab_alloc_ptr_t MEMKIND_ATOMIC slab_alloc_t *
+#endif
+
+typedef struct PoolAllocator {
+    atomic_slab_alloc_ptr_t pool[UINT16_MAX];
+    slab_alloc_t slabSlabAllocator;
+} PoolAllocator;
+
+extern int pool_allocator_create(PoolAllocator *pool);
+extern void pool_allocator_destroy(PoolAllocator *pool);
+
+extern void *pool_allocator_malloc(PoolAllocator *pool, size_t size);
+extern void *pool_allocator_realloc(PoolAllocator *pool, void *ptr,
+                                    size_t size);
+extern void pool_allocator_free(void *ptr);
+
+#ifdef __cplusplus
+}
+#endif
