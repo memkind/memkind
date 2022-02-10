618b572605937ff19ddc3cdb7eddf78a70c6a49d
Maciej Paczocha <maciej.paczocha@intel.com>
2022-02-10 18:22:23 +0100

    Add support for pool allocator to zipf counter_bench

--- utils/memtier_zipf_bench/memtier_zipf_counter_bench.cpp
+++ utils/memtier_zipf_bench/memtier_zipf_counter_bench.cpp
@@ -37,6 +37,32 @@ enum class TestCase
     SEQ_COPY
 };
 
+enum class RawAllocator
+{
+    /// PoolAllocator
+    POOL,
+};
+
+struct GeneralPolicy {
+    enum class GeneralType
+    {
+        /// Use memtier_policy_t
+        Memtier,
+        /// Use RawAllocator
+        RawAllocator
+    } generalType;
+    union {
+        memtier_policy_t policy;
+        RawAllocator allocator;
+    };
+};
+
+enum NonAsciiKeys
+{
+    KEY_NOF_ALLOCATIONS = 0x100,
+    KEY_ALLOCATION_SIZE = 0x101,
+};
+
 static uint64_t clock_bench_time_ms()
 {
     struct timespec tv;
@@ -53,13 +79,13 @@ public:
     virtual void free(void *ptr) = 0;
 };
 
-class StaticRatioAllocator: public Allocator
+class MemtierAllocator: public Allocator
 {
     std::shared_ptr<memtier_memory> memory;
 
 public:
-    StaticRatioAllocator(memtier_policy_t policy,
-                         std::pair<size_t, size_t> dram_pmem_ratio)
+    MemtierAllocator(memtier_policy_t policy,
+                     std::pair<size_t, size_t> dram_pmem_ratio)
     {
         struct memtier_builder *m_tier_builder = memtier_builder_new(policy);
         memtier_builder_add_tier(m_tier_builder, MEMKIND_DEFAULT,
@@ -124,7 +150,7 @@ struct RunInfo {
     size_t types_count;
     size_t iterations_major;
     size_t iterations_minor;
-    memtier_policy_t policy;
+    GeneralPolicy policy;
     std::pair<size_t, size_t> dram_pmem_ratio;
 };
 
@@ -143,7 +169,6 @@ class Fence
     size_t readyClients = 0;
     size_t nofClients;
     std::condition_variable fenceToClientsStart;
-    ;
 
 public:
     Fence(size_t nof_clients = 0) : nofClients(nof_clients)
@@ -192,7 +217,6 @@ class Loader
     {
         const int CACHE_LINE_LEAP = 5;
         uint64_t timestamp_start = clock_bench_time_ms();
-//         const size_t data_size_u64 = data_size / sizeof(uint64_t);
         std::uniform_int_distribution<uint64_t> distr_alloc(0, nof_allocations);
         std::uniform_int_distribution<uint64_t> distr_internal(0, allocation_size);
 
@@ -324,7 +348,6 @@ class LoaderCreator
     size_t allocation_size;
     size_t additional_stack;
     size_t iterations;
-    //     std::shared_ptr<memtier_memory> memory;
     std::shared_ptr<Allocator> allocator;
 
     std::shared_ptr<Loader> CreateLoader_()
@@ -432,29 +455,38 @@ static double calculate_zipf_denominator(int N, double s)
 static std::vector<LoaderCreator>
 create_loader_creators(TestCase test_case, size_t nof_allocations,
                        size_t allocation_size, size_t types_count,
-                       size_t total_iterations, memtier_policy_t policy,
+                       size_t total_iterations, GeneralPolicy policy,
                        std::pair<size_t, size_t> pmem_dram_ratio)
 {
     const double s = 2.0;
     std::vector<LoaderCreator> creators;
 
     std::shared_ptr<Allocator> allocator;
-    switch (policy) {
-        case MEMTIER_POLICY_STATIC_RATIO:
-            allocator =
-                std::make_shared<StaticRatioAllocator>(policy, pmem_dram_ratio);
+    switch (policy.generalType) {
+        case GeneralPolicy::GeneralType::Memtier:
+        {
+            allocator = std::make_shared<MemtierAllocator>(policy.policy,
+                                                           pmem_dram_ratio);
             break;
-        default:
-            assert(false && "case not handled");
-            break;
+        }
+        case GeneralPolicy::GeneralType::RawAllocator:
+        {
+            switch (policy.allocator) {
+                case RawAllocator::POOL:
+                    allocator = std::make_shared<PoolAllocatorWrapper>();
+                    break;
+            }
+        }
     }
     creators.reserve(types_count);
     double zipf_denominator = calculate_zipf_denominator(types_count, s);
 
     // create loader creators that share memory
     double probability_sum = 0;
-    // TODO
-    for (size_t type_idx = 0, k = 1u; type_idx < types_count; ++type_idx, ++k) {
+    // k reverses - coldest data is allocated first
+    // the order of allocs and it's effect might be investigated in the future
+    for (size_t type_idx = 0, k = types_count; type_idx < types_count;
+         ++type_idx, --k) {
         double probability = 1 / pow(k, s) / zipf_denominator;
         probability_sum += probability;
         size_t iterations = (size_t)(total_iterations * probability);
@@ -502,13 +534,15 @@ BenchResults run_test(const RunInfo &info)
 }
 
 struct BenchArgs {
-    memtier_policy_t policy;
+    GeneralPolicy policy;
     TestCase test_case;
     size_t test_iterations;
     size_t access_iterations;
     size_t threads_count;
     size_t dram_ratio_part;
     size_t pmem_ratio_part;
+    size_t nof_allocations;
+    size_t allocation_size;
 };
 
 static int parse_opt(int key, char *arg, struct argp_state *state)
@@ -516,10 +550,16 @@ static int parse_opt(int key, char *arg, struct argp_state *state)
     auto args = (BenchArgs *)state->input;
     switch (key) {
         case 's':
-            args->policy = MEMTIER_POLICY_STATIC_RATIO;
+            args->policy.generalType = GeneralPolicy::GeneralType::Memtier;
+            args->policy.policy = MEMTIER_POLICY_STATIC_RATIO;
             break;
         case 'm':
-            args->policy = MEMTIER_POLICY_DATA_MOVEMENT;
+            args->policy.generalType = GeneralPolicy::GeneralType::Memtier;
+            args->policy.policy = MEMTIER_POLICY_DATA_MOVEMENT;
+            break;
+        case 'p':
+            args->policy.generalType = GeneralPolicy::GeneralType::RawAllocator;
+            args->policy.allocator = RawAllocator::POOL;
             break;
         case 'A':
             args->test_case = TestCase::RANDOM_INC;
@@ -534,18 +574,22 @@ static int parse_opt(int key, char *arg, struct argp_state *state)
             args->test_case = TestCase::SEQ_COPY;
             break;
         case 'i':
-            assert((size_t)arg > 0 &&
-                   "test iterations must be greater than zero");
+            assert(arg && "argument for test iterations not supplied");
             args->test_iterations = std::strtoul(arg, nullptr, 10);
+            assert(args->test_iterations > 0 &&
+                   "test iterations must be greater than zero");
             break;
         case 'a':
-            assert((size_t)arg > 0 &&
-                   "access iterations must be greater than zero");
+            assert(arg && "argument for access iterations not supplied");
             args->access_iterations = std::strtoul(arg, nullptr, 10);
+            assert(args->access_iterations > 0 &&
+                   "access iterations must be greater than zero");
             break;
         case 't':
-            assert((size_t)arg > 0 && "at least one thread is required");
+            assert(arg && "argument for threads not supplied");
             args->threads_count = std::strtoul(arg, nullptr, 10);
+            assert(args->threads_count > 0 &&
+                   "at least one thread is required");
             break;
         case 'r':
         {
@@ -563,6 +607,23 @@ static int parse_opt(int key, char *arg, struct argp_state *state)
                 std::strtoul(pmem_ratio_part_str.c_str(), nullptr, 10);
             break;
         }
+        case KEY_NOF_ALLOCATIONS:
+        {
+            assert(arg &&
+                   "argument for the number of allocations not supplied");
+            args->nof_allocations = std::strtoul(arg, nullptr, 10);
+            assert(args->nof_allocations > 0 &&
+                   "at least one allocation per type is required");
+            break;
+        }
+        case KEY_ALLOCATION_SIZE:
+        {
+            assert(arg && "argument for allocation size not supplied");
+            args->allocation_size = std::strtoul(arg, nullptr, 10);
+            assert(args->allocation_size > 0 &&
+                   "allocation size has to be > 0");
+            break;
+        }
         default:
             return ARGP_ERR_UNKNOWN;
     }
@@ -571,6 +632,7 @@ static int parse_opt(int key, char *arg, struct argp_state *state)
 
 static struct argp_option options[] = {
     {"static", 's', 0, 0, "Benchmark static ratio policy.", 0},
+    {"pool", 'p', 0, 0, "Benchmark pool allocator.", 0},
     {"movement", 'm', 0, 0, "Benchmark data movement policy.", 0},
     {0, 'A', 0, 0, "Benchmark test case Random Incrementation.", 1},
     {0, 'B', 0, 0, "Benchmark test case Sequential Incrementation.", 1},
@@ -580,33 +642,48 @@ static struct argp_option options[] = {
     {"access", 'a', "size_t", 0, "Number of accesses done for each thread.", 2},
     {"threads", 't', "size_t", 0, "Threads count.", 2},
     {"ratio", 'r', "dram:pmem", 0, "DRAM to PMEM ratio.", 2},
+    {"nof_allocs", KEY_NOF_ALLOCATIONS, "size_t", 0,
+     "Number of allocations per type", 3},
+    {"alloc_size", KEY_ALLOCATION_SIZE, "size_t", 0,
+     "Size of each allocation - size of uint64_t buffer", 3},
     {0}};
 
 static struct argp argp = {options, parse_opt, nullptr, nullptr};
 
 int main(int argc, char *argv[])
 {
-    size_t ALLOCATION_SIZE_U64 = 1024 / 8 * 3 / 2; // 1.5 kB
+    // buffer with 1.5*128 uint64_t entries - > 1.5 kB
+    size_t ALLOCATION_SIZE_U64 = 1024 / sizeof(uint64_t) * 3 / 2;
     size_t NOF_ALLOCATIONS = 512 * 1024;
 
-    struct BenchArgs args = {.policy = MEMTIER_POLICY_STATIC_RATIO,
-                             .test_case = TestCase::SEQ_INC,
-                             .test_iterations = 1,
-                             .access_iterations = 500,
-                             .threads_count = 1,
-                             .dram_ratio_part = 1,
-                             .pmem_ratio_part = 8};
+    struct BenchArgs args = {
+        .policy =
+            {
+                .generalType = GeneralPolicy::GeneralType::Memtier,
+                .policy = MEMTIER_POLICY_STATIC_RATIO,
+            },
+        .test_case = TestCase::SEQ_INC,
+        .test_iterations = 1,
+        .access_iterations = 500,
+        .threads_count = 1,
+        .dram_ratio_part = 1,
+        .pmem_ratio_part = 8,
+        .nof_allocations = NOF_ALLOCATIONS,
+        .allocation_size = ALLOCATION_SIZE_U64,
+    };
     argp_parse(&argp, argc, argv, 0, 0, &args);
 
-    RunInfo info = {.test_case = args.test_case,
-                    .nof_allocations = NOF_ALLOCATIONS,
-                    .allocation_size = ALLOCATION_SIZE_U64,
-                    .types_count = args.threads_count,
-                    .iterations_major = args.test_iterations,
-                    .iterations_minor = args.access_iterations,
-                    .policy = args.policy,
-                    .dram_pmem_ratio = std::make_pair(args.dram_ratio_part,
-                                                      args.pmem_ratio_part)};
+    RunInfo info = {
+        .test_case = args.test_case,
+        .nof_allocations = args.nof_allocations,
+        .allocation_size = args.allocation_size,
+        .types_count = args.threads_count,
+        .iterations_major = args.test_iterations,
+        .iterations_minor = args.access_iterations,
+        .policy = args.policy,
+        .dram_pmem_ratio =
+            std::make_pair(args.dram_ratio_part, args.pmem_ratio_part),
+    };
 
     BenchResults stats = run_test(info);
 
