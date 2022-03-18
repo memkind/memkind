24c68b035903aeea9b0d7999f8ef2ffb775556f5
Maciej Paczocha <maciej.paczocha@intel.com>
2022-03-18 13:30:44 +0100

    Use fast_pool_allocator instead of pool_allocator in mtt_allocator

--- include/memkind/internal/mtt_internals.h
+++ include/memkind/internal/mtt_internals.h
@@ -4,8 +4,8 @@
 #pragma once
 
 #include "config.h"
+#include "memkind/internal/fast_pool_allocator.h"
 #include "memkind/internal/mmap_tracing_queue.h"
-#include "memkind/internal/pool_allocator.h"
 #include "memkind/internal/ranking.h"
 
 /// Limits to be used for PMEM vs DRAM distribution
@@ -30,7 +30,7 @@ typedef struct MttInternals {
     ranking_handle pmemRanking;
     /// Used as temporary variable for some internal operations
     metadata_handle tempMetadataHandle;
-    PoolAllocator pool;
+    FastPoolAllocator pool;
     uint64_t lastTimestamp;
     MTTInternalsLimits limits;
     MMapTracingQueue mmapTracingQueue;
@@ -43,7 +43,7 @@ extern void mtt_internals_destroy(MttInternals *internals);
 extern void *mtt_internals_malloc(MttInternals *internals, size_t size);
 extern void *mtt_internals_realloc(MttInternals *internals, void *ptr,
                                    size_t size);
-extern void mtt_internals_free(void *ptr);
+extern void mtt_internals_free(MttInternals *internals, void *ptr);
 
 extern void mtt_internals_touch(MttInternals *internals, uintptr_t address);
 extern void mtt_internals_ranking_update(MttInternals *internals,
