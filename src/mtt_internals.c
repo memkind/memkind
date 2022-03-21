44410291f32ed8335fddb08c33d655afc4752da5
Rafal Rudnicki <rafal.rudnicki@intel.com>
2022-03-21 10:20:20 -0400

    Add support for LD_PRELOAD

--- src/mtt_internals.c
+++ src/mtt_internals.c
@@ -103,8 +103,10 @@ MEMKIND_EXPORT void *mtt_internals_realloc(MttInternals *internals, void *ptr,
 {
     // TODO this is more complicated: realloc might call malloc, so we actually
     // need to extend pool
-    assert(false);
-    return NULL;
+
+    // FIXME!!
+    mtt_internals_free(internals, ptr);
+    return mtt_internals_malloc(internals, size);
 }
 
 MEMKIND_EXPORT void mtt_internals_free(MttInternals *internals, void *ptr)
