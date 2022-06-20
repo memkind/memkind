df8ad28a68267cff723c5140bfc9258e2474d584
Patryk Kaminski <patryk.kaminski@intel.com>
2022-06-20 13:22:53 +0200

    Rename memtier API manual

--- memkind.spec.mk
+++ memkind.spec.mk
@@ -176,6 +176,7 @@ rm -f %{buildroot}/%{_libdir}/libmemtier.{l,}a
 %{_mandir}/man1/*memtier.1.*
 %{_mandir}/man3/hbwmalloc.3.*
 %{_mandir}/man3/hbwallocator.3.*
+%{_mandir}/man3/libmemtier.3.*
 %{_mandir}/man3/pmemallocator.3.*
 %{_mandir}/man3/fixedallocator.3.*
 %{_mandir}/man3/%{namespace}*.3.*
