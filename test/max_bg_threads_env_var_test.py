c3527f2cefb87037e3ae502605247626a1dbbf7b
Patryk Kaminski <patryk.kaminski@intel.com>
2022-09-20 08:07:34 +0200

    Improve test readability

--- test/max_bg_threads_env_var_test.py
+++ test/max_bg_threads_env_var_test.py
@@ -21,23 +21,23 @@ class Test_max_bg_threads_env_var():
             '../environ_max_bg_threads_test')
         command = "MEMKIND_BACKGROUND_THREAD_LIMIT=" \
             f"{self.threads_limit} {cmd_path}"
-        output, retcode = self.cmd_helper.execute_cmd(command)
+        threads_count, retcode = self.cmd_helper.execute_cmd(command)
         assert retcode != 1, \
             self.fail_msg.format(
                 f"\nError: Execution of \'{command}\'"
-                f" returns {retcode}. Output: {output}")
-        return output, retcode
+                f" returns {retcode}. Threads count: {threads_count}")
+        return threads_count, retcode
 
     def test_TC_MEMKIND_max_bg_threads_env_var_min(self):
         """This test checks if MEMKIND_BACKGROUND_THREAD_LIMIT
            environment variable limits the number of
            background threads spawned correctly."""
         self.threads_limit = 1
-        output, retcode = self.run_test_binary()
-        assert int(output) == self.min_threads, \
+        threads_count, _ = self.run_test_binary()
+        assert int(threads_count) == self.min_threads, \
             self.fail_msg.format(
                 f"Error: There should be {self.min_threads} threads running. "
-                f"Counted threads: {output}.")
+                f"Counted threads: {threads_count}.")
 
     @pytest.mark.parametrize("threads_limit", [0, 10])
     def test_TC_MEMKIND_max_bg_threads_env_var_multiple_threads(self,
@@ -45,21 +45,21 @@ class Test_max_bg_threads_env_var():
         """This test checks if MEMKIND_BACKGROUND_THREAD_LIMIT
            environment variable
            allows spawning of multiple threads."""
-        output, retcode = self.run_test_binary()
-        assert int(output) > self.min_threads, \
+        threads_count, _ = self.run_test_binary()
+        assert int(threads_count) > self.min_threads, \
             self.fail_msg.format(
                 f"Error: There should be more than"
                 f" {self.min_threads} threads running. "
-                f"Counted threads: {output}.")
+                f"Counted threads: {threads_count}.")
 
     def test_TC_MEMKIND_max_bg_threads_env_var_err(self):
-        """This test checks if a negative value
+        """This test checks if a negative value of
            MEMKIND_BACKGROUND_THREAD_LIMIT environment variable is handled
            correctly."""
         self.threads_limit = -1
         threads_count, retcode = self.run_test_binary()
         assert retcode == 134, \
             self.fail_msg.format(
-                "Error: Negative value of MEMKIND_BACKGROUND_THREAD_LIMIT"
-                " should not be handled. "
+                f"Error: Negative value of MEMKIND_BACKGROUND_THREAD_LIMIT"
+                f" should not be handled. "
                 f"Counted threads: {threads_count}")
