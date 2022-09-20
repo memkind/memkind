5cff4e6ba9a1f62165ee5ab1b16f8a68e095f85e
Patryk Kaminski <patryk.kaminski@intel.com>
2022-09-20 07:55:07 +0200

    Fix max_bg_thread test not running in make check

--- test/max_bg_threads_env_var_test.py
+++ test/max_bg_threads_env_var_test.py
@@ -2,7 +2,7 @@
 # Copyright (C) 2021 Intel Corporation.
 
 import pytest
-from python_framework import CMD_helper
+from python_framework.cmd_helper import CMD_helper
 
 
 class Test_max_bg_threads_env_var():
@@ -45,7 +45,7 @@ class Test_max_bg_threads_env_var():
         """This test checks if MEMKIND_BACKGROUND_THREAD_LIMIT
            environment variable
            allows spawning of multiple threads."""
-        output, threads_count = self.run_test_binary()
+        output, retcode = self.run_test_binary()
         assert int(output) > self.min_threads, \
             self.fail_msg.format(
                 f"Error: There should be more than"
@@ -57,9 +57,9 @@ class Test_max_bg_threads_env_var():
            MEMKIND_BACKGROUND_THREAD_LIMIT environment variable is handled
            correctly."""
         self.threads_limit = -1
-        output, retcode, threads_count = self.run_test_binary()
+        threads_count, retcode = self.run_test_binary()
         assert retcode == 134, \
             self.fail_msg.format(
                 "Error: Negative value of MEMKIND_BACKGROUND_THREAD_LIMIT"
                 " should not be handled. "
-                f"Counted threads: {threads_count}. Output: {output}")
+                f"Counted threads: {threads_count}")
