80c364abdf4eee0b2424ce9c422e89c16f17817e
Adam <szadamx@gmail.com>
2023-02-08 10:37:56 +0100

    Adding handling of possible exceptions to examples

--- examples/fixed_cpp_allocator.cpp
+++ examples/fixed_cpp_allocator.cpp
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: BSD-3-Clause
-/* Copyright (C) 2021 Intel Corporation. */
+/* Copyright (C) 2021-2023 Intel Corporation. */
 
 #include "fixed_allocator.h"
 
@@ -31,23 +31,29 @@ int main(int argc, char *argv[])
 #ifdef STL_FWLIST_TEST
     {
         std::cout << "FORWARD LIST OPEN" << std::endl;
-        libmemkind::fixed::allocator<int> alc{addr, FIXED_MAP_SIZE};
-        std::forward_list<int, libmemkind::fixed::allocator<int>> fwlist{alc};
-        int i;
+        try {
+            libmemkind::fixed::allocator<int> alc{addr, FIXED_MAP_SIZE};
+            std::forward_list<int, libmemkind::fixed::allocator<int>> fwlist{
+                alc};
+            int i;
 
-        for (i = 0; i <= 20; ++i) {
-            fwlist.push_front(i);
+            for (i = 0; i <= 20; ++i) {
+                fwlist.push_front(i);
+            }
+
+            fwlist.sort(std::greater<int>());
+
+            for (auto &el : fwlist) {
+                i--;
+                assert(el == i);
+            }
+
+            fwlist.clear();
+        } catch (const std::exception &e) {
+            std::cout
+                << "Something went wrong (it's most likely a memory allocation failure): "
+                << e.what() << std::endl;
         }
-
-        fwlist.sort(std::greater<int>());
-
-        for (auto &el : fwlist) {
-            i--;
-            assert(el == i);
-        }
-
-        fwlist.clear();
-
         std::cout << "FORWARD LIST CLOSE" << std::endl;
     }
 #endif
@@ -55,21 +61,26 @@ int main(int argc, char *argv[])
 #ifdef STL_DEQUE_TEST
     {
         std::cout << "DEQUE OPEN" << std::endl;
-        libmemkind::fixed::allocator<int> int_alc{addr, FIXED_MAP_SIZE};
-        std::deque<int, libmemkind::fixed::allocator<int>> deque{int_alc};
+        try {
+            libmemkind::fixed::allocator<int> int_alc{addr, FIXED_MAP_SIZE};
+            std::deque<int, libmemkind::fixed::allocator<int>> deque{int_alc};
 
-        for (int i = 0; i < 10; i++) {
-            deque.push_back(i);
+            for (int i = 0; i < 10; i++) {
+                deque.push_back(i);
+            }
+
+            assert(deque.size() == 10);
+
+            while (!deque.empty()) {
+                deque.pop_front();
+            }
+
+            assert(deque.size() == 0);
+        } catch (const std::exception &e) {
+            std::cout
+                << "Something went wrong (it's most likely a memory allocation failure): "
+                << e.what() << std::endl;
         }
-
-        assert(deque.size() == 10);
-
-        while (!deque.empty()) {
-            deque.pop_front();
-        }
-
-        assert(deque.size() == 0);
-
         std::cout << "DEQUE CLOSE" << std::endl;
     }
 #endif
@@ -77,26 +88,31 @@ int main(int argc, char *argv[])
 #ifdef STL_MULTISET_TEST
     {
         std::cout << "MULTISET OPEN" << std::endl;
-        typedef libmemkind::fixed::allocator<std::string> multiset_alloc_t;
-        multiset_alloc_t multiset_alloc{addr, FIXED_MAP_SIZE};
-        std::multiset<std::string, std::less<std::string>,
-                      std::scoped_allocator_adaptor<multiset_alloc_t>>
-            multiset{std::scoped_allocator_adaptor<multiset_alloc_t>(
-                multiset_alloc)};
+        try {
+            typedef libmemkind::fixed::allocator<std::string> multiset_alloc_t;
+            multiset_alloc_t multiset_alloc{addr, FIXED_MAP_SIZE};
+            std::multiset<std::string, std::less<std::string>,
+                          std::scoped_allocator_adaptor<multiset_alloc_t>>
+                multiset{std::scoped_allocator_adaptor<multiset_alloc_t>(
+                    multiset_alloc)};
 
-        multiset.insert("S");
-        multiset.insert("A");
-        multiset.insert("C");
-        multiset.insert("S");
-        multiset.insert("C");
-        multiset.insert("E");
+            multiset.insert("S");
+            multiset.insert("A");
+            multiset.insert("C");
+            multiset.insert("S");
+            multiset.insert("C");
+            multiset.insert("E");
 
-        std::string test;
-        for (auto &n : multiset) {
-            test.append(n);
+            std::string test;
+            for (auto &n : multiset) {
+                test.append(n);
+            }
+            assert(test == "ACCESS");
+        } catch (const std::exception &e) {
+            std::cout
+                << "Something went wrong (it's most likely a memory allocation failure): "
+                << e.what() << std::endl;
         }
-        assert(test == "ACCESS");
-
         std::cout << "MULTISET CLOSE" << std::endl;
     }
 #endif
